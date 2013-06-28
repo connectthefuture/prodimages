524d3b143bf49b53550f5f620fa34aa0dd12e529
johnb <john.bragato@bluefly.com>
2013-06-28 17:48:21 -0400

    allUpdate script updated

--- bash/allDBUpdates.sh
+++ bash/allDBUpdates.sh
@@ -2,10 +2,28 @@
 
 . ~/.bash_profile
 
+## Import to local Mysql from DSSPRD1
+## import uno
+/usr/local/batchRunScripts/bash/importPoUpcomingStatuses.sh &
+/usr/local/batchRunScripts/bash/importEventStyleStatusImgCpy.sh &
+/usr/local/batchRunScripts/bash/importEventStyleStatus.sh &
+/usr/local/batchRunScripts/bash/import_prodsnapshot_livedata.sh & \; 
 
-
-
-/usr/local/batchRunScripts/bash/importPoUpcomingStatuses.sh & /usr/local/batchRunScripts/bash/importEventStyleStatusImgCpy.sh & /usr/local/batchRunScripts/bash/importEventStyleStatus.sh & /usr/local/batchRunScripts/bash/import_prodsnapshot_livedata.sh & 
-/mnt/Post_Ready/zProd_Server/imageServer7/scripts/bash/importData/importEventStyleInfoPMA.sh & /mnt/Post_Ready/zProd_Server/imageServer7/scripts/bash/sampleAgingImportpma.sh & /usr/local/batchRunScripts/bash/importSampleVendorStyle_pma.sh & /usr/local/batchRunScripts/bash/import_prodsnapshot.sh &
+## 2nd dssprd1 import
+/mnt/Post_Ready/zProd_Server/imageServer7/scripts/bash/importData/importEventStyleInfoPMA.sh &
+/mnt/Post_Ready/zProd_Server/imageServer7/scripts/bash/sampleAgingImportpma.sh &
+/usr/local/batchRunScripts/bash/importSampleVendorStyle_pma.sh &
+/usr/local/batchRunScripts/bash/import_prodsnapshot.sh &
 /usr/local/batchRunScripts/bash/importSKUToPhpMyAdmin.sh & \;
 
+
+### Get and Import the File7 Dir hierarchy
+/usr/local/batchRunScripts/bash/styleStringTestMakerPushed.sh &
+/usr/local/batchRunScripts/bash/styleStringZimagesPro.sh & \;
+
+/usr/local/batchRunScripts/bash/styleStringImportPma.sh \;
+
+
+
+## Regenerate the Lavender Report with updates
+/usr/local/batchRunScripts/bash/morningEventMail.sh \;
