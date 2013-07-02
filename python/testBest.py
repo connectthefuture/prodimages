fda79fbc67f4897f89177f12beae238931bcbe2d
John Bragato <jbragato@yahoo.com>
2013-07-02 17:01:46 -0400

    testbest

--- python/testBest.py
+++ python/testBest.py
@@ -5,16 +5,16 @@
 import os,sqlalchemy,shutil,datetime
 mysql_engine = sqlalchemy.create_engine('mysql+mysqldb://root:mysql@localhost:3301/data_imagepaths')
 connection = mysql_engine.connect()   
-daily_incomplete_query = '''SELECT t1.`file_path`, t1.`colorstyle`, t2.`image_ready_dt` FROM `data_imagepaths`.`push_photoselects` t1 join `data_imports`.`product_snapshot_live` t2 on t1.`colorstyle` = t2.`colorstyle` having (t2.`image_ready_dt` = '0000-00-00') ORDER BY t1.`file_path`  ASC '''
+#daily_incomplete_query = '''SELECT t1.`file_path`, t1.`colorstyle`, t2.`image_ready_dt` FROM `data_imagepaths`.`push_photoselects` t1 join `data_imports`.`product_snapshot_live` t2 on t1.`colorstyle` = t2.`colorstyle` having (t2.`image_ready_dt` = '0000-00-00') ORDER BY t1.`file_path`  ASC '''
 
-#daily_incomplete_query = '''SELECT t1.`file_path`, t1.`colorstyle`, t2.`image_ready_dt` FROM `data_imagepaths`.`push_photoselects` t1 join `data_imagepaths`.`product_snapshot` t2 on t1.`colorstyle` = t2.`colorstyle` having (t2.`image_ready_dt` = '0000-00-00') ORDER BY t1.`file_path`  ASC '''
+daily_incomplete_query = '''SELECT t1.`file_path`, t1.`colorstyle`, t2.`image_ready_dt` FROM `data_imagepaths`.`push_photoselects` t1 join `data_imagepaths`.`product_snapshot` t2 on t1.`colorstyle` = t2.`colorstyle` having (t2.`image_ready_dt` = '0000-00-00') ORDER BY t1.`file_path`  ASC '''
 
 result = connection.execute(daily_incomplete_query)
 
 
 dt = str(datetime.datetime.now())
 today = dt.split(' ')[0]
-today_folder = os.path.join('/mnt/Post_Ready/Daily', today)
+today_folder = os.path.join('/mnt/Post_Ready/Daily', today + '_testbest')
 try:
     os.mkdir(today_folder)
 except:
