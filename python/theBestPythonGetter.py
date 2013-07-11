6cea01f5972ed6c97ffb7caf864136a1dad66eab
johnb <john.bragato@bluefly.com>
2013-07-11 12:59:56 -0400

    python getter fix

--- python/theBestPythonGetter.py
+++ python/theBestPythonGetter.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python
 
-
-
 import os,sqlalchemy,shutil,datetime
 mysql_engine = sqlalchemy.create_engine('mysql+mysqldb://root:mysql@localhost:3301/data_imagepaths')
 connection = mysql_engine.connect()   
-daily_incomplete_query = '''SELECT t1.`file_path`, t1.`colorstyle`, t2.`image_ready_dt` FROM `data_imagepaths`.`push_photoselects` t1 join `data_imports`.`product_snapshot_live` t2 on t1.`colorstyle` = t2.`colorstyle` having (t2.`image_ready_dt` = '0000-00-00') ORDER BY t1.`file_path`  ASC '''
+daily_incomplete_query = '''SELECT t1.`file_path`, t1.`colorstyle`, t2.`image_ready_dt` FROM `data_imagepaths`.`push_photoselects` t1 join `data_imagepaths`.`product_snapshot_live` t2 on t1.`colorstyle` = t2.`colorstyle` having (t2.`image_ready_dt` = '0000-00-00') ORDER BY t1.`file_path`  ASC '''
 
 #daily_incomplete_query = '''SELECT t1.`file_path`, t1.`colorstyle`, t2.`image_ready_dt` FROM `data_imagepaths`.`push_photoselects` t1 join `data_imagepaths`.`product_snapshot` t2 on t1.`colorstyle` = t2.`colorstyle` having (t2.`image_ready_dt` = '0000-00-00') ORDER BY t1.`file_path`  ASC '''
 
@@ -28,9 +26,9 @@ for row in result:
         try:
         
             import os,sqlalchemy,shutil,datetime
-            dt = str(datetime.datetime.now())
-            today = dt.split(' ')[0]
-            today_folder = os.path.join('/mnt/Post_Ready/Daily', today)
+            #dt = str(datetime.datetime.now())
+            #today = dt.split(' ')[0]
+            #today_folder = os.path.join('/mnt/Post_Ready/Daily', today)
             file_path = row[0]
             file_name = file_path.split('/')[-1]
             #cmdargs = file_path, os.path.join(today_folder, file_name)
