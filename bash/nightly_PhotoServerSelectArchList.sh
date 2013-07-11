7af50fc20604cae0cd604dfd261f3dd900a8e373
johnb <john.bragato@bluefly.com>
2013-07-11 15:31:07 -0400

    uncommented Nightly Photo Arch cp as db table name for import

--- bash/nightly_PhotoServerSelectArchList.sh
+++ bash/nightly_PhotoServerSelectArchList.sh
@@ -12,8 +12,8 @@ retouchDirs=/mnt/Post_Ready/Retouch_\*
 find $retouchDirs -type f -iname \*_\?.jpg | sed s/\ /\\\\\ /g | sed s/\"/\\\"/g | sort -k1 -dur | xargs exiftool -i SYMLINKS -i /mnt/Post_Ready/Retouch_Still/EXTRA -d %Y-%m-%d -m -P -f -fast2 -'FileName' -'CreateDate' -csv | grep [2-7] | xargs -n1 | awk -v RS=' ' -v ORS='\n' -FS',' '{ print $0 }' | awk -FS',' '{ print $2, $1 }' | awk -v RS='\n' -FS',' '{ gsub(/.jpg/, "", $2); print $1,$3,$2 }' | sed 's/^ *//g' | awk -v RS='\n' -F, '{ split($2, a, "_"); print a[1], $3, $1, a[2] }' | awk '{print $1,$2,$3,$4}' | awk '{ gsub(/.jpg/, "", $4); print $1, $2, $3, $4 }' | grep "_" | sed 's:/mnt/Post_Ready::g' > $styleStringImport ;
 
 
-## cp $styleStringImport $styleStringImport_open
-#cp $styleStringImport $DATASRV/csv
+cp $styleStringImport $styleStringImport_open
+cp $styleStringImport $DATASRV/csv
 
 
 ###<------------Send Outputted CSV to data_imagepaths MySQL db-------------------->
