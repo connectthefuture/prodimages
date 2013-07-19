3c3c15f60e3d5962cc32bb2cfe7903e99578eb81
johnb <john.bragato@bluefly.com>
2013-07-19 15:07:00 -0400

    walkdir updated

--- python/walkdir_csv.py
+++ python/walkdir_csv.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
-##Walk Root Directory and Return List or all Files in all Subdirs too
+###
+## Walk Root Directory and Return List or all Files in all Subdirs too
 def recursive_dirlist(rootdir):
     import os
     walkedlist = []
@@ -17,17 +18,54 @@ def recursive_dirlist(rootdir):
     #    dirnames.remove('.git')
     return walkedlist
 
-def get_exif(filepath):
+
+###
+## Extract All Metadata from Image File as Dict
+def get_exif(file_path):
     from PIL import Image
     from PIL.ExifTags import TAGS
     exifdata = {}
-    i = Image.open(filepath)
-    info = i._getexif()
+    im = Image.open(file_path)
+    info = im._getexif()
     for tag, value in info.items():
         decoded = TAGS.get(tag, tag)
         exifdata[decoded] = value
     return exifdata
 
+
+###
+## Make Lowres Thumnails from Image files or Directory Full of Image Files
+def make_lowres_thumbnails_dir_or_singlefile(pathname):
+    from PIL import Image
+    import glob, os
+    size = 600, 720
+    
+    ## If input variable is a single File Create 1 Thumb
+    if os.path.isfile(pathname):
+        try:    
+            infile = os.path.abspath(pathname)
+            filename, ext = os.path.splitext(infile)
+            im = Image.open(infile)
+            im.thumbnail(size, Image.ANTIALIAS)
+            im.save(filename + ".thumbnail", "JPG")
+        except:
+            print "Error Creating Single File Thumbnail for {0}".format(infile)
+    
+    ## If input variable is a Directory Decend into Dir and Crate Thumnails for all jpgs
+    elif os.path.isdir(pathname):
+        dirname = os.path.abspath(pathname)
+        for infile in glob.glob(os.path.join(dirname, "*.jpg")):
+            try:
+                filename, ext = os.path.splitext(infile)
+                im = Image.open(infile)
+                im.thumbnail(size, Image.ANTIALIAS)
+                im.save(filename + ".thumbnail", "JPG")
+            except:
+                print "Error Creating Thumbnail for {0}".format(infile)
+
+
+###    
+## Write Rows to Dated CSV in Users Home Dir
 def csv_write_datedOutfile(lines):
     import csv,datetime,os
     dt = str(datetime.datetime.now())
@@ -42,7 +80,6 @@ def csv_write_datedOutfile(lines):
 ############################ Run ######################################################## Run ############################
 
 from PIL import Image
-#import pyexiv2
 import os,sys,re
 
 rootdir = sys.argv[1]
@@ -71,6 +108,7 @@ for line in walkedout:
             stylestringsdict_tmp['alt'] = alt
             stylestringsdict[file_path] = stylestringsdict_tmp
             file_path_reletive = file_path.replace('/mnt/Post_Ready/zImages_1/', '/zImages/')
+            file_path_reletive = file_path.replace('JPG', 'jpg')
             ## Format CSV Rows
             row = "{0},{1},{2},{3}".format(colorstyle,photo_date,file_path_reletive,alt)
             print row
@@ -84,7 +122,7 @@ for line in walkedout:
 
 
 
-## Create Dir Struct and copy files to it
+## Create Dir Struct under ZIMAGES_1 if dir doesnt Exist and copy files to it
 for k,v in stylestringsdict.iteritems():
     import os,sys,shutil
     src = k
@@ -94,7 +132,13 @@ for k,v in stylestringsdict.iteritems():
     try:
         os.mkdirs(destdir)
         shutil.copy2(src,destdir)
- ###------TODO MAKE COPY LOWRES AFTER COPYING TO ZIMAGES USING PIL   
+        
+        ## AFTER COPYING TO HASED DIR STRUCTURE REZ DOWN IMG TO 600X720 For faster Browser Loading
+        try:
+            make_lowres_thumbnails_dir_or_singlefile(destpath)
+        except:
+            print "Error Creating Thumbnail for {0}".format(destpath)
+            
     except:
         #try:
         shutil.copy2(src,destdir)
@@ -106,54 +150,57 @@ for k,v in stylestringsdict.iteritems():
     
 
 
-#Import to MySql DB
-import sqlalchemy #, oursql
-from sqlalchemy import *
-import _mysql
+#Iterate through Dict of Walked Directory, then Import to MySql DB
+import sqlalchemy
+#import _mysql
 
+## First compile the SQL Fields as key value pairs
 fulldict = {}
 for k,v in stylestringsdict.iteritems():
     dfill = {}
     dfill['colorstyle'] = v['colorstyle']
     dfill['photo_date'] = v['photo_date']
-    fpathstrip = k
-    fpathstrip = fpathstrip.replace('/mnt/Post_Ready/zImages_1/', '/zImages/')
-    dfill['file_path'] = fpathstrip
+    file_path = k
+    file_path = file_path.replace('/mnt/Post_Ready/zImages_1/', '/zImages/')
+    dfill['file_path'] = file_path
     dfill['alt'] = v['alt']
     fulldict[k] =  dfill
     #csv_datedOutFile(kvstring)
     #print kvstring
     #d = dict(colorstyle=colorstyle, photo_date=photo_date, file_path=file_path, alt=alt)
-    print dfill
+    #print dfill
     print 'DebuggerLine'
 
-#sql = "INSERT INTO data_imagepaths.push_photoselects (colorstyle, photo_date, file_path, alt) VALUES (%('" + colorstyle + "')s,%('" + photo_date + "')d,%('" + file_path + "')s,%(('" + alt + "'))s"
-#print sql
+
+## Take the compiled k/v pairs and Format + Insert into MySQL DB
 for k,v in fulldict.iteritems():
     try:
         mysql_engine = sqlalchemy.create_engine('mysql+mysqldb://root:mysql@prodimages.ny.bluefly.com:3301/data_imagepaths')
-        #push_photoselects = Table('push_photoselects', mysql_engine)
-        #i = push_photoselects.insert()
         connection = mysql_engine.connect()
-        #i.execute(dfill)
+
         connection.execute("""INSERT INTO push_photoselects (colorstyle, photo_date, file_path, alt) VALUES (%s, %s, %s, %s)""", v['colorstyle'], v['photo_date'], v['file_path'],  v['alt'])
+    
+    
     except sqlalchemy.exc.IntegrityError:
         print "Duplicate Entry {0}".format(fpathstrip)    
 
     #for vals in v:
     #    print v[vals]
 
-
+        #push_photoselects = Table('push_photoselects', mysql_engine)
+        #i = push_photoselects.insert()
+#sql = "INSERT INTO data_imagepaths.push_photoselects (colorstyle, photo_date, file_path, alt) VALUES (%('" + colorstyle + "')s,%('" + photo_date + "')d,%('" + file_path + "')s,%(('" + alt + "'))s"
+#print sql
 #
 #
 #class PhotoMetaData(f):
 #
 #    def __init__(self, f):
 #        #self.files_list = []
-##        self.recursivefilelist = []
+##       self.recursivefilelist = []
 #        self.MetaDict = {}
 #        self.f = f
-##        self.__update(directory)
+##       self.__update(directory)
 #
 #
 #    def get_exif(self,f):
