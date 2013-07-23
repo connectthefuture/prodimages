f430edf1e75174a1ba18ced51ccb4810b6666f86
johnb <john.bragato@bluefly.com>
2013-07-23 14:43:40 -0400

    chmod getter

--- python/theBestPythonGetter.py
+++ python/theBestPythonGetter.py
@@ -35,7 +35,7 @@ for row in result:
             #cmdargs = file_path, os.path.join(today_folder, file_name)
             dest_file = os.path.join(today_folder, file_name)
             shutil.copy2(file_path, dest_file)
-            os.chmod(today_folder,00755)
+            os.chmod(dest_file,00755)
             #rowlist.append(cmdargs)
             ncount += 1
             print "Files Found: {0},\t File Name: {1}".format(ncount,file_name)
