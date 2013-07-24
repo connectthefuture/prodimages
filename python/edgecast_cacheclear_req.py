3adb62ed24a2d6d140068439074be98a0e09a021
johnb <john.bragato@bluefly.com>
2013-07-24 11:54:02 -0400

    mod cacheclear

--- python/edgecast_cacheclear_req.py
+++ python/edgecast_cacheclear_req.py
@@ -1,4 +1,4 @@
-import pycurl
+import pycurl,json,sys,os
 
 
 #if token != "" && account != "" && mediaPath != "" && mediaType != "":
@@ -7,18 +7,21 @@ import pycurl
 ## Setup variables
 token = "9af6d09a-1250-4766-85bd-29cebf1c984f"
 account = "4936"
-mediaPath = sys.argv[1]
+#mediaPath = sys.argv[1]
+mediaPath = 'http://cdn.is.belleandclive.com/mgen/Bluefly/prodImage.ms?productCode=324860301&width=320&height=430&ver=3'
 mediaType = "8"
 
 
 purgeURL = "https://api.edgecast.com/v2/mcc/customers/{0}/edge/purge".format(account)
 
 ## Create send data
-request_params = (
-'MediaPath' = mediaPath,
-'MediaType' = mediaType
-)
-data = json_encode(request_params)
+data = json.dumps({
+"mediaPath" : 'MediaPath',
+"mediaType" : 'MediaType'
+})
+
+
+#data = json_encode(request_params)
 head_authtoken = "Authorization: tok:{0}".format(token)
 head_content_len= "Content-length: {0})".format(str(len(data)))
 head_accept = 'Accept: application/json'
@@ -30,7 +33,7 @@ c.setopt(c.URL, purgeURL)
 c.setopt(c.PORT , 443)
 c.setopt(c.SSL_VERIFYPEER, 0)
 c.setopt(c.HEADER, 0)
-curl_setopt(ch, CURLINFO_HEADER_OUT, 1)
+c.setopt(c.HEADER_OUT, 1)
 c.setopt(c.RETURNTRANSFER, true)
 c.setopt(c.FORBID_REUSE, 1)
 c.setopt(c.FRESH_CONNECT, 1)
@@ -52,23 +55,18 @@ except pycurl.error, error:
 
 
 
-c = pycurl.Curl()
-c.setopt(c.URL, 'http://myappserver.com/ses1')
-c.setopt(c.CONNECTTIMEOUT, 5)
-c.setopt(c.TIMEOUT, 8)
-c.setopt(c.COOKIEFILE, '')
-c.setopt(c.FAILONERROR, True)
-c.setopt(c.HTTPHEADER, ['Accept: text/html', 'Accept-Charset: UTF-8'])
-try:
-    c.perform()
+#c = pycurl.Curl()
+#c.setopt(c.URL, 'http://myappserver.com/ses1')
+#c.setopt(c.CONNECTTIMEOUT, 5)
+#c.setopt(c.TIMEOUT, 8)
+#c.setopt(c.COOKIEFILE, '')
+#c.setopt(c.FAILONERROR, True)
+#c.setopt(c.HTTPHEADER, ['Accept: text/html', 'Accept-Charset: UTF-8'])
+#
+#c.perform()
 
-    c.setopt(c.URL, 'http://myappserver.com/ses2')
-    c.setopt(c.POSTFIELDS, 'foo=bar&bar=foo')
-    c.perform()
-
-
-
-
-
+#c.setopt(c.URL, 'http://myappserver.com/ses2')
+#c.setopt(c.POSTFIELDS, 'foo=bar&bar=foo')
+#c.perform()
 
 
