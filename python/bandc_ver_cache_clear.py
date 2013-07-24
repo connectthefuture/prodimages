22249c41361c2b5f2716557222811b5a9cf37894
johnb <john.bragato@bluefly.com>
2013-07-24 13:48:52 -0400

    working cacheclear

--- python/bandc_ver_cache_clear.py
+++ python/bandc_ver_cache_clear.py
@@ -53,6 +53,54 @@ def get_catid_from_eventid(eventid):
     else:
         print "Event {0} has not been pushed to ATG yet".format(eventid)
 
+
+def send_purge_request_edgecast(mediaPath):
+    import pycurl,json,sys,os
+    ## Setup variables
+    token = "9af6d09a-1250-4766-85bd-29cebf1c984f"
+    account = "4936"
+    #mediaPath = sys.argv[1]
+    #mediaPath = 'http://cdn.is.belleandclive.com/mgen/Bluefly/prodImage.ms?productCode=324860301&width=320&height=430&ver=3'
+    mediaType = "8"
+
+    purgeURL = "https://api.edgecast.com/v2/mcc/customers/{0}/edge/purge".format(account)
+
+    if token != "" and account != "" and mediaPath != "" and mediaType != "":
+        ## Create send data
+        data = json.dumps({
+        'MediaPath' : mediaPath,
+        'MediaType' : mediaType 
+        })
+
+        #data = json_encode(request_params)
+        head_authtoken = "Authorization: tok:{0}".format(token)
+        head_content_len= "Content-length: {0}".format(str(len(data)))
+        head_accept = 'Accept: application/json'
+        head_contenttype = 'Content-Type: application/json'
+        #print head_content_len
+        ## Send the request to Edgecast
+        ### Send the request to Edgecast
+        c = pycurl.Curl()
+        c.setopt(pycurl.URL, purgeURL)
+        c.setopt(pycurl.PORT , 443)
+        c.setopt(pycurl.SSL_VERIFYPEER, 0)
+        c.setopt(pycurl.HEADER, 0)
+        #c.setopt(pycurl.INFOTYPE_HEADER_OUT, 1)
+        #c.setopt(pycurl.RETURNTRANSFER, 1)
+        c.setopt(pycurl.FORBID_REUSE, 1)
+        c.setopt(pycurl.FRESH_CONNECT, 1)
+        c.setopt(pycurl.CUSTOMREQUEST, "PUT")
+        c.setopt(pycurl.POSTFIELDS,data)
+        c.setopt(pycurl.HTTPHEADER, [head_authtoken, head_contenttype, head_accept, head_content_len])
+        try:
+            c.perform()
+            c.close()
+            print "Successfully Sent Purge Request for --> {0}".format(mediaPath)
+        except pycurl.error, error:
+            errno, errstr = error
+            print 'An error occurred: ', errstr
+
+
 ############ RUN ###########
 
 import sys,re,os
@@ -72,12 +120,10 @@ versioned_links = return_versioned_urls(listpage_urllist)
 #count = 0
 if len(versioned_links) <= 50:
 
-    for line in versioned_links:
-        #line = line.strip('[u')
-        #line = line.strip(']')
-        #print "Formatted {0}".format(line)
-        print str(line)
+    for url_purge in versioned_links:
+        send_purge_request_edgecast(url_purge)
         #versioned_links.pop()
         #csv_write_datedOutfile(line)
-
+else:
+    print "Failed -- Over 50 URLs Submitted"    
 ### RUN with bandc_ver_cache_clear.py 3380 | xargs -L1 | sed -E 's/\[u//g' | sed -E 's/\]//g'
--- python/edgecast_cacheclear_req.py
+++ python/edgecast_cacheclear_req.py
@@ -1,9 +1,5 @@
 import pycurl,json,sys,os
 
-
-#if token != "" && account != "" && mediaPath != "" && mediaType != "":
-
-
 ## Setup variables
 token = "9af6d09a-1250-4766-85bd-29cebf1c984f"
 account = "4936"
@@ -11,62 +7,74 @@ account = "4936"
 mediaPath = 'http://cdn.is.belleandclive.com/mgen/Bluefly/prodImage.ms?productCode=324860301&width=320&height=430&ver=3'
 mediaType = "8"
 
-
 purgeURL = "https://api.edgecast.com/v2/mcc/customers/{0}/edge/purge".format(account)
 
-## Create send data
-data = json.dumps({
-"mediaPath" : 'MediaPath',
-"mediaType" : 'MediaType'
-})
+if token != "" and account != "" and mediaPath != "" and mediaType != "":
+    ## Create send data
+    data = json.dumps({
+    'MediaPath' : mediaPath,
+    'MediaType' : mediaType 
+    })
+
+    #data = json_encode(request_params)
+    head_authtoken = "Authorization: tok:{0}".format(token)
+    head_content_len= "Content-length: {0}".format(str(len(data)))
+    head_accept = 'Accept: application/json'
+    head_contenttype = 'Content-Type: application/json'
+    #print head_content_len
+    ## Send the request to Edgecast
+    ### Send the request to Edgecast
+    c = pycurl.Curl()
+    c.setopt(pycurl.URL, purgeURL)
+    c.setopt(pycurl.PORT , 443)
+    c.setopt(pycurl.SSL_VERIFYPEER, 0)
+    c.setopt(pycurl.HEADER, 0)
+    #c.setopt(pycurl.INFOTYPE_HEADER_OUT, 1)
+    #c.setopt(pycurl.RETURNTRANSFER, 1)
+    c.setopt(pycurl.FORBID_REUSE, 1)
+    c.setopt(pycurl.FRESH_CONNECT, 1)
+    c.setopt(pycurl.CUSTOMREQUEST, "PUT")
+    c.setopt(pycurl.POSTFIELDS,data)
+    c.setopt(pycurl.HTTPHEADER, [head_authtoken, head_contenttype, head_accept, head_content_len])
+    try:
+        c.perform()
+        c.close()
+        print "Successfully Sent Purge Request for --> {0}".format(mediaPath)
+    except pycurl.error, error:
+        errno, errstr = error
+        print 'An error occurred: ', errstr
 
 
-#data = json_encode(request_params)
-head_authtoken = "Authorization: tok:{0}".format(token)
-head_content_len= "Content-length: {0})".format(str(len(data)))
-head_accept = 'Accept: application/json'
-head_contenttype = 'Content-Type: application/json'
+    ### Send the request to Edgecast
+    #c = pycurl.Curl()
+    #c.setopt(pycurl.URL, purgeURL)
+    #c.setopt(pycurl.PORT , 443)
+    #c.setopt(pycurl.SSL_VERIFYPEER, 0)
+    #c.setopt(pycurl.HEADER, 0)
+    ##c.setopt(c.HEADER_OUT, 1)
+    #c.setopt(pycurl.RETURNTRANSFER, true)
+    #c.setopt(pycurl.FORBID_REUSE, 1)
+    #c.setopt(pycurl.FRESH_CONNECT, 1)
+    #c.setopt(pycurl.CUSTOMREQUEST, "PUT")
+    #c.setopt(pycurl.POSTFIELDS,data)
+    #c.setopt(pycurl.HTTPHEADER, [
+    #head_authtoken,
+    #head_contenttype,
+    #head_accept,
+    #head_content_len
+    #])
+    #c = pycurl.Curl()
+    #c.setopt(c.URL, 'http://myappserver.com/ses1')
+    #c.setopt(c.CONNECTTIMEOUT, 5)
+    #c.setopt(c.TIMEOUT, 8)
+    #c.setopt(c.COOKIEFILE, '')
+    #c.setopt(c.FAILONERROR, True)
+    #c.setopt(c.HTTPHEADER, ['Accept: text/html', 'Accept-Charset: UTF-8'])
+    #
+    #c.perform()
 
-## Send the request to Edgecast
-c = pycurl.Curl()
-c.setopt(c.URL, purgeURL)
-c.setopt(c.PORT , 443)
-c.setopt(c.SSL_VERIFYPEER, 0)
-c.setopt(c.HEADER, 0)
-c.setopt(c.HEADER_OUT, 1)
-c.setopt(c.RETURNTRANSFER, true)
-c.setopt(c.FORBID_REUSE, 1)
-c.setopt(c.FRESH_CONNECT, 1)
-c.setopt(c.CUSTOMREQUEST, "PUT")
-c.setopt(c.POSTFIELDS,data)
-c.setopt(c.HTTPHEADER, [
-head_authtoken,
-head_contenttype,
-head_accept,
-head_content_len
-])
-try:
-    c.perform()
+    #c.setopt(c.URL, 'http://myappserver.com/ses2')
+    #c.setopt(c.POSTFIELDS, 'foo=bar&bar=foo')
+    #c.perform()
 
-except pycurl.error, error:
-    errno, errstr = error
-    print 'An error occurred: ', errstr
-
-
-
-
-#c = pycurl.Curl()
-#c.setopt(c.URL, 'http://myappserver.com/ses1')
-#c.setopt(c.CONNECTTIMEOUT, 5)
-#c.setopt(c.TIMEOUT, 8)
-#c.setopt(c.COOKIEFILE, '')
-#c.setopt(c.FAILONERROR, True)
-#c.setopt(c.HTTPHEADER, ['Accept: text/html', 'Accept-Charset: UTF-8'])
-#
-#c.perform()
-
-#c.setopt(c.URL, 'http://myappserver.com/ses2')
-#c.setopt(c.POSTFIELDS, 'foo=bar&bar=foo')
-#c.perform()
-
-
+    
