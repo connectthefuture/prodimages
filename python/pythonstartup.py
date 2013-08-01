b5297069a1aa3c20cb4d52a014cad04e33c9ec3e
johnb <john.bragato@bluefly.com>
2013-08-01 15:52:59 -0400

    added recurse dir function to pythonstartup -- adjusted Imap-unseen script maybe?

--- python/pythonstartup.py
+++ python/pythonstartup.py
@@ -80,6 +80,23 @@ regex_exif = re.compile(pattern_exiftag + "=" + pattern_exifval)
 #startDateFrom=&startDateTo=&colorGroup=&searchBrand=&eventId=&productStatus=&merchantStatus=&inventory=&active=&store=&styleNumbers=324162301&vendorStyleNumbers=&shortName=&poHdrs=&searchCategory=&jdaCategory=&Submit=Search&exportToExcel=false&exportImages=false&solrQuery=&currentPage=0
 
 
+###
+## Walk Root Directory and Return List or all Files in all Subdirs too
+def recursive_dirlist(rootdir):
+    import os
+    walkedlist = []
+    for dirname, subdirnames, filenames in os.walk(rootdir):
+        # append path of all filenames to walkedlist
+        for filename in filenames:
+            file_path = os.path.abspath(os.path.join(dirname, filename))
+            if os.path.isfile(file_path):
+                walkedlist.append(file_path)
+    # Advanced usage:
+    # editing the 'dirnames' list will stop os.walk() from recursing into there.
+    #if '.git' in dirnames:
+    # don't go into any .git directories.
+    #    dirnames.remove('.git')
+    return walkedlist
 
 
 #END###### REGEX PATTERN DEFINITIONS
