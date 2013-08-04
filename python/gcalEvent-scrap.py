ed54a36bfc496670ded8c0de4964a38b000c34f2
jb <relic7@aol.com>
2013-08-04 01:35:43 -0400

    gcalScraps and Dev -- Templates and default cal parsing

--- python/gcalEvent-scrap.py
+++ python/gcalEvent-scrap.py
@@ -69,3 +69,68 @@ def UpdateCalendar(calendar_name=selected_calendar, cal_client=gcal_connection, 
         calendar_name.color = gdata.calendar.data.ColorProperty(value=color)
         updated_calendar = cal_client.Update(calendar_name)
     return updated_calendar
+    
+    
+    
+    
+import GCalendar_Class
+import gdata
+#gcal_client = GCalendar_Class.GCalendar(email='john.bragato@gmail.com', password='yankee17')
+#https://www.google.com/calendar/feeds/khn4f4kmgcu19h7tgh8ejv8894%40group.calendar.google.com/private-8cd060685625899c147572bcfe26fc55/basic'
+myemail = 'john.bragato@gmail.com'
+password = 'yankee17'
+#calendar_id = 'khn4f4kmgcu19h7tgh8ejv8894%40group.calendar.google.com'
+#private_id = '/private-8cd060685625899c147572bcfe26fc55/basic'
+ev_start = '2013-08-14'
+ev_end = '2013-08-24'
+#calendar_id = 'khn4f4kmgcu19h7tgh8ejv8894%40group.calendar.google.com/private-8cd060685625899c147572bcfe26fc55/basic/onusa2c5mbu26tpo710hljd1hc'
+calendar_id = 'khn4f4kmgcu19h7tgh8ejv8894%40group.calendar.google.com/private-8cd060685625899c147572bcfe26fc55/basic'
+default1_calid = 'http://www.google.com/calendar/feeds/khn4f4kmgcu19h7tgh8ejv8894%40group.calendar.google.com/private-8cd060685625899c147572bcfe26fc55/basic'
+
+## Cal Ent FEED      
+
+gcal_client = gdata.calendar.client.CalendarClient(source=myemail)
+gcal_client.ClientLogin(myemail, password, gcal_client.source)       
+feed_uri = gcal_client.get_calendar_event_feed(calendar=calendar_id)
+event_uri = os.path.join(feed_uri)
+calendar_entry = gcal_client.get_calendar_entry(event_uri, desired_class=gdata.calendar.data.CalendarEventEntry())
+
+## EVENTS FEED       
+eventsfeed = gcal_client.get_calendar_event_feed()        
+event_entries = eventsfeed.entry[:]
+for entry in event_entries:
+    print entry
+    #entry_color = entry.color.value
+
+
+eventsfeed.title = atom.Title(text='1AMTitle')
+
+event.title = atom.Title(text=titleid)
+event.when.append(gdata.calendar.When(start_time=ev_start, end_time=ev_end))
+
+## OwnCalFeed  
+## Get Entries from own cal
+ownfeed = gcal_client.GetOwnCalendarsFeed()
+own_etag = ownfeed.etag
+ownfeed.title = atom.Title(text=titleid)
+
+entries = ownfeed.entry[:]
+for entry in entries:
+    print entry
+    entry_color = entry.color.value
+
+
+wc = gdata.calendar.WebContent()
+wc.url = 'http://www.thefreedictionary.com/_/WoD/wod-module.xml'
+wc.width = '300'
+wc.height = '136'
+wc.gadget_pref.append(gdata.calendar.WebContentGadgetPref(name='Days', value='1'))
+wc.gadget_pref.append(gdata.calendar.WebContentGadgetPref(name='Format', value='0'))
+
+wcl = gdata.calendar.WebContentLink()
+wcl.title = 'Word of the Day'
+wcl.href = 'http://www.thefreedictionary.com/favicon.ico'
+wcl.type = 'application/x-google-gadgets+xml'
+wcl.web_content = wc
+
+event.link.append(wcl)
