b69381cce07d8762c86ff6f293ac88ce74c24968
johnb <john.bragato@bluefly.com>
2013-08-08 12:30:09 -0400

    gcal Prod

--- python/gcal_ProductionNumbers.py
+++ python/gcal_ProductionNumbers.py
@@ -38,7 +38,7 @@ def sql_query_production_numbers():
     retouchcomplete_dict = {}
     for row in retouchcomplete:
             retouchcomplete_dict[row['retouch_complete_dt']] = row['retouch_complete_dt']
-            retouchcomplete_dict[row['completion_total']] = row['completion_total']
+            retouchcomplete_dict[row['retouch_total']] = row['retouch_total']
 
 
     ### Get Copy Complete Totals and Build Dict of key value pairs
@@ -52,7 +52,7 @@ def sql_query_production_numbers():
     copycomplete_dict = {}
     for row in copycomplete:
             copycomplete_dict[row['copy_complete_dt']] = row['copy_complete_dt']
-            copycomplete_dict[row['completion_total']] = row['completion_total']
+            copycomplete_dict[row['copy_total']] = row['copy_total']
 
 
 
@@ -63,7 +63,21 @@ def sql_query_production_numbers():
 #for key,value in future_events.iteritems():
     #for kv in [value]:
 
-
+def recursive_dirlist(rootdir):
+    import os
+    walkedlist = []
+    for dirname, subdirnames, filenames in os.walk(rootdir):
+        # append path of all filenames to walkedlist
+        for filename in filenames:
+            file_path = os.path.abspath(os.path.join(dirname, filename))
+            if os.path.isfile(file_path):
+                walkedlist.append(file_path)
+    # Advanced usage:
+    # editing the 'dirnames' list will stop os.walk() from recursing into there.
+    #if '.git' in dirnames:
+    # don't go into any .git directories.
+    #    dirnames.remove('.git')
+    return walkedlist
 
 
 def gcal_insert_bc_event(titleid, descfull, lockv, sdatekv, edatekv):
@@ -145,7 +159,6 @@ def delete_gcalendar_event(titleid, calendar_name='Default1', myemail='john.brag
     calendar = gCalMNG.getCalendar(calendar_name)
     events = calendar.getEvents()
     for event in events:
-#        if event.getTitle().split(' ')[1] == titleid.split(' ')[1]:
         if event.getTitle() == titleid:
             event.delete()
             return "Deleted {0}".format(titleid)
@@ -163,111 +176,74 @@ def delete_gcalendar_event(titleid, calendar_name='Default1', myemail='john.brag
 # Print the updated date.
 #print updated_event['updated']
 
-#years_dict = dict()
+
 #
-#for line in list:
-#    if line[0] in years_dict:
-#        # append the new number to the existing array at this slot
-#        years_dict[line[0]].append(line[1])
-#    else:
-#        # create a new array in this slot
-#        years_dict[line[0]] = [line[1]]
+prodcomplete_dict, retouchcomplete_dict, copycomplete_dict = sql_query_production_numbers()
 
-#from collections import defaultdict
-#styles = defaultdict(list)
+print prodcomplete_dict
+
+#count = 0
+#for k,v in future_events.iteritems():
+#    import datetime, time
+#    for value in [v]:
+#        titlekv = str(value['event_id'])
+#        desckv = str(value['event_title'])
+#        desckv = desckv.replace('&', 'And')
+#        desckv = desckv.replace('%', ' Percent')
+#        colorstyles = future_styles.get(value['event_id'])
+#        colorstyles = sorted(colorstyles)
+#        still_complete = []
+#        fashion_complete = []
+#        
+#        
+#        for colorstyle in colorstyles:
+#            if colorstyle[1] == 'Production Complete':
+#                complete.append(colorstyle)
+#            elif colorstyle[1] == 'Production Incomplete':
+#                incomplete.append(colorstyle)
+#        incomplete_styles = "{0} Incomplete Styles = {1}".format(len(incomplete),incomplete)
+#        complete_styles = "{0} Complete Styles = {1}".format(len(complete),complete)
+#        colorstyles_statuses = "{0}\n{1}".format(incomplete_styles,complete_styles)
 #
-future_events, future_styles = sqlQueryEventsUpcoming()
-#for key,value in future_styles.iteritems():
-#    #for kv in [value]:
-#    #d[value['event_id']].append(key)
-#    print "Event {0} has {1} Styles".format(key,len(value))
-
-#print d
-
-count = 0
-for k,v in future_events.iteritems():
-    import datetime, time
-    for value in [v]:
-        titlekv = str(value['event_id'])
-        desckv = str(value['event_title'])
-        desckv = desckv.replace('&', 'And')
-        desckv = desckv.replace('%', ' Percent')
-        colorstyles = future_styles.get(value['event_id'])
-        colorstyles = sorted(colorstyles)
-        incomplete = []
-        complete = []
-        for colorstyle in colorstyles:
-            if colorstyle[1] == 'Production Complete':
-                complete.append(colorstyle)
-            elif colorstyle[1] == 'Production Incomplete':
-                incomplete.append(colorstyle)
-        incomplete_styles = "{0} Incomplete Styles = {1}".format(len(incomplete),incomplete)
-        complete_styles = "{0} Complete Styles = {1}".format(len(complete),complete)
-        colorstyles_statuses = "{0}\n{1}".format(incomplete_styles,complete_styles)
-
-        count_complete = len(complete)
-        count_incomplete = len(incomplete)
-        count_total = count_complete + count_incomplete
-
-        progress = count_complete/count_total*100
-
-        if len(incomplete) == 0:
-            event_complete_flag = True
-        else:
-            event_complete_flag = False
-
-        status = value['production_status']
-        prod_category = value['prod_category']
-        category_id = value['category_id']
-
-        pmurl = "http://pm.bluefly.corp/manager/event/editevent.html?id="
-        pmimgs = "http://pm.bluefly.corp/manager/event/viewproductimages.html?id="
-        bcurl = "http://www.belleandclive.com/browse/sales/details.jsp?categoryId="
-
-        pmurl = pmurl + titlekv
-        pmimgs = pmimgs + titlekv
-        bcurl = pmurl + titlekv
-
-        try:
-            if colorstyles == None:
-                lockv = str(pmurl)
-            else:
-                lockv = str(pmimgs)
-        except TypeError:
-            lockv = str(bcurl)
-
-        sdatekvraw = '{:%Y,%m,%d,%H,%M,%S,00,00,00}'.format(value['ev_start'])
-        edatekvraw = '{:%Y,%m,%d,%H,%M,%S,00,00,00}'.format(value['ev_end'])
-        sdatekvsplit = sdatekvraw.split(",")
-        edatekvsplit = edatekvraw.split(",")
-        sdatekv = map(int,sdatekvsplit)
-        edatekv = map(int,edatekvsplit)
-        titleid = 'Event {0} -- {1}'.format(titlekv,desckv)
-        descfull = '{0} {1} in Event {2}:\n {3}\n'.format(len(colorstyles), prod_category, titlekv, colorstyles_statuses)
-        descfull = str(descfull)
-        #print titleid, descfull, edatekv, prod_category, lockv
-        count += 1
-        #print count
-
-        try:
-        #gcal_insert_bc_event(titleid, descfull, lockv, sdatekv, edatekv)
-            gCalMNG = gcal_login_jb()
-            #calendar = gCalMNG.getCalendar("Default1")
-            gcalevents = gCalMNG.getCalendar("ProductionNumbers").getEvents()
-            print len(gcalevents)
-            gcaleventslist = []
-            for event in gcalevents:
-                gcalevent = event.getTitle()
-                if gcalevent == titleid:
-                    continue
-                else:
-                    print event.getContent()
-                    #print time.strftime("%Y-%m-%dT%H:%M:%S" , time.localtime(event.getStartTime()))
-                    #print time.strftime("%Y-%m-%dT%H:%M:%S" , time.localtime(event.getEndTime()))
-            ev = newEvent(myname, myemail, titleid, descfull, lockv, time.mktime(sdatekv), time.mktime(edatekv))
-            print ev
-            calendar.addEvent (ev)
-        except xml.parsers.expat.ExpatError:
-        #except:
-            print "FAILED" #+ k,v
-        #    continue
+#        count_complete = len(complete)
+#        count_incomplete = len(incomplete)
+#        count_total = count_complete + count_incomplete
+#
+#        progress = count_complete/count_total*100
+#
+#
+#        sdatekvraw = '{:%Y,%m,%d,%H,%M,%S,00,00,00}'.format(value['ev_start'])
+#        edatekvraw = '{:%Y,%m,%d,%H,%M,%S,00,00,00}'.format(value['ev_end'])
+#        sdatekvsplit = sdatekvraw.split(",")
+#        edatekvsplit = edatekvraw.split(",")
+#        sdatekv = map(int,sdatekvsplit)
+#        edatekv = map(int,edatekvsplit)
+#        titleid = 'Event {0} -- {1}'.format(titlekv,desckv)
+#        descfull = '{0} {1} in Event {2}:\n {3}\n'.format(len(colorstyles), prod_category, titlekv, colorstyles_statuses)
+#        descfull = str(descfull)
+#        #print titleid, descfull, edatekv, prod_category, lockv
+#        count += 1
+#        #print count
+#
+#        try:
+#        #gcal_insert_bc_event(titleid, descfull, lockv, sdatekv, edatekv)
+#            gCalMNG = gcal_login_jb()
+#            #calendar = gCalMNG.getCalendar("Default1")
+#            gcalevents = gCalMNG.getCalendar("ProductionNumbers").getEvents()
+#            print len(gcalevents)
+#            gcaleventslist = []
+#            for event in gcalevents:
+#                gcalevent = event.getTitle()
+#                if gcalevent == titleid:
+#                    continue
+#                else:
+#                    print event.getContent()
+#                    #print time.strftime("%Y-%m-%dT%H:%M:%S" , time.localtime(event.getStartTime()))
+#                    #print time.strftime("%Y-%m-%dT%H:%M:%S" , time.localtime(event.getEndTime()))
+#            ev = newEvent(myname, myemail, titleid, descfull, lockv, time.mktime(sdatekv), time.mktime(edatekv))
+#            print ev
+#            calendar.addEvent (ev)
+#        except xml.parsers.expat.ExpatError:
+#        #except:
+#            print "FAILED" #+ k,v
+#        #    continue
