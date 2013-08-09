5282ab651142198eadcb4c5b13d136c91931dcd4
jb <relic7@aol.com>
2013-08-09 12:07:58 -0400

    gcalScatcgh Auth

--- python/gcal_scrap/gcalScratcgh.py
+++ python/gcal_scrap/gcalScratcgh.py
@@ -1,94 +1,163 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Mar 12 11:23:55 2013
+This scripts reqire a third party module 'requests'.
+You can get it from PyPI, i.e. you can install it using
+easy_install or pip.
 
-@author: jb
+   http://docs.python-requests.org/en/v0.10.4/
+
+Original source code is written by shin1ogawa, which is in Java.
+
+   https://gist.github.com/1899391
+
+Materials for this session are avaliable in following URLs:
+
+  - Hands on material: http://goo.gl/oAhzI
+  - Google APIs Console: https://code.google.com/apis/console/
+  - Google APIs Explorer: http://code.google.com/apis/explorer/
+  - OAuth 2.0 Playground: https://code.google.com/oauthplayground/
 """
-def sqlQueryEventsUpcoming():
-    import sqlalchemy
-    orcl_engine = sqlalchemy.create_engine('oracle+cx_oracle://jbragato:Blu3f!y@192.168.30.66:1531/dssprd1')
-    connection = orcl_engine.connect()
-    querymake_eventscal = 'SELECT ATG_SNP.EVENT.ID AS "event_id", ATG_SNP.LK_EVENT_PRODUCT_CATEGORY.NAME AS "category", ATG_SNP.EVENT.EVENT_DESCRIPTION AS "event_title", ATG_SNP.EVENT.START_DATE AS "ev_start", ATG_SNP.EVENT.END_DATE AS "ev_end", ATG_SNP.EVENT_PRODUCT_COLOR.PRODUCT_COLOR_ID AS "colorstyle", POMGR_SNP.LK_PRODUCT_STATUS.NAME AS "production_status" FROM ATG_SNP.EVENT INNER JOIN ATG_SNP.LK_EVENT_PRODUCT_CATEGORY ON ATG_SNP.EVENT.PRODUCT_CATEGORY_ID = ATG_SNP.LK_EVENT_PRODUCT_CATEGORY.ID RIGHT JOIN ATG_SNP.EVENT_PRODUCT_COLOR ON ATG_SNP.EVENT.ID = ATG_SNP.EVENT_PRODUCT_COLOR.EVENT_ID INNER JOIN ATG_SNP.PRODUCT_COLOR ON ATG_SNP.EVENT_PRODUCT_COLOR.PRODUCT_COLOR_ID = ATG_SNP.PRODUCT_COLOR.ID INNER JOIN POMGR_SNP.LK_PRODUCT_STATUS ON ATG_SNP.PRODUCT_COLOR.PRODUCTION_STATUS_ID = POMGR_SNP.LK_PRODUCT_STATUS.ID WHERE ATG_SNP.EVENT.START_DATE >= TRUNC(SysDate) ORDER BY ATG_SNP.EVENT.ID DESC, ATG_SNP.EVENT.START_DATE DESC Nulls Last'
-    result = connection.execute(querymake_eventscal)
-    
-    events = {}
-    for row in result:
-        event = {}        
-        event['event_id'] = row['event_id']
-        event['category'] = row['category']                
-        event['event_title'] = row['event_title']
-        event['ev_start'] = row['ev_start']
-        event['ev_end'] = row['ev_end']        
-        event['colorstyle'] = row['colorstyle']        
-        event['production_status'] = row['production_status']  
-        events[row['colorstyle']] = event
-       
-    print events
-    connection.close()
-    return events
 
-# First retrieve the event from the API.
-'''
-event = service.events().get(calendarId='primary', eventId='eventId').execute()
+__author__ = "@ymotongpoo"
 
-event['summary'] = 'Appointment at Somewhere'
+import requests
+from urllib import urlencode
+import json
+from subprocess import Popen
 
-updated_event = service.events().update(calendarId='primary', eventId=event['id'], body=event).execute()
+client_id='222573514309.apps.googleusercontent.com'
+client_secret='r4BerSFPl7p6bHr2uYK4MHik'
+user_agent='gcal_tests/v01'
+developerKey='AIzaSyB101MP8UXS7I8jIgJ0IYEDhr3arua5mB0'
+redirect_uri = "urn:ietf:wg:oauth:2.0:oob"
+base_url = r"https://accounts.google.com/o/oauth2/"
+authorization_code = ""
+access_token = ""
 
-# Print the updated date.
-print updated_event['updated']
-'''
 
-future_events = sqlQueryEventsUpcoming()
+"""
+Retrieving authorization_code from authorization API.
+"""
+def retrieve_authorization_code():
+  authorization_code_req = {
+    "response_type": "code",
+    "client_id": client_id,
+    "redirect_uri": redirect_uri,
+    "scope": (r"https://www.googleapis.com/auth/userinfo.profile" +
+              r" https://www.googleapis.com/auth/userinfo.email" +
+              r" https://www.googleapis.com/auth/calendar")
+    }
 
-for key,value in future_events.iteritems():
-    import datetime, time    
-    for kv in iter(value):
-        titlekv = value['event_id']
-        desckv = value['event_title']
-        colorstyle = value['colorstyle']
-        status = value['production_status']
-        category = value['category']
-        lockv = str(category)
-        sdatekvraw = '{:%Y,%m,%d,%H,%M,%S,00,00,00}'.format(value['ev_start'])
-        edatekvraw = '{:%Y,%m,%d,%H,%M,%S,00,00,00}'.format(value['ev_end'])
-        sdatekvsplit = sdatekvraw.split(",")
-        edatekvsplit = edatekvraw.split(",")
-        sdatekv = map(int,sdatekvsplit)
-        edatekv = map(int,edatekvsplit)
-        titleid = '{0}_{1}'.format(titlekv,desckv)
-        descfull = '{0}_{1}'.format(colorstyle,status)
-        descfull = str(descfull)    
-        try:
-            from GoogleCalendar import *
-            gCalMNG = GoogleCalendarMng()
-            myname = "john bragato"
-            myemail = "john.bragato@gmail.com"
-            gCalMNG.connect (myemail, "yankee17")
-            calendar = gCalMNG.getCalendar ("Default1")
-            gcalevents = calendar.getEvents()
-            for event in gcalevents:
-                if event.getTitle() == titleid:
-                    event.setContent (event.getContent() + "Changes on location")
-                    event.setLocation (lockv)
-                    event.update()
-                    contents = event.getContent()
-                    for content in contents:
-                        if content.getContent() == descfull:
-                            content.setContent(descfull)
-                            content.update()
-                else:
-                    for event in gcalevents:
-                        print event.getTitle()
-                        print event.getContent()
-                        print time.strftime("%Y-%m-%dT%H:%M:%S" , time.localtime(event.getStartTime()))
-                        print time.strftime("%Y-%m-%dT%H:%M:%S" , time.localtime(event.getEndTime()))
-                    ev = newEvent(myname, myemail, titleid, descfull, lockv, time.mktime(sdatekv), time.mktime(edatekv))
-                    print ev
-                    calendar.addEvent (ev)
-                        
-            
-            
-        except xml.parsers.expat.ExpatError:
-            print "FAILED" + key,value
-            continue
+  r = requests.get(base_url + "auth?%s" % urlencode(authorization_code_req),
+                   allow_redirects=False)
+  print "Request Gotten"
+  url = r.headers.get('location')
+  Popen(["open", url])
+
+  authorization_code = raw_input("\nAuthorization Code >>> ")
+  return authorization_code
+
+
+"""
+Retrieving access_token and refresh_token from Token API.
+"""
+def retrieve_tokens(authorization_code):
+  access_token_req = {
+    "code" : authorization_code,
+    "client_id" : client_id,
+    "client_secret" : client_secret,
+    "redirect_uri" : redirect_uri,
+    "grant_type": "authorization_code",
+    }
+  content_length=len(urlencode(access_token_req))
+  access_token_req['content-length'] = str(content_length)
+
+  r = requests.post(base_url + "token", data=access_token_req)
+  data = json.loads(r.text)
+  return data
+
+
+
+"""
+Sample code of fetching user information from userinfo API.
+"""
+def get_userinfo():
+  global authorization_code
+  authorization_code = retrieve_authorization_code()
+  tokens = retrieve_tokens(authorization_code)
+  access_token = tokens['access_token']
+  authorization_header = {"Authorization": "OAuth %s" % access_token}
+  r = requests.get("https://www.googleapis.com/oauth2/v2/userinfo",
+                   headers=authorization_header)
+  print r.text
+
+
+
+def get_calendar_list():
+  global authorization_code
+  global access_token
+
+  authorization_code = retrieve_authorization_code()
+  tokens = retrieve_tokens(authorization_code)
+  access_token = tokens['access_token']
+  authorization_header = {"Authorization": "OAuth %s" % access_token}
+
+  r = requests.get("https://www.googleapis.com/calendar/v3/users/me/calendarList",
+                   headers=authorization_header)
+  return r.text
+
+
+def _get_start_end_time(event):
+  try:
+    if event['start'].has_key('date'):
+      start = event['start']['date']
+    elif event['start'].has_key('dateTime'):
+      start = event['start']['dateTime']
+    else:
+      start = 'N/A'
+
+    if event['end'].has_key('date'):
+      end = event['end']['date']
+    elif event['end'].has_key('dateTime'):
+      end = event['end']['dateTime']
+    else:
+      end = 'N/A'
+    return start, end
+
+  except:
+    return event['etag'], event['status']
+
+
+def get_events_list():
+  global authorization_code
+  global access_token
+
+  data = json.loads(get_calendar_list())
+  for calendar in data['items']:
+    calendar_id = calendar['id']
+    print calendar['summary']
+
+    if authorization_code == "" or access_token == "":
+      authorization_code = retrieve_authorization_code()
+      tokens = retrieve_tokens(authorization_code)
+      access_token = tokens['access_token']
+
+    authorization_header = {"Authorization": "OAuth %s" % access_token}
+    url = ("https://www.googleapis.com/calendar/v3/calendars/%s/events?key=%s" %
+           (quote_plus(calendar_id), quote_plus(api_key)))
+    r = requests.get(url, headers=authorization_header)
+
+    events = json.loads(r.text)
+    for event in events['items']:
+      print event.get('summary', '(Event title not set)')
+      if event['status'] != 'cancelled':
+        start, end = _get_start_end_time(event)
+        print "   start : ", start, "  end : ", end
+
+
+def main():
+  get_events_list()
+
+
+if __name__ == '__main__':
+  main()
