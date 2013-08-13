9c9063a406e79b9d61d38d5acbc22b2da6bd5677
johnb <john.bragato@bluefly.com>
2013-08-13 18:35:34 -0400

    added stad alone image thumb maker for RAW

--- python/walkdir_RAW-File6_exiv2.py
+++ python/walkdir_RAW-File6_exiv2.py
@@ -313,17 +313,17 @@ for k,v in fulldict.iteritems():
         ## Test File path String to Determine which Table needs to be Updated Then Insert SQL statement
         sqlinsert_choose_test = v['file_path']
         regex_productionraw = re.compile(r'^/.+?/ON_FIGURE/.+?RAW_FILES.*?[0-9]{9}_[1-9]_[0-9]{1,4}\.[CR2]{3}$')
-        regex_productionraw_jpg = re.compile(r'^/.+?Raw/\.zImages_1/.*?[0-9]{9}_[1-9]\.[jpgJPG]{3}$')
+        regex_productionraw_zimages = re.compile(r'^/.+?Raw/\.zImages_1/.*?[0-9]{9}_[1-9]\.[jpgJPG]{3}$')
         regex_photoselects = re.compile(r'^/.+?/Post_Ready/.+?Push/.*?[0-9]{9}_[1-6]\.[jpgJPG]{3}$')
         regex_postreadyoriginal = re.compile(r'^/Retouch_.+?/.*?[0-9]{9}_[1-6]\.[jpgJPG]{3}$')
         regex_zimages = re.compile(r'^/zImages.*?/[0-9]{4}/.*?[0-9]{9}_[1-6]\.[jpgJPG]{3}$')
 
 ## ProdRaw RAW
-        if re.findall(regex_photoselects, sqlinsert_choose_test):
-            connection.execute("""INSERT INTO production_raw_onfigure (colorstyle, photo_date, file_path, alt) VALUES (%s, %s, %s, %s)""", v['colorstyle'], v['photo_date'], v['file_path'],  v['alt'])
+        if re.findall(regex_productionraw, sqlinsert_choose_test):
+            connection.execute("""INSERT INTO production_raw_onfigure (colorstyle, photo_date, file_path, alt, shot_number) VALUES (%s, %s, %s, %s, %s)""", v['colorstyle'], v['photo_date'], v['file_path'],  v['alt'], v['shot'])
             print "Successful Insert production_raw_onfigure --> {0}".format(k)
 ## ProdRaw Thumbs
-        if re.findall(regex_photoselects, sqlinsert_choose_test):
+        if re.findall(regex_productionraw_zimages, sqlinsert_choose_test):
             connection.execute("""INSERT INTO production_raw_onfigure (colorstyle, photo_date, file_path, alt) VALUES (%s, %s, %s, %s)""", v['colorstyle'], v['photo_date'], v['file_path'],  v['alt'])
             print "Successful Insert production_raw_onfigure --> {0}".format(k)
 ## PostReady Daily Selects To get images for Retouching     
