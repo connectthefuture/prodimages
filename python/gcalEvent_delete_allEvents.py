f5c4bacbc84fe7491870db83652b7c0c4047f795
jb <relic7@aol.com>
2013-08-12 21:41:41 -0400

    delete all events fix

--- python/gcalEvent_delete_allEvents.py
+++ python/gcalEvent_delete_allEvents.py
@@ -16,13 +16,13 @@ events = default_cal.getEvents()
 # Delete all Calendar Events prior to Fresh insert
 print len(events)
 while len(events) >= 1:
-    for event in events:
-        event.delete()
-        events = default_cal.getEvents()
-        print len(events)
     try:
-        default_cal = gcal_login_jb().getCalendars()[0]
-        events = default_cal.getEvents()
+		for event in events:
+			event.delete()
+			events = default_cal.getEvents()
+			print len(events)
+			default_cal = gcal_login_jb().getCalendars()[0]
+			events = default_cal.getEvents()
     except AttributeError:
         print "Error"
         pass
