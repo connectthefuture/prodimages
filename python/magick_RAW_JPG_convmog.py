97e634f9fa3af49e5b21b6543d55279bb8606b0d
johnb <john.bragato@bluefly.com>
2013-08-20 12:34:00 -0400

    auto gamma

--- python/magick_RAW_JPG_convmog.py
+++ python/magick_RAW_JPG_convmog.py
@@ -21,25 +21,27 @@ def subproc_mogrify_RAWtoJPG4800h(srcdir):
     "-colorspace",
     "sRGB",
     '*.CR2',
-#        "-resample",
-#        outsize,
     "-define",
-    'jpg:size=x4800', 
+    'jpg:size=3744x5616',     
     "-define",
     'jpg:profile=/usr/local/color_profiles/sRGB.icm', 
     "-define",
     'jpg:colorspace=sRGB',
+    "-define",
+    'jpeg:fancy-upsampling=on',
     "-density",
     "350x350",
+    "-auto-gamma",
     "-level",
     "0\%,100\%,1.3",
     "-adaptive-sharpen",
-    "30",
+    "20",
     "-unsharp",
-    "70",
+    "80",
     "-quality",
     "100",
     ])
+
 
 import sys
 if sys.argv[1]:
