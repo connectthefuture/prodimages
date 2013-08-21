41e668f47193354dfff3d7427063e59e47810b83
jb <relic7@aol.com>
2013-08-21 01:10:00 -0400

    tidying

--- misc/TAG_fromDirName.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-. ~/.bash_profile
-DROP_RETOUCHERS=$PRODSRV/tmp/imagedrop/retouchers/
-
-find $DROP_RETOUCHERS -type f -iname \*.jpg | sed s/\ /'\\ '/g | xargs exiftool -'Directory' -csv | awk '{ split($1, a, "//"); print $0" "a[3] }' | awk '{ split($1, a, ","); print "exiftool -f -overwrite_original -P -fast2 -m -'IPTC:RetoucherName'="$NF, a[1] }' | /bin/bash
