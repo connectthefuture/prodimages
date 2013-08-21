58295c15d244304002c987ab0db2f424177d3cdb
johnb <john.bragato@bluefly.com>
2013-08-21 12:29:01 -0400

    connection strings changed to borac101-vip

--- python/gcal_ProductionNumbers.py
+++ python/gcal_ProductionNumbers.py
@@ -11,7 +11,7 @@ def sql_query_production_numbers():
     import sqlalchemy
     import datetime
     from collections import defaultdict
-    orcl_engine = sqlalchemy.create_engine('oracle+cx_oracle://prod_team_ro:9thfl00r@192.168.30.165:1521/bfyprd11')
+    orcl_engine = sqlalchemy.create_engine('oracle+cx_oracle://prod_team_ro:9thfl00r@borac101-vip.l3.bluefly.com:1521/bfyprd11')
     connection = orcl_engine.connect()
 
     ### Get Production Complete Totals and Build Dict of key value pairs
