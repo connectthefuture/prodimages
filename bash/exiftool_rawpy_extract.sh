a6afb78697ba32be0faf113daa71d7d028532267
johnb <john.bragato@bluefly.com>
2014-02-12 15:08:45 -0500

    cx

--- bash/exiftool_rawpy_extract.sh
+++ bash/exiftool_rawpy_extract.sh
@@ -3,5 +3,5 @@
 for i in {0..1}; 
 do 
 fpath=`date +"/mnt/Production_Raw/PHOTO_STUDIO_OUTPUT/ON_FIGURE/%m_%Y/%m%d%y_*" --date="$i days ago"`; 
-find "$fpath" -wholename \*RAW\* -type d -exec /usr/local/batchRunScripts/python/raw_jpg_extractor.py {} \; &
+find $fpath -wholename \*RAW\* -type d -exec /usr/local/batchRunScripts/python/raw_jpg_extractor.py {} \; &
 done;
