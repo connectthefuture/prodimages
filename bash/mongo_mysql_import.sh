19ebe90c5bcfc32eed372f0f279f75e2d6a8c717
jb <jbragato@yahoo.com>
2014-02-20 11:33:43 -0500

    mongo import from mysql updated

--- bash/mongo_mysql_import.sh
+++ bash/mongo_mysql_import.sh
@@ -5,13 +5,13 @@
 
 
 
-mysqldump -u root -pmysql -T/var/tmp data_imagepaths --fields-terminated-by=, --fields-enclosed-by=\"
+mysqldump -u root -pmysql -T/var/tmp www_django --fields-terminated-by=, --fields-enclosed-by=\"
 
 
+mongoimport -d www_django --headerline -c zimages -type csv -f fields-sep-by-coma --drop /var/tmp/zimages1_photoselects.txt
 
+mongoimport -d www_django --headerline -c pushselects -type csv -f fields-sep-by-coma --drop /var/tmp/push_photoselects.txt
 
-mongoimport -d data_imagepaths --headerline -c zimages -type csv -f fields-sep-by-coma --drop /var/tmp/zimages1_photoselects.txt
-
-mongoimport -d data_imagepaths --headerline -c pushselects -type csv -f fields-sep-by-coma --drop /var/tmp/push_photoselects.txt
-
-mongoimport -d data_imagepaths --headerline -c archselects -type csv -f fields-sep-by-coma --drop /var/tmp/post_ready_original.txt
+mongoimport -d www_django --headerline -c archselects -type csv -f field]s-sep-by-coma --drop /var/tmp/post_ready_original.txt
+mongoimport -d www_django --headerline -c outtakes -type csv -f field]s-sep-by-coma --drop /var/tmp/production_raw_zimages.txt
+mongoimport -d www_django --headerline -c pmdata -type csv -f field]s-sep-by-coma --drop /var/tmp/product_snapshot_live.txt
