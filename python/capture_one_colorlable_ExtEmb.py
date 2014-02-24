424d800d77916089ced67ccc4ce1ac1a619bb7e6
jb <relic7@aol.com>
2014-02-24 01:41:28 -0500

    c

--- python/capture_one_colorlable_ExtEmb.py
+++ python/capture_one_colorlable_ExtEmb.py
@@ -9,34 +9,37 @@ def get_exif_all_data(image_filepath):
         #['XMP:DateCreated'][:10].replace(':','-')
     return metadata
     
-    
+
+def capture_one_colorconv(colortagindex):
+    capture_one_colortag = ''
+    if colortagindex       == 0:
+        capture_one_colortag = 'None'
+    elif colortagindex     == 1:
+        capture_one_colortag = 'Issues-RED'
+    elif colortagindex     == 2:    
+        capture_one_colortag = 'Special-Blue'
+    elif colortagindex     == 3:    
+        capture_one_colortag = 'PreSelect-YELLOW'
+    elif colortagindex     == 4:
+        capture_one_colortag = 'Select-GREEN'
+    return capture_one_colortag
+
+
 ######################   
 import os,sys,re
-from collections import defaultdict
+# from collections import defaultdict
 filepath = os.path.abspath(sys.argv[1])
+
 directory = os.path.dirname(filepath)
 filename  = os.path.basename(filepath)
 
 capture1settings="{0}/CaptureOne/Settings50/{1}.cos".format(directory,filename)
-
-
-capture_one_colortag = ''
-if ['XML:Color_tag_index'] == 0:
-    capture_one_colortag = 'None'
-elif ['XML:Color_tag_index'] == 1:
-    capture_one_colortag = 'Issues-RED'
-elif ['XML:Color_tag_index'] == 2:    
-    capture_one_colortag = 'Special-Blue'
-elif ['XML:Color_tag_index'] == 3:    
-    capture_one_colortag = 'PreSelect-YELLOW'
-elif ['XML:Color_tag_index'] == 4:
-    capture_one_colortag = 'Select-GREEN'
-    
-    
     
     
 mdata         = get_exif_all_data(filepath)
 capture1data  = get_exif_all_data(capture1settings)
 
 
-mdata         = sorted(mdata.items())capture1data  = sorted(capture1data.items())
+mdata                   = sorted(mdata.items())capture1data            = sorted(capture1data.items())capture_one_colortag    = capture_one_colorconv(capture1data.get('XML:Color_tag_index'))
+capture_one_rating      = capture1data.get('XML:Rating')
+
