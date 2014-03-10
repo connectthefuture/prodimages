69a6d72742d3fc58376c090e97c1f4418e7fd319
johnb <john.bragato@bluefly.com>
2014-03-10 18:19:52 -0400

    magickThumbs

--- bash/magickThumbs_600x.sh
+++ bash/magickThumbs_600x.sh
@@ -7,45 +7,44 @@ Today=`date +%Y-%m-%d`
 #find $1 -type f -iname \*.* | grep ' ' | xargs -I '{}' mv -f '{}' $2
 
 
-if [ "$#" -lt 1 ]; then
-    
-    echo "1"
-    rootdir=/mnt/Production_Raw/.zImages_1
-    dirs=`find "$rootdir" -type d -wholename \*/[3-9][0-9][0-9][0-9]/\* -maxdepth 1 -atime -3`
-    #identify -precision 5 -define identify:locate=maximum -define identify:limit=3
-
-elif [ "$#" -gt 1 ]; then
-    
-    echo "2"
-    dirs="$@"  
-    ##=`find "$@" -type d -maxdepth 1 -wholename \*/[3-9][0-9][0-9][0-9]/\*`
-
-elif [ "$#" -eq 1 ]; then
-    ## If the sysargv1 listing has more jpgs than directories, dont decend dirs and do the root dir only
-    echo "3"
-    #dirs=`find "'"$1"'" -type d -wholename \*/[3-9][0-9][0-9][0-9]/\* -maxdepth 1 -atime -3`
-    
-    #if [ `echo "$dirs" | wc -l` -lt `find "$dirs" -name \*\.jp[g$] -maxdepth 1 -atime -3 | wc -l` ];
-    #then
-    dirs="$1"
-    #fi;
-
-fi;
+#if [ "$#" -eq 1 ]; then
+#    ## If the sysargv1 listing has more jpgs than directories, dont decend dirs and do the root dir only
+#    echo "1Arg"
+#    rootdir="$1"
+#    dirs=`find "$rootdir" -type d -wholename \*/[3-9][0-9][0-9][0-9]/\* -mindepth 1 -maxdepth 1`
+#elif [ "$#" -lt 1 ]; then
+#    echo "Zero"
+#    rootdir=/mnt/Production_Raw/.zImages_1
+#    dirs=`find "$rootdir" -type d -wholename \*/[3-9][0-9][0-9][0-9]/\* -mindepth 1 -maxdepth 2 -atime -7`
+#    #identify -precision 5 -define identify:locate=maximum -define identify:limit=3
+#elif [ "$#" -gt 1 ]; then
+#    echo "2Plus"
+#    dirs="$@"  
+#    ##=`find "$@" -type d -maxdepth 1 -wholename \*/[3-9][0-9][0-9][0-9]/\*`
+#fi;
 
 
-for d in "$dirs"; do
-    #echo "33Find"
 
-for f in `find "$d" -type f -wholename \*\/[^.]\*\.jp[g$] -maxdepth 1`; do 
+#for d in "$dirs"; do
+#    #echo "33Find"
+#    echo $d
+for f in `find "$1" -type f -wholename \*\/[^.]\*\.jp[g$] -mindepth 1 -maxdepth 1`; do 
 ## First Test if a thumb has already been made and skip if exists since it is a long process and waste to redo
 #if [[ `test -f "$f"_thumb` -gt 0 ]]; then
-    #echo $f
-    convert "$f" -auto-orient \
-        -background "rgb(255,255,255)" \
-        -filter Mitchell \
-        -resize 600x \
-        -unsharp 10% \
-        -quality 65% "$f"_thumb.jpg; 
+    if [ ! -f "$f"_thumb ]; then
+        echo "$f"_thumb     
+        convert "$f" -auto-orient \
+            -background "rgb(255,255,255)" \
+            -filter Mitchell \
+            -resize 600x \
+            -unsharp 10% \
+            -quality 65% "$f"_thumb.jpg; 
+    elif [ -f "$f"_thumb ]; then
+        echo ThumbExists_"$f"
+    fi;
+
+done;
+#done;
     
     #echo "MAKINGTHUMb77"
 # -filter sinc \
@@ -58,5 +57,3 @@ for f in `find "$d" -type f -wholename \*\/[^.]\*\.jp[g$] -maxdepth 1`; do
 #    echo "$f"_thumb"AlreadyExists"
 #fi;
 
-done; 
-done;
