74a3703913ca99ed2d8dc8ac7335c275dc1e1805
johnb <john.bragato@bluefly.com>
2014-03-28 18:07:47 -0400

    xY

--- python/india_ftp_funx.py
+++ python/india_ftp_funx.py
@@ -3,11 +3,11 @@ import os, sys, re, csv
 
 
 
-def upload_to_indiaDrop(filepath):
+def upload_to_indiaDrop(localfilepath):
     import ftplib
     session = ftplib.FTP('prepressoutsourcing.com', 'bf', 'B1002#@F')
-    fileread = open(filepath, 'rb')
-    filename = str(filepath.split('/')[-1])
+    fileread = open(localfilepath, 'rb')
+    filename = str(localfilepath.split('/')[-1])
     session.cwd("Drop/")
     session.storbinary('STOR ' + filename, fileread, 8*1024)
     fileread.close()
@@ -15,23 +15,23 @@ def upload_to_indiaDrop(filepath):
     
     
 
-def pycurl_upload_indiadrop(localFilePath):
+def pycurl_upload_indiadrop(localfilepath):
     import pycurl, os
     #import FileReader
-    localFileName = localFilePath.split('/')[-1]
+    localfilename = localfilepath.split('/')[-1]
 
     mediaType = "8"
     
     ftpURL = r'ftp://prepressoutsourcing.com//Pick/'
     ftpUSERPWD = "bf:B1002#@F"
 
-    ftpFilePath = os.path.join(ftpURL, localFileName)
-    if localFilePath != "" and ftpFilePath != "":
+    ftpfilepath = os.path.join(ftpURL, localfilename)
+    if localfilepath != "" and ftpfilepath != "":
         ## Create send data
 
         ### Send the request to Edgecast
         c = pycurl.Curl()
-        c.setopt(pycurl.URL, ftpFilePath)
+        c.setopt(pycurl.URL, ftpfilepath)
 #        c.setopt(pycurl.PORT , 21)
         c.setopt(pycurl.USERPWD, ftpUSERPWD)
         #c.setopt(pycurl.VERBOSE, 1)
@@ -40,10 +40,10 @@ def pycurl_upload_indiadrop(localFilePath):
         c.setopt(c.FAILONERROR, True)
 #        c.setopt(pycurl.FORBID_REUSE, 1)
 #        c.setopt(pycurl.FRESH_CONNECT, 1)
-        f = open(localFilePath, 'rb')
+        f = open(localfilepath, 'rb')
         c.setopt(pycurl.INFILE, f)
-        c.setopt(pycurl.INFILESIZE, os.path.getsize(localFilePath))
-        c.setopt(pycurl.INFILESIZE_LARGE, os.path.getsize(localFilePath))
+        c.setopt(pycurl.INFILESIZE, os.path.getsize(localfilepath))
+        c.setopt(pycurl.INFILESIZE_LARGE, os.path.getsize(localfilepath))
 #        c.setopt(pycurl.READFUNCTION, f.read());        
 #        c.setopt(pycurl.READDATA, f.read()); 
         c.setopt(pycurl.UPLOAD, 1L)
@@ -51,7 +51,7 @@ def pycurl_upload_indiadrop(localFilePath):
         try:
             c.perform()
             c.close()
-            print "Successfully Uploaded --> {0}".format(localFileName)
+            print "Successfully Uploaded --> {0}".format(localfilename)
             ## return 200
         except pycurl.error, error:
             errno, errstr = error
@@ -71,28 +71,28 @@ def pycurl_download_indiaComplete(zipfilename, saveDir=None):
     if not saveDir:
         saveDir = os.path.abspath('/mnt/Post_Complete/Complete_Archive/SendReceive_BGRemoval/2_Returned')
     
-    localFileName = "{0}".format(zipfilename)
-    localFilePath = os.path.join(saveDir, localFileName)
+    localfilename = "{0}".format(zipfilename)
+    localfilepath = os.path.join(saveDir, localfilename)
     
     mediaType = "8"
     ftpURL = r'ftp://prepressoutsourcing.com//Pick/'
     
-    ftpFilePath = os.path.join(ftpURL, colorstyle[:4], localFileName)
+    ftpfilepath = os.path.join(ftpURL, localfilename)
     ftpUSERPWD = "bf:B1002#@F"
 
-    if localFilePath != "" and ftpFilePath != "":
+    if localfilepath != "" and ftpfilepath != "":
         ## Create send data
 
         ### Send the request to Edgecast
         c = pycurl.Curl()
-        c.f = open(localFilePath, 'wb')
-        c.setopt(pycurl.URL, ftpFilePath)
+        c.f = open(localfilepath, 'wb')
+        c.setopt(pycurl.URL, ftpfilepath)
         c.setopt(pycurl.USERPWD, ftpUSERPWD)
         c.setopt(pycurl.WRITEDATA, c.f)
         #m.add_handle(c)
         # store some info
-        c.filename = localFilePath
-        c.url = ftpFilePath
+        c.filename = localfilepath
+        c.url = ftpfilepath
 
 #        c.setopt(pycurl.PORT , 21)
 
@@ -104,8 +104,8 @@ def pycurl_download_indiaComplete(zipfilename, saveDir=None):
 #        c.setopt(pycurl.FRESH_CONNECT, 1)
         
         #c.setopt(pycurl.INFILE, f)
-        #c.setopt(pycurl.INFILESIZE, os.path.getsize(localFilePath))
-        #c.setopt(pycurl.INFILESIZE_LARGE, os.path.getsize(localFilePath))
+        #c.setopt(pycurl.INFILESIZE, os.path.getsize(localfilepath))
+        #c.setopt(pycurl.INFILESIZE_LARGE, os.path.getsize(localfilepath))
 #        c.setopt(pycurl.READFUNCTION, f.read());        
 #        c.setopt(pycurl.READDATA, f.read()); 
         #c.setopt(pycurl.UPLOAD, 1L)
@@ -113,7 +113,7 @@ def pycurl_download_indiaComplete(zipfilename, saveDir=None):
         try:
             c.perform()
             c.close()
-            print "Successfully Uploaded --> {0}".format(localFileName)
+            print "Successfully Uploaded --> {0}".format(localfilename)
             ## return 200
         except pycurl.error, error:
             errno, errstr = error
