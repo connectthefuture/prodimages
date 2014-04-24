3367cb1742558ce252d520f6edb33695c0e47a33
johnb <john.bragato@bluefly.com>
2014-04-24 19:16:22 -0400

    chmod

--- python/flaskr/flaskr.py
+++ python/flaskr/flaskr.py
@@ -1,4 +1,8 @@
-import os import sqlite3 
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os 
+import sqlite3 
 from flask import Flask, request, session, g, redirect, url_for, abort, \
      render_template, flash
 
