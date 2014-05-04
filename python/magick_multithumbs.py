a395ac92dcab429834a0b3cddd46f8cd89016e03
jb <relic7@aol.com>
2014-05-04 00:50:59 -0400

    xY

--- python/magick_multithumbs.py
+++ python/magick_multithumbs.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python
 
-def subproc_multithumbs_8_4_2(filepath,destdir):
+def subproc_multithumbs_8_4_2(filepath,destdir,eight=None):
     import subprocess, os
     
     fname = filepath.split("/")[-1].split('.')[0].strip('_LP').replace('_1.','.').lower()
     ext = filepath.split(".")[-1]
     
-    outfile_z = os.path.join(destdir, fname + "_z.jpg")    
+    if eight:
+        outfile_z = os.path.join(destdir, fname + "_z.jpg")    
     outfile_l = os.path.join(destdir, fname + "_l.jpg")    
     outfile_m = os.path.join(destdir, fname + "_m.jpg")
-    #try:            
+
     subprocess.call([
         'convert', 
         filepath, 
@@ -17,42 +18,22 @@ def subproc_multithumbs_8_4_2(filepath,destdir):
         ext,
         #'-quality',
         #'85',
-#        '-colorspace',
-#        'rgb',
-#        '+profile',
-#        '-filter',
-#        'Lanczos',
+        #'-colorspace',
+        #'rgb',
+        #'+profile "*"',
+        #'-filter',
+        #'Lanczos',
         '-write',
         'mpr:copy-of-original',
         '+delete',
-        ##'mpr:copy-of-huge-original-crop"3000x2000+0+480"-resize"200x125!>"-writethumb1-extract.jpg+delete',
-        #'mpr:copy-of-huge-original-crop"2000x1500+280+220"-resize"75x75!>"-writethumb2-extract.jpg+delete',
-        #
-        ## Zoom Largest Jpeg
+
+            ## Begin generating imgs 
+            # --> Large Jpeg
             'mpr:copy-of-original',
             '-format', 
             'jpg',
-#            '-colorspace',
-#            'sRGB',
-            '-channel', 
-            'RGBA',
-            '-resize',
-            '800x960',
-            #'compress', 
-            #'none', 
-            '-unsharp',
-            '2x0.5+0.5+0', 
-            '-quality', 
-            '100',
-            '-write',
-            outfile_z,
-            '+delete',
-        ## Large Jpeg
-            'mpr:copy-of-original',
-            '-format', 
-            'jpg',
-#            '-colorspace',
-#            'sRGB',
+            #'-colorspace',
+            #'sRGB',
             '-channel', 
             'RGBA',
             '-resize',
@@ -66,12 +47,13 @@ def subproc_multithumbs_8_4_2(filepath,destdir):
             '-write',
             outfile_l,
             '+delete',
-        ## Medium Jpeg
+            
+            ## Medium Jpeg
             'mpr:copy-of-original',
             '-format', 
             'jpg',
-#            '-colorspace',
-#            'sRGB',
+             #'-colorspace',
+             #'sRGB',
             '-channel', 
             'RGBA',
             '-resize',
@@ -85,14 +67,42 @@ def subproc_multithumbs_8_4_2(filepath,destdir):
             '-write',
             outfile_m,
             '+delete',
+            ])
+            
+            ##'mpr:copy-of-huge-original-crop"3000x2000+0+480"-resize"200x125!>"-writethumb1-extract.jpg+delete',
+            #'mpr:copy-of-huge-original-crop"2000x1500+280+220"-resize"75x75!>"-writethumb2-extract.jpg+delete',
+            #
+                
+                ## Eight --> Zoom Largest Jpeg
+    #            'mpr:copy-of-original',
+    #            '-format', 
+    #            'jpg',
+    #             #'-colorspace',
+    #             #'sRGB',
+    #            '-channel', 
+    #            'RGBA',
+    #            '-resize',
+    #            '800x960',
+    #            #'compress', 
+    #            #'none', 
+    #            '-unsharp',
+    #            '2x0.5+0.5+0', 
+    #            '-quality', 
+    #            '100',
+    #            '-write',
+    #            outfile_z,
+    #            '+delete',
+               
         #str('mpr:copy-of-huge-original-resize"1000x1200"-writesample-1000x1200_z.jpg+delete'),
         #str('mpr:copy-of-huge-original-resize"800x960"-writesample-800x960_x.jpg+delete'),
         #str('mpr:copy-of-huge-original-resize"400x480"-writesample-400x480_l.jpg+delete'),
         #str('mpr:copy-of-huge-original '-resize', '"200x240"','-writesample','-200x240_m.jpg+delete'),
         ##'mpr:copy-of-huge-original-resize"163x163!>"-writesample-163x163.jpg'
-        ])
+        #])
         
-import sys,os
+import sys,os, datetime
+todaysdirdate = datetime.datetime.strftime(datetime.datetime.now(),'%Y-%m-%d_%f')
+
 originaldir = os.path.abspath(sys.argv[1])
 os.chdir(originaldir)
 listeddir = os.listdir(originaldir)
@@ -101,7 +111,7 @@ destdir = ''
 try:
     destdir = os.path.abspath(sys.argv[2])
 except IndexError:
-    destdir = os.path.join(originaldir, 'converteddir')
+    destdir = os.path.join(originaldir, 'converteddir_' + todaysdirdate)
 
 if not os.path.isdir(destdir):
     try:
@@ -109,6 +119,7 @@ if not os.path.isdir(destdir):
     except:
         print "Failed {}".format(destdir)
         pass
+
 print destdir        
 for filepath in listeddir:
     subproc_multithumbs_8_4_2(os.path.abspath(filepath),destdir)
