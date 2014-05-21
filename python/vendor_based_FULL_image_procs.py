129b1f3faf11dc5015a22af41de3132520cb1246
relic7 <relic7@users.noreply.github.com>
2014-05-21 01:39:05 -0400

    Create vendor_based_FULL_image_procs.py

---
+++ python/vendor_based_FULL_image_procs.py
@@ -0,0 +1,442 @@
+#!/usr/bin/env python
+import os, sys, re, csv
+
+
+def rename_retouched_file(src_imgfilepath):
+    import os,re
+    regex_coded = re.compile(r'.+?/[1-9][0-9]{8}_[1-6]\.jpg')
+    imgfilepath = src_imgfilepath
+    if re.findall(regex_coded,imgfilepath):
+        filedir = imgfilepath.split('/')[:-1]
+        filedir = '/'.join(filedir)
+        print filedir
+        filename = imgfilepath.split('/')[-1]
+        colorstyle = str(filename[:9])
+        testimg = filename.split('_')[-1]
+        alttest = testimg.split('.')[0]
+        ext = filename.split('.')[-1]
+        ext = ".{}".format(ext.lower())
+        # if its 1
+        if str.isdigit(alttest) & len(alttest) == 1:
+            if alttest == '1':
+                src_img_primary = src_imgfilepath.replace('_1.','.')
+                os.rename(src_imgfilepath, src_img_primary)
+                return src_img_primary
+            else:
+                alttest = int(alttest)
+                print alttest
+                alttest = alttest - 1
+                alt = '_alt0{}'.format(str(alttest))
+                print alt
+
+                if alt:
+                    filename = "{}{}{}".format(colorstyle,alt,ext)
+                    renamed = os.path.join(filedir, filename)
+                    print renamed
+        ##        except UnboundLocalError:
+        ##            print "UnboundLocalError{}".format(imgfilepath)
+                if renamed:
+                    os.rename(src_imgfilepath, renamed)
+                    if os.path.isfile(renamed):
+                        return renamed
+        else:
+            return src_imgfilepath
+    
+
+######## Make Images For Upload to Website ##########
+
+### Large Jpeg Mogrfy Dir with _l jpgs
+def subproc_magick_large_jpg(imgdir):
+    import subprocess,os,re
+
+    ### Change to Large jpg dir to Mogrify using Glob
+    os.chdir(imgdir)
+    
+    subprocess.call([
+    "mogrify",
+    '*.jpg[400x480]',
+    "-filter",
+    "Mitchell",
+    "-compress",
+    "none",
+    "-format",
+    "jpeg",
+    "-adaptive-sharpen",
+    "100",
+    "-unsharp",
+    "50",
+    "-quality",
+    "100",
+    ])
+
+### Medium Jpeg Mogrfy Dir with _m jpgs
+def subproc_magick_medium_jpg(imgdir):
+    import subprocess,os,re
+
+    ### Change to Medium jpg dir to Mogrify using Glob
+    os.chdir(imgdir)
+    
+    subprocess.call([
+    "mogrify",
+    '*.jpg[200x240]',
+    "-filter",
+    "Mitchell",
+    "-compress",
+    "none",
+    "-format",
+    "jpeg",
+    "-adaptive-sharpen",
+    "100",
+    "-unsharp",
+    "50",
+    "-quality",
+    "100",
+    ])
+
+
+### Png Create with Mogrify globbing png directories
+def subproc_magick_png(imgdir):
+    import subprocess,re,os
+    #imgdestpng_out = os.path.join(tmp_processing, os.path.basename(imgsrc_jpg))
+    os.chdir(imgdir)
+    
+    subprocess.call([
+    "mogrify",
+    "-format",
+    "png",
+    '*.jpg',
+    "-define",
+    "png:preserve-colormap",
+    "-define",
+    "png:format=png24",
+    "-define",
+    "png:compression-level=N",
+    "-define",
+    "png:compression-strategy=N",
+    "-define",
+    "png:compression-filter=N",
+    "-format",
+    "png",
+    "-quality",
+    "100",
+    "-adaptive-sharpen",
+    "50",
+    "-unsharp",
+    "75",
+    ])
+    
+    print "Done {}".format(imgdir)
+    return
+
+
+#### Upload to usr:imagedrop pwd:imagedrop0
+def pycurl_upload_imagedrop(localFilePath):
+    import pycurl, os
+    localFileName = localFilePath.split('/')[-1]
+
+    mediaType = "8"
+    ftpURL = "ftp://file3.bluefly.corp/ImageDrop/"
+    ftpFilePath = os.path.join(ftpURL, localFileName)
+    ftpUSERPWD = "imagedrop:imagedrop0"
+
+    if localFilePath != "" and ftpFilePath != "":
+        c = pycurl.Curl()
+        c.setopt(pycurl.URL, ftpFilePath)
+        c.setopt(pycurl.USERPWD, ftpUSERPWD)
+        #c.setopt(pycurl.VERBOSE, 1)
+        c.setopt(c.CONNECTTIMEOUT, 5)
+        c.setopt(c.TIMEOUT, 8)
+        c.setopt(c.FAILONERROR, True)
+        f = open(localFilePath, 'rb')
+        c.setopt(pycurl.INFILE, f)
+        c.setopt(pycurl.INFILESIZE, os.path.getsize(localFilePath))
+        c.setopt(pycurl.INFILESIZE_LARGE, os.path.getsize(localFilePath))
+        c.setopt(pycurl.UPLOAD, 1L)
+
+        try:
+            c.perform()
+            c.close()
+            print "Successfully Uploaded --> {0}".format(localFileName)
+            ## return 200
+        except pycurl.error, error:
+            errno, errstr = error
+            print 'An error occurred: ', errstr
+            try:
+                c.close()
+            except:
+                print "Couldnt Close Cnx"
+                pass
+            return errno
+            
+            
+
+
+########### RUN #################
+import os, sys, re, shutil, datetime, glob
+
+### Can pass as sys.argv a direcectory with nested directories containing jpgs. Must have nested dirs
+try:
+    testdir = sys.argv[1]
+    if os.path.isdir(testdir):
+       rootdir = testdir
+    #else:
+    #    rootdir = '/mnt/Post_Complete/Complete_to_Load/Drop_FinalFilesOnly'
+except IndexError:
+    rootdir = '/mnt/Post_Complete/.Vendor_to_Load/upload_drop/VAULT_test'
+
+    
+
+vendorname = rootdir.split('/')[-1].split('_')[0]
+### Regex Pattern Defs
+regex_CR2 = re.compile(r'.+?\.[CR2cr2]{3}')
+regex_jpg = re.compile(r'.+?\.[JPGjpg]{3}')
+regex_png = re.compile(r'.+?\.[pngPNG]{3}')
+regex_coded = re.compile(r'.+?/[1-9][0-9]{8}_[1-6]\.jpg')
+regex_primary_jpg = re.compile(r'.+?/[1-9][0-9]{8}\.jpg')
+regex_alt_jpg = re.compile(r'.+?/[1-9][0-9]{8}_alt0[1-6]\.jpg')
+
+### Date Defs
+todaysdate = '{:%Y,%m,%d}'.format(datetime.datetime.now())
+todaysdatefull = '{:%Y,%m,%d,%H,%M}'.format(datetime.datetime.now())
+todaysdatearch = '{:%Y,%m,%d,%H,%M}'.format(datetime.datetime.now())
+
+### Define tmp and archive paths prior to Creating
+tmp_processing = os.path.join("/mnt/Post_Complete/.Vendor_to_Load/.tmp_processing" , "tmp_" + str(todaysdatefull).replace(",", ""))
+tmp_processing_l = os.path.join(tmp_processing, "largejpg")
+tmp_processing_m = os.path.join(tmp_processing, "mediumjpg")
+tmp_loading = os.path.join("/mnt/Post_Complete/.Vendor_to_Load/.tmp_loading" , "tmp_" + str(todaysdatefull).replace(",", ""))
+
+## Define for Creating Archive dirs
+archive = '/mnt/Post_Complete/.Vendor_to_Load/upload_complete/original_by_vendor'
+archive_uploaded = os.path.join(archive, "uploaded_" + str(todaysdate).replace(",", ""), vendorname.lower() + str(todaysdatearch).replace(",", ""))
+
+imgdest_jpg_final = os.path.join(archive_uploaded, 'JPG_RETOUCHED_ORIG')
+imgdest_png_final = os.path.join(archive_uploaded, 'PNG')
+
+
+###################
+## Create Lock File
+###################
+#locker = os.path.join(rootdir, 'LOCKED.lock')
+#if os.path.isfile(locker):
+#    break
+#else:
+#    with open(locker, 'wb') as f:
+#        f.write(todaysdatefull)
+#        f.close()
+############
+## Test for existing files to load or kill entire process prior to dir creation
+###########
+walkedout_tmp = glob.glob(os.path.join(rootdir, '*.*g'))
+if len(walkedout_tmp) == 0:
+    print "Nothing to Process"
+else:
+### Make Tmp Folders for Processing And Uploading -- tmp_dirs are dated with time(hr:min)to prevent collisions
+    try:
+        os.makedirs(archive_uploaded, 16877)
+    except:
+        pass
+
+    try:
+        os.makedirs(tmp_processing, 16877)
+    except:
+        pass
+
+    try:
+        os.makedirs(tmp_processing_l, 16877)
+    except:
+        pass
+
+    try:
+        os.makedirs(tmp_processing_m, 16877)
+    except:
+        pass
+
+    try:
+        os.makedirs(tmp_loading, 16877)
+    except:
+        pass
+
+    try:
+        os.makedirs(imgdest_png_final, 16877)
+    except:
+        pass
+
+    try:
+        os.makedirs(imgdest_jpg_final, 16877)
+    except:
+        pass
+
+####################################################
+## Begin Processing and compiling images for Loading
+####################################################
+
+
+## Move All DropFinal Files from Retouchers dirs to tmp_processing from drop folders Then Mogrify to create pngs copy to load and arch dirs
+walkedout_tmp = glob.glob(os.path.join(rootdir, '*.*g'))
+[ shutil.move(file, os.path.join(tmp_processing, os.path.basename(file))) for file in walkedout_tmp ]
+
+### Rename Files moved into Temp Processing Floder
+walkedout_tmp = glob.glob(os.path.join(tmp_processing, '*.jpg'))
+[ rename_retouched_file(file) for file in walkedout_tmp ]
+
+
+## Copy Full Size Retouched Jpg to tmp Large and Med jpg folders for Glob Mogrify AND to Final Archive JPG_RETOUCHED_ORIG
+walkedout_renamed = glob.glob(os.path.join(tmp_processing, '*.jpg'))
+
+
+
+
+
+####### FRAGRANCENET ALT PROCESS DETOUR ####
+def query_vendors_styles(vendorname):
+    import sqlalchemy
+    orcl_engine = sqlalchemy.create_engine('oracle+cx_oracle://prod_team_ro:9thfl00r@borac101-vip.l3.bluefly.com:1521/bfyprd11')    
+    connection = orcl_engine.connect()
+    ## POMGR.INVENTORY.TOTAL_WH_RCVD_QTY,
+    ## POMGR.INVENTORY.SOLD,
+    ## POMGR.INVENTORY.AVAL_ON_HAND,
+    # querymake_vendor_po_DSSPRD1 = "SELECT POMGR_SNP.PRODUCT_COLOR.ID AS colorstyle, POMGR_SNP.PRODUCT_COLOR.VENDOR_STYLE AS vendor_style, POMGR_SNP.PO_LINE.PO_HDR_ID AS po_hdr_id FROM POMGR_SNP.PRODUCT_COLOR INNER JOIN POMGR_SNP.PO_LINE ON POMGR_SNP.PRODUCT_COLOR.ID = POMGR_SNP.PO_LINE.PRODUCT_COLOR_ID WHERE POMGR_SNP.PRODUCT_COLOR.VENDOR_STYLE LIKE '" + vnum + "%' AND POMGR_SNP.PO_LINE.PO_HDR_ID = '" + ponum + "'"
+    # querymake_vendors_styles = "SELECT DISTINCT POMGR.PO_LINE.PRODUCT_COLOR_ID as colorstyle, POMGR.PO_LINE.PO_HDR_ID as po_hdr_id, POMGR.VENDOR.NAME AS vendor_name, POMGR.PRODUCT_COLOR.COPY_READY_DT as copy_ready_dt, POMGR.PRODUCT_COLOR.IMAGE_READY_DT as image_ready_dt, POMGR.PRODUCT_COLOR.PRODUCTION_COMPLETE_DT, POMGR.PRODUCT_COLOR.ACTIVE, POMGR.VENDOR.THIRD_SUPPLIER_ID, POMGR.LK_PO_TYPE.NAME AS po_type, POMGR.PRODUCT_COLOR.CREATED_DATE as create_dt, POMGR.PRODUCT_COLOR.MODIFIED_DATE as modify_dt, POMGR.PRODUCT_DETAIL.MATERIAL as material, POMGR.PRODUCT_DETAIL.LONG_DESCRIPTION as description, POMGR.PRODUCT_COLOR.VERSION as version FROM POMGR.PRODUCT_COLOR RIGHT JOIN POMGR.PO_LINE ON POMGR.PO_LINE.PRODUCT_COLOR_ID = POMGR.PRODUCT_COLOR.ID RIGHT JOIN POMGR.PO_HDR ON POMGR.PO_HDR.ID = POMGR.PO_LINE.PO_HDR_ID RIGHT JOIN POMGR.VENDOR ON POMGR.VENDOR.ID = POMGR.PO_HDR.VENDOR_ID INNER JOIN POMGR.LK_PO_TYPE ON POMGR.LK_PO_TYPE.ID = POMGR.PO_HDR.PO_TYPE_ID LEFT JOIN POMGR.INVENTORY ON POMGR.INVENTORY.PRODUCT_COLOR_ID = POMGR.PRODUCT_COLOR.ID LEFT JOIN POMGR.PRODUCT_DETAIL ON POMGR.PRODUCT_COLOR.PRODUCT_ID = POMGR.PRODUCT_DETAIL.PRODUCT_ID WHERE POMGR.VENDOR.NAME LIKE '%{0}%' ORDER BY POMGR.PO_LINE.PRODUCT_COLOR_ID DESC Nulls Last, POMGR.PRODUCT_COLOR.IMAGE_READY_DT DESC Nulls Last".format(vendorname)
+    querymake_vendors_product_details = "SELECT DISTINCT POMGR.PO_LINE.PRODUCT_COLOR_ID as colorstyle, POMGR.PO_LINE.PO_HDR_ID as po_hdr_id, POMGR.VENDOR.NAME AS vendor_name, POMGR.PRODUCT_COLOR.COPY_READY_DT as copy_ready_dt, POMGR.PRODUCT_COLOR.IMAGE_READY_DT as image_ready_dt, POMGR.PRODUCT_COLOR.PRODUCTION_COMPLETE_DT, POMGR.PRODUCT_COLOR.ACTIVE, POMGR.VENDOR.THIRD_SUPPLIER_ID, POMGR.LK_PO_TYPE.NAME AS po_type, POMGR.PRODUCT_COLOR.CREATED_DATE as create_dt, POMGR.PRODUCT_COLOR.MODIFIED_DATE as modify_dt, POMGR.PRODUCT_DETAIL.MATERIAL as material, POMGR.PRODUCT_COLOR_DETAIL.SHORT_NAME as short_name, POMGR.PRODUCT_DETAIL.LONG_DESCRIPTION as description, POMGR.PRODUCT_COLOR_DETAIL.BULLET_1 as bullet1, POMGR.PRODUCT_COLOR_DETAIL.BULLET_2 as bullet2, POMGR.PRODUCT_COLOR_DETAIL.BULLET_3 as bullet3, POMGR.PRODUCT_COLOR_DETAIL.BULLET_4 as bullet4, POMGR.PRODUCT_COLOR_DETAIL.BULLET_5 as bullet5, POMGR.PRODUCT_COLOR_DETAIL.BULLET_6 as bullet6, POMGR.PRODUCT_COLOR_DETAIL.BULLET_7 as bullet7, POMGR.PRODUCT_COLOR_DETAIL.BULLET_8 as bullet8, POMGR.PRODUCT_COLOR_DETAIL.BULLET_9 as bullet9, POMGR.PRODUCT_COLOR.VERSION as version FROM POMGR.PRODUCT_COLOR RIGHT JOIN POMGR.PO_LINE ON POMGR.PO_LINE.PRODUCT_COLOR_ID = POMGR.PRODUCT_COLOR.ID RIGHT JOIN POMGR.PO_HDR ON POMGR.PO_HDR.ID = POMGR.PO_LINE.PO_HDR_ID RIGHT JOIN POMGR.VENDOR ON POMGR.VENDOR.ID = POMGR.PO_HDR.VENDOR_ID INNER JOIN POMGR.LK_PO_TYPE ON POMGR.LK_PO_TYPE.ID = POMGR.PO_HDR.PO_TYPE_ID LEFT JOIN POMGR.INVENTORY ON POMGR.INVENTORY.PRODUCT_COLOR_ID = POMGR.PRODUCT_COLOR.ID LEFT JOIN POMGR.PRODUCT_DETAIL ON POMGR.PRODUCT_COLOR.PRODUCT_ID = POMGR.PRODUCT_DETAIL.PRODUCT_ID LEFT JOIN POMGR.PRODUCT_COLOR_DETAIL ON POMGR.PRODUCT_COLOR.PRODUCT_ID = POMGR.PRODUCT_COLOR_DETAIL.PRODUCT_COLOR_ID WHERE POMGR.VENDOR.NAME LIKE '%{0}%' ORDER BY POMGR.PO_LINE.PRODUCT_COLOR_ID DESC Nulls Last, POMGR.PRODUCT_COLOR.IMAGE_READY_DT DESC Nulls Last".format(vendorname)
+    #querymake_vendors_productimage_details = "SELECT DISTINCT POMGR.PO_LINE.PRODUCT_COLOR_ID as colorstyle, POMGR.PO_LINE.PO_HDR_ID as po_hdr_id, POMGR.VENDOR.NAME AS vendor_name, POMGR.PRODUCT_COLOR.COPY_READY_DT as copy_ready_dt, POMGR.PRODUCT_COLOR.IMAGE_READY_DT as image_ready_dt, POMGR.PRODUCT_COLOR.PRODUCTION_COMPLETE_DT, POMGR.PRODUCT_COLOR.ACTIVE, POMGR.VENDOR.THIRD_SUPPLIER_ID, POMGR.LK_PO_TYPE.NAME AS po_type, POMGR.PRODUCT_COLOR.CREATED_DATE as create_dt, POMGR.PRODUCT_COLOR.MODIFIED_DATE as modify_dt, POMGR.PRODUCT_DETAIL.MATERIAL as material, POMGR.PRODUCT_COLOR_DETAIL.SHORT_NAME as short_name, POMGR.PRODUCT_DETAIL.LONG_DESCRIPTION as description, POMGR.PRODUCT_COLOR_DETAIL.ALTERNATE_IMAGE_1 as alt1, POMGR.PRODUCT_COLOR_DETAIL.ALTERNATE_IMAGE_2 as alt2, POMGR.PRODUCT_COLOR_DETAIL.ALTERNATE_IMAGE_3 as alt3, POMGR.PRODUCT_COLOR_DETAIL.ALTERNATE_IMAGE_4 as alt4, POMGR.PRODUCT_COLOR_DETAIL.ALTERNATE_IMAGE_5 as alt5, POMGR.PRODUCT_COLOR.VERSION as version FROM POMGR.PRODUCT_COLOR RIGHT JOIN POMGR.PO_LINE ON POMGR.PO_LINE.PRODUCT_COLOR_ID = POMGR.PRODUCT_COLOR.ID RIGHT JOIN POMGR.PO_HDR ON POMGR.PO_HDR.ID = POMGR.PO_LINE.PO_HDR_ID RIGHT JOIN POMGR.VENDOR ON POMGR.VENDOR.ID = POMGR.PO_HDR.VENDOR_ID INNER JOIN POMGR.LK_PO_TYPE ON POMGR.LK_PO_TYPE.ID = POMGR.PO_HDR.PO_TYPE_ID LEFT JOIN POMGR.INVENTORY ON POMGR.INVENTORY.PRODUCT_COLOR_ID = POMGR.PRODUCT_COLOR.ID LEFT JOIN POMGR.PRODUCT_DETAIL ON POMGR.PRODUCT_COLOR.PRODUCT_ID = POMGR.PRODUCT_DETAIL.PRODUCT_ID LEFT JOIN POMGR.PRODUCT_COLOR_DETAIL ON POMGR.PRODUCT_COLOR.PRODUCT_ID = POMGR.PRODUCT_COLOR_DETAIL.PRODUCT_COLOR_ID WHERE POMGR.VENDOR.NAME LIKE '%{0}%' ORDER BY POMGR.PO_LINE.PRODUCT_COLOR_ID DESC Nulls Last, POMGR.PRODUCT_COLOR.IMAGE_READY_DT DESC Nulls Last".format(vendorname)
+
+    result = connection.execute(querymake_vendors_product_details)
+    styles = {}
+    for row in result:
+        style = {}
+        style['vendor_name'] = row['vendor_name']
+        style['short_name'] = row['short_name']
+        style['version'] = row['version']
+
+        styles[row['colorstyle']] = style
+        
+    #print consigstyles
+    connection.close()
+    return styles
+
+fragrancenet_imgs = [ f for f in walked_out_
+####### END FRAGRANCENET DETOUR ############
+
+## Copy Full Size Retouched Jpg to tmp Large and Med jpg folders for Glob Mogrify AND to Final Archive JPG_RETOUCHED_ORIG
+walkedout_renamed_wout_special = glob.glob(os.path.join(tmp_processing, '*.jpg'))
+
+
+## Large Without Special Processing needed 
+[ shutil.copy2(file, os.path.join(tmp_processing_l, os.path.basename(file))) for file in walkedout_renamed_wout_special ]
+walkedout_large = glob.glob(os.path.join(tmp_processing_l, '*.jpg'))
+### Remove alt images and rename as _l
+for f in walkedout_large:
+    if re.findall(regex_alt_jpg, f):
+        os.remove(f)
+    elif re.findall(regex_primary_jpg, f):
+        f_large = f.replace('.jpg', '_l.jpg')
+        os.rename(f, f_large)
+
+## Mofrify directory of only primary renamed _l Files  to 400x480
+subproc_magick_large_jpg(tmp_processing_l)
+
+
+## Medium
+[ shutil.copy2(file, os.path.join(tmp_processing_m, os.path.basename(file))) for file in walkedout_renamed ]
+walkedout_medium = glob.glob(os.path.join(tmp_processing_m, '*.jpg'))
+### Bypass rename alt images and rename only primary jpgs as _m
+for f in walkedout_medium:
+    if re.findall(regex_primary_jpg, f):
+        f_medium = f.replace('.jpg', '_m.jpg')
+        os.rename(f, f_medium)
+
+## Mofrify directory of renamed _m Files and unrenamed alts to 200x240
+subproc_magick_medium_jpg(tmp_processing_m)
+
+####
+#### JPEGS Have Been CREATED in Each of the tmp_processing folders named _l + _m
+####
+
+## PNG
+##### PNG CREATE FROM RETOUCHED JPGS ## All files in Root of tmp_processing will be mogrified to PNGs leaving JPG to Arch
+##  make png frpm hirez jpg then move copy to losding and orig to archive
+subproc_magick_png(tmp_processing)
+
+### Glob created PNGs and copy to Load Dir then Store in Arch dir 
+tmp_png = glob.glob(os.path.join(tmp_processing, '*.png'))
+[ shutil.copy2(file, os.path.join(tmp_loading, os.path.basename(file))) for file in tmp_png ]
+[ shutil.move(file, os.path.join(imgdest_png_final, os.path.basename(file))) for file in tmp_png ]
+
+## ARCHIVED Backup
+## All JPGs in Root dir Only of tmp_processing will be now Archived as all Conversions are completed
+jpgs_to_archive = glob.glob(os.path.join(tmp_processing, '*.jpg'))
+[ shutil.move(file, os.path.join(imgdest_jpg_final, os.path.basename(file))) for file in jpgs_to_archive ]
+
+
+###### All PNGs Created and moved to Archive plus Copy sent to Load Directory
+###
+######
+#### All Files Converted for Upload, Now glob search and move large and medium named jpgs to tmp loading
+###
+load_jpgs = glob.glob(os.path.join(tmp_processing, '*/*.jpg'))
+[ shutil.move(file, os.path.join(tmp_loading, os.path.basename(file))) for file in load_jpgs ]
+
+
+
+## UPLOAD FTP with PyCurl everything in tmp_loading
+###
+import time
+upload_tmp_loading = glob.glob(os.path.join(tmp_loading, '*.*g'))
+for upload_file in upload_tmp_loading:
+    #### UPLOAD upload_file via ftp to imagedrop using Pycurl
+    ## Then rm loading tmp dir
+    try:
+        if sys.argv[2] == str('DEV'):
+            print "Definately Not Uploaded {}".format(upload_file)
+            time.sleep(float(.3))
+            shutil.move(upload_file, archive_uploaded)
+            pass
+        elif sys.argv[2] == str('PRD'):
+            
+            code = pycurl_upload_imagedrop(upload_file)
+            if code:
+                print code, upload_file
+                time.sleep(float(3))
+                try:
+                    ftpload_to_imagedrop(upload_file)
+                    print "Uploaded {}".format(upload_file)
+                    time.sleep(float(.3))
+                    shutil.move(upload_file, archive_uploaded)
+                except:
+                    pass
+            else:
+                print "Uploaded {}".format(upload_file)
+                time.sleep(float(.3))
+                shutil.move(upload_file, archive_uploaded)
+        
+    except:
+        print "Error moving Finals to Arch {}".format(file)
+        
+### Delete redundant PNGs in upload dir
+del_extra_png = os.listdir(imgdest_png_final)
+loaded_png = os.listdir(archive_uploaded)
+for f in del_extra_png:
+    if f in loaded_png:
+        os.chdir(imgdest_png_final)
+        os.remove(f)
+
+### delete empty upload dir
+del_extra_png = os.listdir(imgdest_png_final)
+if len(del_extra_png) == 0:
+    os.rmdir(imgdest_png_final)
+
+## After completed Process and Load to imagedrop
+###  Finally Remove the 2 tmp folder trees for process and load if Empty
+upload_tmp_loading_remainder = glob.glob(os.path.join(tmp_loading, '*.*g'))
+if len(upload_tmp_loading_remainder) == 0:
+    shutil.rmtree(tmp_loading)
+
+## upload_tmp_processing_png_remainder = glob.glob(os.path.join(tmp_processing, '*/*.png'))
+upload_tmp_processing_jpg_remainder = glob.glob(os.path.join(tmp_processing, '*/*.jpg'))
+if len(upload_tmp_processing_jpg_remainder) == 0:
+    shutil.rmtree(tmp_processing)
+
+##### Remove Lock file
+#if os.path.isfile(locker):
+#    os.remove(locker)
