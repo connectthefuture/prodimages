a4a69494a0c7fd032278b6d70f23ef2bc3891f22
jb <relic7@aol.com>
2014-05-21 21:35:01 -0400

    x

---
+++ python/magick_colormanage_infiles.py
@@ -0,0 +1,74 @@
+#!/usr/local/env python
+# -*- unicode-8 -*-
+#=/Users/johnb/Downloads/bella42413/bellaapendix/one/324264801/324264801_1.jpg
+import os,sys,re,subprocess,glob
+
+## CMYK Profile Files
+cmyk_uswebcoat='/usr/local/color_profiles/standard/USWebCoatedSWOP.icc'
+cmyk_ussheetfedcoat='/usr/local/color_profiles/standard/USSheetfedCoated.icc'
+cmyk_jpn01coat='/usr/local/color_profiles/standard/JapanColor2001Coated.icc'
+cmyk_FOGRA39coat='/usr/local/color_profiles/standard/CoatedFOGRA39.icc'
+cmyk_FOGRA27coat='/usr/local/color_profiles/standard/CoatedFOGRA27.icc'
+
+## RGB Profile Files
+adobe98='/usr/local/color_profiles/standard/AdobeRGB1998.icc'
+srgb_webrdy='/usr/local/color_profiles/standard/sRGB.icm'
+
+regex_geometry = re.compile(r'^Geometry.+?$')
+
+def metadata_dimension_pair(inputfile):
+    import os,sys,re,subprocess,glob
+    regex_geometry = re.compile(r'^Geometry.+?$')
+    metadict = {}
+    fileinfo = {}
+    fname=os.path.basename(inputfile)
+    dname=os.path.dirname(inputfile)
+    regex_geometry_attb = re.compile(r'.*?Geometry.*?[0-9,{1,4}]x[0-9,{1,4}].*?$')
+    regex_border_clr   = re.compile(r'.*?Bordercolor.*?$')
+    regex_colorspace  = re.compile(r'.*?Colorspace.*?$')
+    
+    metadata=subprocess.check_output(['identify', 
+                                       '-verbose', 
+                                        inputfile
+                                        ])
+            
+    metadata_list = metadata.replace(' ','').split('\n')
+    
+    g_width = [ g.split(':')[-1].split('+')[0].split('x')[0] for g in metadata_list if regex_geometry.findall(g) ]
+    g_height = [ g.split(':')[-1].split('+')[0].split('x')[1] for g in metadata_list if regex_geometry.findall(g) ]
+    
+    metadata_width    = float(g_width[0])
+    metadata_height   = float(g_height[0])
+    
+    dominantclr_list = subprocess.check_output(['convert', 
+                                                inputfile, 
+                                                '-posterize',
+                                                '3',
+                                                '-define',
+                                                'histogram:unique-colors=true',
+                                                '-format',
+                                                '"%c"',
+                                                'histogram:info:-'
+                                                ])
+                    
+    dominantclr_list = metadata.replace(' ','').split('\n')
+    borderclr = [ borderclr.split(':')[-1] for borderclr in dominantclr_list if regex_border_clr.findall(borderclr) ]
+    colorspace = [ colorspace.split(':')[-1] for colorspace in dominantclr_list if regex_colorspace.findall(colorspace) ]
+    
+    aspect_ratio =  metadata_height/metadata_width
+    aspect_ratio = "{0:.2f}".format(round(aspect_ratio,2))
+    
+    fileinfo['width'] = "{0:.0f}".format(round(metadata_width,2))
+    fileinfo['height'] = "{0:.0f}".format(round(metadata_height,2))
+    fileinfo['aspect'] = aspect_ratio
+    fileinfo['borderclr'] = borderclr
+    fileinfo['colorspace'] = colorspace
+    metadict[inputfile] = fileinfo
+    
+    return metadict
+    
+####### RUN
+if __name__ == "__main__":
+    import sys
+    metadata_dimension_pair(sys.argv[1])
+    
