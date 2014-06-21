51d42daeab647c333e72adb1567592fa778d2808
johnb <jbragato@yahoo.com>
2014-06-21 17:21:59 -0400

    x

--- bash/cronjob_nightly_india_cache_clear.sh
+++ bash/cronjob_nightly_india_cache_clear.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 . ~/.bash_profile
 
-styles=$(find /mnt/Post_Complete/Complete_Archive/SendReceive_BGRemoval/4_Archive/JPG/LIST_PAGE_LOADED -iname \*_l.jpg -mtime -2 -exec basename {} \; | sed 's/_l.jpg//g' | xargs)
+styles=`find /mnt/Post_Complete/Complete_Archive/SendReceive_BGRemoval/4_Archive/JPG/LIST_PAGE_LOADED -iname \*_l.jpg -mtime -2 -exec basename {} \; | sed 's/_l.jpg//g' | xargs`
 
 for s in "$styles"; do 
 blueflypdp_AllServer_cache_clear.py $s; 
