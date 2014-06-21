505302df574dfd56441936f17c30b4785458b1ae
johnb <jbragato@yahoo.com>
2014-06-21 17:11:05 -0400

    cronjob edgecast

---
+++ bash/cronjob_nightly_india_cache_clear.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+. ~/.bash_profile
+
+styles=$(find /mnt/Post_Complete/Complete_Archive/SendReceive_BGRemoval/4_Archive/JPG/LIST_PAGE_LOADED -iname \*_l.jpg -mtime -3 -exec basename {} \; | sed 's/_l.jpg//g' | xargs)
+
+for s in "$styles"; do 
+blueflypdp_AllServer_cache_clear.py $f; 
+echo "$f";
+done
+
