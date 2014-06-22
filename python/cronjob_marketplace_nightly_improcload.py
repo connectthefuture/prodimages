d8346c9aa99fafddfa7746944a25e09fdb3695b7
johnb <jbragato@yahoo.com>
2014-06-22 14:02:08 -0400

    x

--- python/cronjob_marketplace_nightly_improcload.py
+++ python/cronjob_marketplace_nightly_improcload.py
@@ -62,7 +62,7 @@ def sqlQuery_GetIMarketplaceImgs(vendor=None,vendor_brand=None, po_number=None):
 
 ############################################################ RUN ##################################################
 ############################################################ RUN ##################################################
-import os,re,sys,urllib, glob
+import os,re,sys,urllib, glob, re
 import requests
 
 ## Create image dir Root if not exist
