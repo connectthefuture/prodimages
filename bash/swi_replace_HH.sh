c489d7b9da1d80a462075119dcb7e69d9c976401
johnb <jbragato@yahoo.com>
2014-06-26 17:25:31 -0400

    x

--- bash/swi_replace_HH.sh
+++ bash/swi_replace_HH.sh
@@ -2,8 +2,8 @@
 . ~/.bash_profile
 
 
-swidir="$1"
+swidir=$1
 
-for f in `find $swidir -type f -iname *_H*`; do
+for f in $(find $swidir -type f -iname *_H*); do
 mv $f $(echo $f|sed 's/_HH//g'|sed $f 's/_H//g');
 done
