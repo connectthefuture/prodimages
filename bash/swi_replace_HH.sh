8ae9da3fbe08df0405621c555affbc09d1c8631d
johnb <jbragato@yahoo.com>
2014-06-26 17:23:46 -0400

    xx

--- bash/swi_replace_HH.sh
+++ bash/swi_replace_HH.sh
@@ -4,6 +4,6 @@
 
 swidir = "$1"
 
-for f in `find "$swidir" -type f -iname *_H*`; do
-mv "$f" $(echo $f|sed 's/_HH//g'|sed $f 's/_H//g');
+for f in `find $swidir -type f -iname *_H*`; do
+mv $f $(echo $f|sed 's/_HH//g'|sed $f 's/_H//g');
 done
