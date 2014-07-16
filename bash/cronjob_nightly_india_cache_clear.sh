5ea3d39963501a03239ee88c28ce5a05d7e84905
johnb <jbragato@yahoo.com>
2014-07-16 16:51:07 -0400

    cron india

--- bash/cronjob_nightly_india_cache_clear.sh
+++ bash/cronjob_nightly_india_cache_clear.sh
@@ -2,10 +2,12 @@
 
 #. ~/.bash_profile
 
-styles=`find /mnt/Post_Complete/Complete_Archive/SendReceive_BGRemoval/4_Archive/JPG/LIST_PAGE_LOADED -iname \*_l.jpg -mtime -2 -exec basename {} \; | sed 's/_l.jpg//g' | xargs`
+#styles=`find /mnt/Post_Complete/Complete_Archive/SendReceive_BGRemoval/4_Archive/JPG/LIST_PAGE_LOADED -iname \*_l.jpg -mtime -2 -exec basename {} \; | sed 's/_l.jpg//g' | xargs`
+#
+#for s in $styles; do 
+/usr/bin/python /usr/local/batchRunScripts/python/readcsv_clearcache.py
 
-for s in $styles; do 
-/usr/bin/python /usr/local/batchRunScripts/python/blueflypdp_AllServer_cache_clear.py $s; 
-echo $s;
-done
+#$s; 
+#echo $s;
+#done
 
