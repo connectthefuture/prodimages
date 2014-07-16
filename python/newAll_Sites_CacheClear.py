79747d178470c0754d97b130f8231c3d4ee485f9
johnb <jbragato@yahoo.com>
2014-07-16 15:14:37 -0400

    x

--- python/newAll_Sites_CacheClear.py
+++ python/newAll_Sites_CacheClear.py
@@ -162,6 +162,9 @@ def send_purge_request_edgecast(mediaPath):
 def main(colorstyle_list=None):
     import sys,re,os
     
+    if not colorstyle_list:
+        colorstyle_list = sys.argv[1:]
+
     alturl = 'altimage.ms'
 
 
@@ -424,6 +427,4 @@ def main(colorstyle_list=None):
 #print edgecast_listurls
 
 if __name__ == '__main__':
-    if not colorstyle_list:
-        colorstyle_list = sys.argv[1:]
-    main(colorstyle_list=colorstyle_list)
+    main()
