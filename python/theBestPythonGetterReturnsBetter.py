5ed17c07db46bc702963c573c77f4aa7874e882b
johnb <jbragato@yahoo.com>
2014-07-23 22:23:31 -0400

    x

--- python/theBestPythonGetterReturnsBetter.py
+++ python/theBestPythonGetterReturnsBetter.py
@@ -47,15 +47,16 @@ def main():
     connection = mysql_engine.connect()   
     daily_incomplete_query = '''SELECT t1.`file_path`, t1.`colorstyle`, t2.`image_ready_dt` FROM `data_imagepaths`.`push_photoselects` t1 join `data_imagepaths`.`product_snapshot_live` t2 on t1.`colorstyle` = t2.`colorstyle` having (t2.`image_ready_dt` = '0000-00-00') ORDER BY t1.`file_path`  ASC '''
 
-    daily_reshoot_query = '''select distinct data.`file_path`,data.`photo_date`,data.`colorstyle`, t3.`file_path`, t3.`photo_date` 
+    daily_reshoot_query = """select distinct CONCAT('/mnt/Post_Ready', t3.`file_path`),t3.`photo_date`,data.`colorstyle`, data.`file_path`, data.`photo_date` 
                 FROM 
                     (SELECT t1.`file_path`, t1.`colorstyle`, t2.`image_ready_dt` , t1.`photo_date`,t1.alt
                     FROM `data_imagepaths`.`push_photoselects` t1 
                     join `data_imagepaths`.`product_snapshot_live` t2 on t1.`colorstyle` = t2.`colorstyle` 
+                    where t1.photo_date BETWEEN SYSDATE( ) - INTERVAL 30 DAY AND SYSDATE( ) + INTERVAL 2 DAY
                     having (t2.`image_ready_dt` != '0000-00-00'))
                 AS data
                 join `data_imagepaths`.`post_ready_original` t3 on data.`colorstyle` = t3.`colorstyle`
-                where data.`photo_date` != t3.`photo_date` and data.`alt` = t3.`alt`'''
+                where data.`image_ready_dt` < t3.`photo_date` and data.`alt` = t3.`alt`"""
     #daily_incomplete_query = '''SELECT t1.`file_path`, t1.`colorstyle`, t2.`image_ready_dt` FROM `data_imagepaths`.`push_photoselects` t1 join `data_imagepaths`.`product_snapshot` t2 on t1.`colorstyle` = t2.`colorstyle` having (t2.`image_ready_dt` = '0000-00-00') ORDER BY t1.`file_path`  ASC '''
 
     result = connection.execute(daily_incomplete_query)
