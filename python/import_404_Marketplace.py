92a8ac3004b5f7a720a551ddbd2b75f947818e4a
johnb <jbragato@yahoo.com>
2014-07-24 18:09:02 -0400

    x

--- python/import_404_Marketplace.py
+++ python/import_404_Marketplace.py
@@ -1,8 +1,39 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import os,sys,sqlalchemy,glob
+import os,sys,sqlalchemy,glob,io
 
 
-badurldir = '/mnt/Post_Complete/Complete_Archive/MARKETPLACE/ERRORS/'
-errorfiles = glob.glob(os.path.join(os.path.abspath(badurldir), '*.txt'))
+def import_404_data():
+    mysql_engine_data = sqlalchemy.create_engine('mysql+mysqldb://root:mysql@prodimages.ny.bluefly.com:3301/data_imagepaths')
+    mysql_engine_www  = sqlalchemy.create_engine('mysql+mysqldb://root:mysql@prodimages.ny.bluefly.com:3301/www_django')
+    connection_data = mysql_engine_data.connect()
+    connection_www = mysql_engine_www.connect()
+    
+    badurldir = '/mnt/Post_Complete/Complete_Archive/MARKETPLACE/ERRORS/'
+    errorfiles = ''
+    try:
+        errorfiles = [io.open(f,'rbU').read().split('/')[0] for f in glob.glob(os.path.join(os.path.abspath(badurldir), '*.txt'))]
+    except:
+        pass
+    errorstyles = [f.split('/')[-1][:9] for f in glob.glob(os.path.join(os.path.abspath(badurldir), '*.txt'))]
+    if errorfiles:
+        zipped = zip(errorstyles,errorfiles)
+        print errorfiles
+    
+    for colorstyle in errorstyles:
+        errorcode = '404'
+        try:
+
+            connection_www.execute("""INSERT INTO supplier_ingest_404 (colorstyle, errorcode) VALUES (%s,%s)
+                                    ON DUPLICATE KEY UPDATE 
+                                    errorcode  = VALUES(errorcode);""", v['colorstyle'], v['errorcode'])
+        except:
+            pass
+
+def main():
+    import_404_data()
+
+
+if __name__ == '__main__':
+    main()
