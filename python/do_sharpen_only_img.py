cad4a50edb2ed594d09e1589b4e7cc91df8b4e1f
johnb <john.bragato@bluefly.com>
2014-08-06 15:13:11 -0400

    x

--- python/do_sharpen_only_img.py
+++ python/do_sharpen_only_img.py
@@ -22,43 +22,9 @@ def sharpen_image(img, destdir=None):
         destdir = os.path.abspath('.')
     else:
         destdir = os.path.abspath(destdir)
-
+ 
     outfile = os.path.join(destdir, img.split('/')[-1].split('.')[0] + '.png')
-
-    dimensions = ''
-    ## Get variable values for processing
-    aspect_ratio = get_aspect_ratio(img)
-    dimensions = get_dimensions(img)
-    width  = dimensions.split('x')[0]
-    height = dimensions.split('x')[1]
     
-    if aspect_ratio == '1.2':
-        vert_horiz = '{0}x{1}'.format(width,height)  
-        dimensions = '{0}x{1}'.format(int(width),int(height))
-    elif float(aspect_ratio) > float(int(1.2)):
-        vert_horiz = 'x{0}'.format(height)
-        w = float(0.8) * float(height)
-        #w = float(round(w,2)*float(aspect_ratio))
-        dimensions = '{0}x{1}'.format(int(w),int(height))
-        print "W",w, aspect_ratio
-    elif float(aspect_ratio) < float(1.2):
-        vert_horiz = '{0}x'.format(width)
-        h = float(1.2) * float(width)
-        #h = float(round(h,2)*float(aspect_ratio))
-        dimensions = '{0}x{1}'.format(int(width),int(h))
-        print "H",h, aspect_ratio
-    
-    if not dimensions:
-        dimensions = '100%'
-        vert_horiz = '100%'
-    outfile = os.path.join(destdir, img.split('/')[-1].split('.')[0] + '.png')
-    if os.path.isfile(outfile):
-        try:
-            os.remove(outfile)
-        except:
-            pass
-    else:
-        pass
     subprocess.call([
             'mogrify',
             '-format',
@@ -87,7 +53,7 @@ def sharpen_image(img, destdir=None):
             '-quality', 
             '95'
             ])
-    os.rename(img,outfile)
+    #os.rename(img,outfile)
     print 'Done {}'.format(img)
     return os.path.join(destdir, img.split('/')[-1].split('.')[0] + '.png')
     
