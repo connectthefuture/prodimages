b36b009fde97df547f37dbcc890fc637fcc95e2b
johnb <jbragato@yahoo.com>
2014-08-06 05:26:11 -0400

    WORKING MULTI DLOADER

--- python/swi_multi_dload.py
+++ python/swi_multi_dload.py
@@ -2,23 +2,40 @@
 # -*- coding: utf-8 -*-
 
 def sqlQuery_GetStyleVendor_ByPO(ponum=None):
-    import sqlalchemy, sys
+    import sqlalchemy, sys, re
     orcl_engine = sqlalchemy.create_engine('oracle+cx_oracle://prod_team_ro:9thfl00r@borac101-vip.l3.bluefly.com:1521/bfyprd11')
     connection = orcl_engine.connect()
+    print 'THIS IS PO',ponum
     if ponum:
         querymake_StylesByPO="SELECT POMGR.PRODUCT_COLOR.ID AS colorstyle, POMGR.PRODUCT_COLOR.VENDOR_STYLE AS vendor_style, POMGR.PO_LINE.PO_HDR_ID AS ponumber FROM POMGR.PRODUCT_COLOR INNER JOIN POMGR.PO_LINE ON POMGR.PRODUCT_COLOR.ID = POMGR.PO_LINE.PRODUCT_COLOR_ID WHERE POMGR.PO_LINE.PO_HDR_ID = '" + ponum + "' AND PRODUCT_COLOR.IMAGE_READY_DT is null"
     else:
-        ## Get all missing Styles vs above which takes a PO list
-        querymake_StylesByPO="SELECT POMGR.PRODUCT_COLOR.ID AS colorstyle, POMGR.PRODUCT_COLOR.VENDOR_STYLE AS vendor_style, POMGR.PO_LINE.PO_HDR_ID AS ponumber FROM POMGR.PRODUCT_COLOR INNER JOIN POMGR.PO_LINE ON POMGR.PRODUCT_COLOR.ID = POMGR.PO_LINE.PRODUCT_COLOR_ID WHERE PRODUCT_COLOR.IMAGE_READY_DT is null"
+        ## Get all missing SWISS WATCH Styles vs above which takes a PO list
+        querymake_StylesByPO="""SELECT POMGR.PRODUCT_COLOR.ID      AS colorstyle,
+                                POMGR.PRODUCT_COLOR.VENDOR_STYLE AS vendor_style,
+                                POMGR.PO_LINE.PO_HDR_ID          AS ponumber
+                                FROM POMGR.PRODUCT_COLOR
+                                RIGHT JOIN POMGR.PO_LINE
+                                ON POMGR.PRODUCT_COLOR.ID = POMGR.PO_LINE.PRODUCT_COLOR_ID
+                                RIGHT JOIN POMGR.PO_HDR
+                                ON POMGR.PO_LINE.PO_HDR_ID = POMGR.PO_HDR.ID
+                                RIGHT JOIN POMGR.VENDOR
+                                ON POMGR.VENDOR.ID                        = POMGR.PO_HDR.VENDOR_ID
+                                WHERE POMGR.VENDOR.NAME like 'Swiss Watch%'
+                                and POMGR.PRODUCT_COLOR.IMAGE_READY_DT IS NULL
+                                ORDER BY POMGR.PRODUCT_COLOR.IMAGE_READY_DT DESC nulls Last"""
 
     result = connection.execute(querymake_StylesByPO)
     styles = {}
+    regex = re.compile(r'^\d{8}[^0]{1}$')
     for row in result:
-        style = {}        
-        style['colorstyle'] = row['colorstyle']
-        style['ponumber'] = row['ponumber']
-        styles[row['vendor_style']] = style
-        
+        if regex.findall(str(row['colorstyle'])):
+            style = {}        
+            style['colorstyle'] = str(row['colorstyle'])
+            style['ponumber'] = str(row['ponumber'])
+            styles[row['vendor_style']] = style
+            print style
+        else:
+            pass    
     connection.close()
     return styles
 
@@ -45,7 +62,7 @@ def define_variables_mkdirs():
             pass
     return destdir
 
-     
+
 def url_download_file(url,filepath):
     import urllib
     
@@ -113,7 +130,7 @@ def get_postyles_dict(polist=None):
     if polist:
         for ponum in polist:
             try:
-                stylesDict = sqlQuery_GetStyleVendor_ByPO(ponum)
+                stylesDict = sqlQuery_GetStyleVendor_ByPO(ponum=ponum)
                 stylesDictsDict.append(stylesDict)
             except:
                 pass
@@ -122,84 +139,83 @@ def get_postyles_dict(polist=None):
     return stylesDictsDict
 
 
-def download_urls_bypo(stylesDictsDict):
+def download_urls_bypo(ponum):
     destdir = ''
     
     if not destdir:
         destdir = define_variables_mkdirs()
-    
     originaldest = destdir
+    print ponum,'PREE'
+    stylesDict = sqlQuery_GetStyleVendor_ByPO(ponum=ponum)
+    for k,v in stylesDict.iteritems():
+        print k,v 
+        vendor_url = "http://admin.swisswatchintl.com/Z/{0}.jpg".format(k)
+        vendor_url_side = vendor_url.replace('.jpg', '-side.jpg')
+        vendor_url_back = vendor_url.replace('.jpg', '-back.jpg')
+        vendor_url_boxset = vendor_url.replace('.jpg', '-boxset.jpg')
+        vendor_url_straps = vendor_url.replace('.jpg', '-straps.jpg')
+        vendor_url_main = vendor_url.replace('.jpg', '-main.jpg')
 
-    for stylesDict in stylesDictsDict:
-        if type(stylesDictsDict) == dict:
-            stylesDict = stylesDictsDict
-        else:
+        
+        style = str(v['colorstyle'])
+        ponumber = str(v['ponumber'])
+        colorstyle = ''
+        colorstyle =  style          +   "_1.jpg"
+        colorstyle_side = style      +   "_2.jpg"
+        colorstyle_back = style      +   "_3.jpg"
+        colorstyle_boxset = style    +   "_4.jpg"
+        colorstyle_straps = style    +   "_5.jpg"
+        colorstyle_main = style      +   "_6.jpg"
+        
+        # Make the subdir by POnum
+        destdir = os.path.join(os.path.abspath(originaldest),ponumber)
+        try:
+            os.makedirs(destdir, 16877)
+        except OSError:
             pass
-        for k,v in stylesDict.iteritems():
-                
-            vendor_url = "http://admin.swisswatchintl.com/Z/{0}.jpg".format(k)
-            vendor_url_side = vendor_url.replace('.jpg', '-side.jpg')
-            vendor_url_back = vendor_url.replace('.jpg', '-back.jpg')
-            vendor_url_boxset = vendor_url.replace('.jpg', '-boxset.jpg')
-            vendor_url_straps = vendor_url.replace('.jpg', '-straps.jpg')
-            vendor_url_main = vendor_url.replace('.jpg', '-main.jpg')
-
-            
-            style = str(v['colorstyle'])
-            ponumber = str(v['ponumber'])
-            colorstyle = ''
-            colorstyle =  style          +   "_1.jpg"
-            colorstyle_side = style      +   "_2.jpg"
-            colorstyle_back = style      +   "_3.jpg"
-            colorstyle_boxset = style    +   "_4.jpg"
-            colorstyle_straps = style    +   "_5.jpg"
-            colorstyle_main = style      +   "_6.jpg"
-            
-            # Make the subdir by POnum
-            # destdir = os.path.join(os.path.abspath(originaldest),ponumber)
-            # try:
-            #     os.makedirs(destdir, 16877)
-            # except OSError:
-            #     pass
-            
-            colorstyle_file = os.path.join(os.path.abspath(destdir), colorstyle)
-            colorstyle_side_file = os.path.join(os.path.abspath(destdir),colorstyle_side)
-            colorstyle_back_file = os.path.join(os.path.abspath(destdir), colorstyle_back)
-            colorstyle_boxset_file = os.path.join(os.path.abspath(destdir), colorstyle_boxset)
-            colorstyle_straps_file = os.path.join(os.path.abspath(destdir), colorstyle_straps)
-            colorstyle_main_file = os.path.join(os.path.abspath(destdir), colorstyle_main)
+        
+        colorstyle_file = os.path.join(os.path.abspath(destdir), colorstyle)
+        colorstyle_side_file = os.path.join(os.path.abspath(destdir),colorstyle_side)
+        colorstyle_back_file = os.path.join(os.path.abspath(destdir), colorstyle_back)
+        colorstyle_boxset_file = os.path.join(os.path.abspath(destdir), colorstyle_boxset)
+        colorstyle_straps_file = os.path.join(os.path.abspath(destdir), colorstyle_straps)
+        colorstyle_main_file = os.path.join(os.path.abspath(destdir), colorstyle_main)
 
 
-            #imagefalse = sqlQuery_GetStyleVendor_ByPO()
-            #if imagefalse:
-            
-            ##_1
-            try:            
-                url_download_file(vendor_url,colorstyle_file)
-            except:
-                print "Failed {}{}".format(vendor_url,colorstyle_file)
+        #imagefalse = sqlQuery_GetStyleVendor_ByPO()
+        #if imagefalse:
+        
+        ##_1
+        try:            
+            url_download_file(vendor_url,colorstyle_file)
+        except:
+            print "Failed {}{}".format(vendor_url,colorstyle_file)
 
-            ##_2
-            try:            
-                url_download_file(vendor_url_side,colorstyle_side_file)
-                print "Downloaded {}".format(colorstyle_side_file)
-            except:
-                print "Failed {}{}".format(vendor_url,colorstyle_side_file)
+        ##_2
+        try:            
+            url_download_file(vendor_url_side,colorstyle_side_file)
+            print "Downloaded {}".format(colorstyle_side_file)
+        except:
+            print "Failed {}{}".format(vendor_url,colorstyle_side_file)
 
-            ## _3
-            try:            
-                url_download_file(vendor_url_back,colorstyle_back_file)
-                print "Downloaded {}".format(colorstyle_back_file)
+        ## _3
+        try:            
+            url_download_file(vendor_url_back,colorstyle_back_file)
+            print "Downloaded {}".format(colorstyle_back_file)
+        except:
+            try:
+                url_download_file(vendor_url_back,colorstyle_back_file.replace('-back','-clasp'))
+                print "Downloaded {}".format(colorstyle_back_file.replace('-back','-clasp'))
             except:
                 try:
-                    url_download_file(vendor_url_back,colorstyle_back_file.replace('-back','-clasp'))
-                    print "Downloaded {}".format(colorstyle_back_file.replace('-back','-clasp'))
+                   url_download_file(vendor_url_back,colorstyle_back_file.replace('-back','-Clasp'))
                 except:
-                    try:
-                       url_download_file(vendor_url_back,colorstyle_back_file.replace('-back','-Clasp'))
-                    except:
-                        print "Failed {}{}".format(vendor_url,colorstyle_back_file.replace('-back','-Clasp'))
-
+                    print "Failed {}{}".format(vendor_url,colorstyle_back_file.replace('-back','-Clasp'))
+        # # Try to remove empty dirs
+        # try:
+        #     os.rmdir(destdir)
+        # except:
+        #     pass
 
 ## Run MAin as a multiprocessor by PO
 def run_multiproccesses_download(cmd_process=None,args=None):
@@ -209,12 +225,23 @@ def run_multiproccesses_download(cmd_process=None,args=None):
     pool = multiprocessing.Pool(4)
     
     if not args:
-        args = get_postyles_dict()
+        # args = get_postyles_dict()
+        popre= get_postyles_dict()
+        polist = []
+        for v in popre.itervalues():
+            po = v['ponumber']
+            polist.append(po)
+        args = sorted(list(set(sorted(polist))))
+        # if type(args) == list:
+        #     polist =  str(args).strip('[]')
+        #args =  str(args).strip('[]')
+        #args = (args,)
     #cmd_process = getattr(.,"{}".format(cmd_process)) #locals()["{}".format(cmd_process)]()
     #func = getattr(sys.modules[__name__], 'download_urls_bypo')
-    print type(args)
-    print type(getattr(sys.modules[__name__], 'download_urls_bypo'))
-    results = pool.map(download_urls_bypo,args)
+    #print type(args)
+    
+    func = getattr(sys.modules[__name__], str(cmd_process))
+    results = pool.map(func,args)
     print type(results)
     
     # close the pool and wait for the work to finish
@@ -227,34 +254,35 @@ def run_multiproccesses_download(cmd_process=None,args=None):
 if __name__ == '__main__':
     import sys, importlib
     
+    # try:
+    #     polist = set(list(sys.argv[1:]))
+    #     stylesDictsDict = get_postyles_dict(polist)
+
+    #     #mod  = importlib.import_module(swi_multi_dload)
+    #     #mod =  dir(sys.modules[__name__])
+    #     #mod3 = dir(__name__)
+    #     #print mod,mod3
+        
+    #     func =  'download_urls_bypo'
+    #     run_multiproccesses_download(cmd_process=func,args=stylesDictsDict)
+    
+    # except IndexError:
+
     try:
-        polist = set(list(sys.argv[1:]))
-        stylesDictsDict = get_postyles_dict(polist)
+        # popre= get_postyles_dict()
+        # polist = []
+        # for v in popre.itervalues():
+        #     po = v['ponumber']
+        #     polist.append(po)
+        # polist = sorted(list(set(sorted(polist))))
+        # stylesDictsDict = get_postyles_dict(polist.reverse())
 
         #mod  = importlib.import_module(swi_multi_dload)
         mod =  dir(sys.modules[__name__])
-        mod3 = dir(__name__)
-        print mod,mod3
+        #print mod,mod3
+        
         func =  'download_urls_bypo'
-        run_multiproccesses_download(cmd_process=func,args=stylesDictsDict)
-    
-    except IndexError:
-
-        try:
-            popre= get_postyles_dict()
-            polist = []
-            for k,v in propre.iteritems():
-                po = v['ponumber']
-                polist.append(po)
-            
-            stylesDictsDict = get_postyles_dict(polist)
-
-            #mod  = importlib.import_module(swi_multi_dload)
-            mod =  dir(sys.modules[__name__])
-            print mod,mod3
-            
-            func =  'download_urls_bypo'
-            run_multiproccesses_download(cmd_process=func,args=stylesDictsDict)
-        except:
-            print 'EXCEPT MAIN only'
-            
+        run_multiproccesses_download(cmd_process=func,args=None)
+    except:
+        print 'EXCEPT MAIN only'
+        
