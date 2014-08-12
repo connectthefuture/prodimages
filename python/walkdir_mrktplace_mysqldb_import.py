87d1a057b1b967e8a8106a73660151f3c72cf6c7
johnb <jbragato@yahoo.com>
2014-08-11 21:33:19 -0400

    x

--- python/walkdir_mrktplace_mysqldb_import.py
+++ python/walkdir_mrktplace_mysqldb_import.py
@@ -261,7 +261,7 @@ def main(rootdir=None):
             #regex_productionraw = re.compile(r'^/.+?/ON_FIGURE/.+?RAW_FILES.*?/[0-9]{9}_[1-9]_[0-9]{1,4}\.[jpgJPGCR2]{3}$')
             #regex_mediarepo = re.compile(r'^.+?MEDIAREPO.+?\.[NnjpgJPG]$')
             regex_consigANY = re.compile(r'^/.+?/MARKETPLACE/.+?/output/[0-9]{9}?_?[1-9]?_?[0-9]{,4}?.*?\.[jpengJPENGCR2]{3}$')
-            regex_consigConvertedPNG = re.compile(r'^/.+?/MARKETPLACE/.+?/output/[0-9]{9}_?.*?\.[png]{3}$')
+            regex_consigConvertedPNG = re.compile(r'^/.+?/MARKETPLACE/.+?/output/.+?/[0-9]{9}_?.*?\.[png]{3}$')
             
             regex_postreadyoriginal = re.compile(r'^/Retouch_.+?/.*?[0-9]{9}_[1-6]\.[jpgJPG]{3}$')
             regex_zimages = re.compile(r'^/zImages.*?/[0-9]{4}/.*?[0-9]{9}_[1-6]\.[jpgJPG]{3}$')
