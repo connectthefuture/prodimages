c38ccf728227c49d5e17d519e7d82a537c85a3a9
johnb <jbragato@yahoo.com>
2014-08-20 15:28:23 -0400

    fixed lazy load breaking sxripts to scrape, an extra none type is to blame

--- python/bfly_listpage_scrape_clear.py
+++ python/bfly_listpage_scrape_clear.py
@@ -264,26 +264,30 @@ found_links = url_get_links(bfly_url)
 
 colorstyles = []
 for link in found_links:
-    list_urllist.append(link)
-    colorstyle=link.split('/prodImage.ms?productCode=')[-1][:9]
-    if colorstyle.isdigit():
-        colorstyles.append(colorstyle)
-        ## Create list page urls for Edgecast
-        oldlistpg      =   'http://cdn.is.bluefly.com/mgen/Bluefly/prodImage.ms?productCode={0}&width=157&height=188'.format(colorstyle)
-        newlistpg      =   'http://cdn.is.bluefly.com/mgen/Bluefly/prodImage.ms?productCode={0}&width=251&height=300'.format(colorstyle)
-        pdpg           =   'http://cdn.is.bluefly.com/mgen/Bluefly/prodImage.ms?productCode={0}&width=340&height=408'.format(colorstyle)
-        pmlistpg       =   'http://cdn.is.bluefly.com/mgen/Bluefly/prodImage.ms?productCode={0}&width=50&height=60&ver=null'.format(colorstyle)
-        pmeventimg     =   'http://cdn.is.bluefly.com/mgen/Bluefly/eqzoom85.ms?img={0}.pct&outputx=200&outputy=240&level=1&ver=null'.format(colorstyle)				
-        email_img1     =   'http://cdn.is.bluefly.com/mgen/Bluefly/prodImage.ms?productCode={0}&width=140&height=182'.format(colorstyle)
-        email_img2     =   'http://cdn.is.bluefly.com/mgen/Bluefly/prodImage.ms?productCode={0}&width=200&height=250'.format(colorstyle)
+    if link:
+        list_urllist.append(link)
+        colorstyle=link.split('/prodImage.ms?productCode=')[-1][:9]
+        if colorstyle.isdigit():
+            colorstyles.append(colorstyle)
+            ## Create list page urls for Edgecast
+            oldlistpg      =   'http://cdn.is.bluefly.com/mgen/Bluefly/prodImage.ms?productCode={0}&width=157&height=188'.format(colorstyle)
+            newlistpg      =   'http://cdn.is.bluefly.com/mgen/Bluefly/prodImage.ms?productCode={0}&width=251&height=300'.format(colorstyle)
+            pdpg           =   'http://cdn.is.bluefly.com/mgen/Bluefly/prodImage.ms?productCode={0}&width=340&height=408'.format(colorstyle)
+            pmlistpg       =   'http://cdn.is.bluefly.com/mgen/Bluefly/prodImage.ms?productCode={0}&width=50&height=60&ver=null'.format(colorstyle)
+            pmeventimg     =   'http://cdn.is.bluefly.com/mgen/Bluefly/eqzoom85.ms?img={0}.pct&outputx=200&outputy=240&level=1&ver=null'.format(colorstyle)				
+            email_img1     =   'http://cdn.is.bluefly.com/mgen/Bluefly/prodImage.ms?productCode={0}&width=140&height=182'.format(colorstyle)
+            email_img2     =   'http://cdn.is.bluefly.com/mgen/Bluefly/prodImage.ms?productCode={0}&width=200&height=250'.format(colorstyle)
 
-        edgecast_listurls.append(oldlistpg)
-        edgecast_listurls.append(newlistpg)
-        edgecast_listurls.append(pdpg)
-        edgecast_listurls.append(pmlistpg)
-        edgecast_listurls.append(pmeventimg)
-        edgecast_listurls.append(email_img1)
-        edgecast_listurls.append(email_img2)
+            edgecast_listurls.append(oldlistpg)
+            edgecast_listurls.append(newlistpg)
+            edgecast_listurls.append(pdpg)
+            edgecast_listurls.append(pmlistpg)
+            edgecast_listurls.append(pmeventimg)
+            edgecast_listurls.append(email_img1)
+            edgecast_listurls.append(email_img2)
+    except AttributeError:
+        pass
+
 
 colorstyles = list(set(sorted(colorstyles)))
 clrversions = query_version_number(colorstyles)
