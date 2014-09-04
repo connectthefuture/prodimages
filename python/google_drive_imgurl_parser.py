6c9a0e3e29ccb378aaf66bcc14aa9f203a086919
johnb <john.bragato@gmail.com>
2014-09-03 20:40:26 -0400

    parser google drive

--- python/google_drive_imgurl_parser.py
+++ python/google_drive_imgurl_parser.py
@@ -1,30 +1,5 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import re
-
-regex = re.compile(
-        r'^(?:http|ftp)s?://' # http:// or https://
-        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' #domain...
-        r'localhost|' #localhost...
-        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' # ...or ip
-        r'(?::\d+)?' # optional port
-        r'(?:/?|[/?]\S+)$', re.IGNORECASE)
-
-
-def get_image_urls_query(query):
-    result = connection.execute(query)
-    styles = {}
-    for row in result:
-        style_info = {}
-        style_info['colorstyle'] = row['bluefly_product_color']
-        style_info['alt'] = row['image_number']
-        style_info['url'] = row['url']
-        # Convert Colorstyle to string then set as KEY
-        styles[str(row['url'])] = style_info
-
-    connection.close()
-    return styles
-
 
 def request_status_code(url):
     import requests
@@ -33,7 +8,6 @@ def request_status_code(url):
         return r.status_code
     except requests.ConnectionError:
         print "failed to connect"
-
 
 
 def parse_html_for_urls(url):
@@ -48,19 +22,17 @@ def parse_html_for_urls(url):
 
     res = [ r for r in response_list if regex_hex.findall(r) ]
     ret = list(set(sorted(res)))
-    
     if len(ret) == 1:
         return ret
     else:
+        
         valid = [ r for r in ret if not renoequal.findall(r) ]
         imgurl = valid[0]
         if request_status_code(imgurl) == 200:
             return imgurl
         else:
             return '{} is not a valid URL'.format(imgurl)
-
-
-
+            
 def download_file(url, colorstyle=None, alt=None, ext='.jpg', destdir=None):
     import os, sys, requests
     if not destdir:
@@ -73,31 +45,19 @@ def download_file(url, colorstyle=None, alt=None, ext='.jpg', destdir=None):
         with open(destpath, 'ab+') as f:
             f.write(res.content)
             f.close()
-        return destpath
+        return destpath            
 
+def main(url):
+    result = parse_html_for_urls(url)
+    return result
 
-#for style in styless:
-#    url = style['image_url']
-#    alt = str(style['alt'])
-#    colorstyle = str(style['colorstyle'])
-#    res = download_file(url.strip('/edit?usp=sharing'), colorstyle=colorstyle, alt=alt, destdir=destdir)
+#    urlok='https://drive.google.com/file/d/0B4p-sxy24gtqOGlERmRkQmpoRkU/edit?usp=sharing'
+#    urlbad='https://drive.google.com/file/d/0B4p-sxy24gtqb3dLQjZzZUJqSmc/edit?usp=sharing'
+#    urlbad2='https://drive.google.com/file/d/0B4p-sxy24gtqWng3LTd4RXlxSXM/edit?usp=sharing'
+#    print parse_html_for_urls(urlok)
+#    print parse_html_for_urls(urlbad)
+#    print parse_html_for_urls(urlbad2)
 
-
-#import httplib
-#def get_status_code(host=None, path="/"):
-#    try:
-#        conn = httplib.HTTPConnection(host)
-#        conn.request("HEAD", path)
-#        return conn.getresponse().status
-#    except StandardError:
-#        return None
-#        
-#
-#def main(url_path=None):
-#    host = url_path.split('.com/')[0] + '.com'
-#    path = url_path.split('.com/')[-1] + '/'
-#    status_code = get_status_code(host=host, path=path)
-#    return status_code
 
 
 if __name__ == '__main__':
@@ -107,6 +67,5 @@ if __name__ == '__main__':
         status_code = request_status_code(url)
         print status_code
     except IndexError:
-        print 'You need to provide a URL as your first and only argument'
-    #except:
-    #    print 'Error occurred. Unable to get Http Code'
+        main()
+        #print 'You need to provide a URL as your first and only argument'
