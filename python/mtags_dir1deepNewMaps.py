400156980475bd9810324a6530dc5b6e181d05e6
johnb <john.bragato@gmail.com>
2014-10-19 23:45:35 -0400

    x

---
+++ python/mtags_dir1deepNewMaps.py
@@ -0,0 +1,248 @@
+#!/usr/bin/python
+
+# #!/bin/bash
+# 
+# . ~/.bash_profile
+# . ~/.bashrc
+# 
+# DATE=`date "+%Y-%m-%d"`
+# DAY=`date "+%Y-%m-%d-RetouchToDo"`
+# 
+# searchDir="$1"
+# 
+# stylePath=`find $searchDir -iname \*[^2-9][0-9,{8}]_[1-6].\*`
+# 
+# for f in $stylePath:
+# do
+# style=`basename $f | awk -F_ '{ print $1 }'`
+# paired=`echo "$style,$f"`
+# 
+# echo $paired >> $LIMBO/$DATE_tagpairs.csv
+# 
+# done
+
+
+"""
+Created on Fri Oct  18 14:48:56 2014
+
+@author: jb
+"""
+def sqlQueryMetatags(style,f):
+    import sqlalchemy
+    orcl_engine = sqlalchemy.create_engine('oracle+cx_oracle://jbragato:Blu3f!y@192.168.30.66:1531/dssprd1')
+    connection = orcl_engine.connect()
+    querymake_metatags="""SELECT DISTINCT
+      POMGR_SNP.PRODUCT_COLOR.ID                  AS colorstyle,
+      POMGR_SNP.BRAND.NAME                        AS brand,
+      POMGR_SNP.COLOR_GROUP.DESCRIPTION           AS color_group,
+      POMGR_SNP.PRODUCT_FOLDER_DENORMALIZED.LABEL AS category_parent,
+      POMGR_SNP.PRODUCT_FOLDER.LABEL              AS category_sub,
+      MAX(ATG_SNP.EVENT.ID)                       AS event_id,
+      ATG_SNP.EVENT.EVENT_DESCRIPTION             AS event_title,
+      POMGR_SNP.PRODUCT_FOLDER_DENORMALIZED.PATH  AS product_path,
+      ATG_SNP.EVENT.SHOT_LIST_DATE                AS shot_list_dt,
+      ATG_SNP.EVENT.BRAND_EDITORIAL               AS brand_editorial,
+      ATG_SNP.EVENT.CATEGORY                      AS cat_id,
+      POMGR_SNP.PRODUCT_COLOR.VENDOR_STYLE        AS vendor_style,
+      POMGR_SNP.LK_PRODUCT_STATUS.NAME            AS production_status
+    FROM
+      POMGR_SNP.PRODUCT_COLOR
+    LEFT JOIN ATG_SNP.EVENT_PRODUCT_COLOR
+    ON
+      POMGR_SNP.PRODUCT_COLOR.ID = ATG_SNP.EVENT_PRODUCT_COLOR.PRODUCT_COLOR_ID
+    LEFT JOIN POMGR_SNP.LK_PRODUCT_STATUS
+    ON
+      POMGR_SNP.PRODUCT_COLOR.PRODUCTION_STATUS_ID = POMGR_SNP.LK_PRODUCT_STATUS.ID
+    LEFT JOIN ATG_SNP.EVENT
+    ON
+      ATG_SNP.EVENT_PRODUCT_COLOR.EVENT_ID = ATG_SNP.EVENT.ID
+    LEFT JOIN POMGR_SNP.PRODUCT
+    ON
+      POMGR_SNP.PRODUCT_COLOR.PRODUCT_ID = POMGR_SNP.PRODUCT.ID
+    LEFT JOIN POMGR_SNP.PRODUCT_FOLDER
+    ON
+      POMGR_SNP.PRODUCT.PRODUCT_FOLDER_ID = POMGR_SNP.PRODUCT_FOLDER.ID
+    LEFT JOIN POMGR_SNP.BRAND
+    ON
+      POMGR_SNP.PRODUCT.BRAND_ID = POMGR_SNP.BRAND.ID
+    LEFT JOIN POMGR_SNP.PRODUCT_FOLDER_DENORMALIZED
+    ON
+      POMGR_SNP.PRODUCT_FOLDER.PARENT_PRODUCT_FOLDER_ID =
+      POMGR_SNP.PRODUCT_FOLDER_DENORMALIZED.ID
+    LEFT JOIN POMGR_SNP.COLOR_GROUP
+    ON
+      POMGR_SNP.PRODUCT_COLOR.COLOR_GROUP_ID = POMGR_SNP.COLOR_GROUP.ID
+    WHERE
+      POMGR_SNP.PRODUCT_COLOR.ID = COLORSTYLESEARCH
+    GROUP BY
+      POMGR_SNP.PRODUCT_COLOR.ID,
+      POMGR_SNP.BRAND.NAME,
+      POMGR_SNP.PRODUCT_FOLDER_DENORMALIZED.LABEL,
+      POMGR_SNP.PRODUCT_FOLDER.LABEL,
+      ATG_SNP.EVENT.EVENT_DESCRIPTION,
+      POMGR_SNP.COLOR_GROUP.DESCRIPTION,
+      POMGR_SNP.PRODUCT_FOLDER_DENORMALIZED.PATH,
+      POMGR_SNP.PRODUCT_COLOR.VENDOR_STYLE,
+      ATG_SNP.EVENT.SHOT_LIST_DATE,
+      ATG_SNP.EVENT.BRAND_EDITORIAL,
+      ATG_SNP.EVENT.CATEGORY,
+      POMGR_SNP.LK_PRODUCT_STATUS.NAME
+    ORDER BY
+      POMGR_SNP.PRODUCT_COLOR.ID DESC"""
+
+##   --POMGR_SNP.PRODUCT_COLOR.MODIFIED_DATE >= TRUNC(SysDate - 365)
+
+##   --RENAME INPUT VARIABLE PRIOR TO QUERY
+    querymake_metatags = querymake_metatags.replace('COLORSTYLESEARCH', str(style))
+    result = connection.execute(querymake_metatags)
+    
+
+    metatags = {}
+    for row in result:
+        metatag = {}
+#        metatag['colorstyle'] = row['colorstyle']
+#        metatag['IPTC:PONumber'] = row['po_num']
+        metatag['IPTC:VendorStyle'] = row['vendor_style']
+        metatag['IPTC:Brand'] = row['brand']
+        metatag['XMP:Genre'] = row['color_group']
+        metatag['IPTC:ProductType'] = row['category_sub']
+        metatag['EventID'] = row['event_id']
+        try:
+            metatag['XMP:Album'] = "EventID " + str(row['event_id'])
+        except:
+            pass
+        metatag['IPTC:Credit'] = row['product_path']
+        metatag['IPTC:CopyrightNotice'] = row['brand']
+        metatag['IPTC:SpecialInstructions'] = row['production_status']
+        metatag['Keywords'] = row['category_parent']
+        metatag['IPTC:Source'] = row['shot_list_dt']
+#        metatag['IPTC:SpecialInstructions'] = '{:%Y-%m-%d}'.format(metatag['brand_editorial'])
+#        metatag['IPTC:SampleStatusDate'] = '{:%Y-%m-%d}'.format(row['sample_dt'])
+#        metatag['IPTC:Source'] = '{:%Y-%m-%d}'.format(row['sample_dt'])
+#        metatag['IPTC:Source'] = row['sample_dt']
+#        metatag['SourceFile'] = f
+        ## file path as dict KEY
+        metatags[f] = metatag
+        ## colorstyle as dict KEY
+        #metatags[row['colorstyle']] = metatag
+
+    connection.close()
+    return metatags
+
+    
+
+def get_dbinfo_for_metatags_filelist(filelist):   
+    metafield_dict = {}
+    listed = []
+    for f in filelist:
+        stylefile = os.path.basename(f)
+        style = stylefile.split('_')[0].strip('.png').strip('.jpg')
+        #print style, f
+        ### string = key/val as k=filepath, val=all metadata as k/v pairs
+        exiftoolstring = sqlQueryMetatags(style,f)
+        #pairs = zip(exiftoolstring.values(), exiftoolstring.keys())
+    
+        for k,v in exiftoolstring.iteritems():
+            #full = tuple(value.values())
+            #full = value
+            tmpd = {}
+            for val in v:
+                #valpair = val,v[val]
+                #listed.append
+                tmpd[val] = v[val]
+                listed.append(tmpd)
+            metafield_dict[k] = tmpd
+
+    return metafield_dict
+    #return listed
+
+
+
+def main(directory=None):
+    #sqlQueryConsigRename(vnum, ponum)[vnum]    
+    import sys, os, glob, sqlalchemy
+    
+    if not directory:
+        glbdir = sys.argv[1]
+    else:
+        glbdir = directory
+    
+    #glbdir='/mnt/Post_Ready/aPhotoPush'
+    #glbdir = '/mnt/Post_Ready/zProd_Server/imageServer7/var/consignment/images_for_conversion/117257'
+    #globtoconvert = os.path.join('/mnt/Post_Ready/zProd_Server/imageServer7/var/consignment/images_for_conversion/117147', '*.jpg')
+    globtoconvert = glob.glob(os.path.join(os.path.realpath(glbdir), '*.??g'))
+    #print globtoconvert
+
+
+            #pairs = zip(exiftoolstring.values(), exiftoolstring.keys())
+        
+    #for f in globtoconvert:
+    #    metafield_dict = {}
+    #    style = stylefile.split('_')[0]
+    #    exiftoolstring = sqlQueryMetatags(style,f)
+    #    for k,v in exiftoolstring.items():
+    #        metatags = []
+    #        for val in v:
+    #            metapairs = val,v[val]
+    #            metapairs = tuple(metapairs)
+    #            metatags.append(metapairs)
+    #            #print metatags
+    #    metafield_dict[k] = metatags
+
+    metadict = get_dbinfo_for_metatags_filelist(globtoconvert)
+    exiftags = []
+    exifdict = {}
+    for k,v in metadict.items():
+        metatags = []
+
+        for val in v:
+            #m = []
+            filename = str(k)
+            exiftag = val
+            exifvalue = v[val]
+            #exifpart = str(' -' + "'" + str(exiftag) + "=" + str(exifvalue) + "'" + ''),
+            exifpart = "-'{exiftag}'='{exifvalue}'".format(exiftag=exiftag,exifvalue=exifvalue)
+            #print exifpart
+            #exifcmd = str('exiftool -' + "'" + str(exiftag) + "=" + str(exifvalue) + "'" + '')
+            #lines = str(exifcmd + " " + filename)
+            metatags.append(exifpart)
+            #print metatags
+            #m.append(exifpart)
+            #print val,v[val]
+        #exifdict[filename] = [x for x in metatags]
+        #metatags = (str(tag) for tag in metatags)
+        exifdict[filename] = " ".join(metatags)
+    #    for line in metatags:
+    #        print line[0]
+    #        exifcmd = str('exiftool -m -fast2 -q ' + line[0] + ' ' + filename)
+    #        exiftags.append(exifcmd)
+    execlist = []
+    for key,value in exifdict.iteritems():
+        execstring = "exiftool -m -overwrite_original_in_place -fast2 -q {0} {1}".format(value,key)
+        execlist.append(execstring)
+
+    def bashexec_subproc(cmdstring):
+        import subprocess
+        p = subprocess.Popen(cmdstring, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        for line in p.stdout.readlines():
+            print line,
+        retval = p.wait()
+        return retval
+
+    for line in execlist:
+        os.system(line)
+        print line
+
+#print execlist
+#print exifdict
+    
+
+#         
+#     
+#     except KeyError:
+#         continue
+#     except sqlalchemy.exc.DatabaseError:
+#         continue
+#         print "DBERR" + f
+
+
