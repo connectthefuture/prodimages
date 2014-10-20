29094c669984beca0f172647d62eccf2b70f5e77
johnb <john.bragato@gmail.com>
2014-10-19 23:20:53 -0400

    x

--- python/downloadfile_HTTPS_requests.py
+++ python/downloadfile_HTTPS_requests.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python
-import os, sys, requests
+def dloader(url,stylenum,imgnum):
+	import os, sys, requests, re
+	regex=re.compile(r'^(http://.*?)\?dl\=\d{1,2}.*?$')
+	urlget = url
+	localpath = os.path.join('/Users/johnb/Pictures', stylenum+ '_'+imgnum+'.jpg')
+	import subprocess
+	subprocess.call(['wget', url, '-o',localpath])
+	if regex.findall(urlget):
+		regexfnd=regex.findall(urlget)
+		print regexfnd 
+		res = requests.get(urlget, stream=True, timeout=5).content
+		with __builtin__.open(localpath, 'wb') as f:
+		    f.write(res.content)
+		    f.close()
+	else: pass
 
-urlget = sys.argv[1]
-localpath = sys.argv[2]
- 
-res = requests.get(urlget, stream=True, timeout=1)
-with open(localpath, 'ab+') as f:
-    f.write(res.content)
-    f.close()
