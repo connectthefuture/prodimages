e7e27d469518cabdbba2bec9942010a926762b37
johnb <john.bragato@gmail.com>
2014-10-22 17:05:00 -0400

    change the url from jpg standard

--- python/cronjob_marketplace_nightly_improcloadPatched.py
+++ python/cronjob_marketplace_nightly_improcloadPatched.py
@@ -101,7 +101,10 @@ for k,v in vaultstyles.iteritems():
     po_number   = v['po_number']
     vendor_name = v['vendor_name']
     alt_number  = v['alt']
-    ext = '.jpg'
+    if len(image_url.split('.')[-1]) == 3:
+        ext = image_url.split('.')[-1]
+    else: 
+        ext = '.jpg'
     if alt_number:
         bfly_ext = "_{0}{1}".format(alt_number,ext)
         ext = bfly_ext
