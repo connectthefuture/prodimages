bf76467a353e10aa5f3a0cf6879911fed606bb94
relic7 <relic7@users.noreply.github.com>
2014-11-06 03:34:32 -0500

    Update eventmail_sp.php

--- php/eventmail_sp.php
+++ php/eventmail_sp.php
@@ -10,7 +10,7 @@
 	if(isset($_POST['mailfrom'])) $mailfrom   		= $_POST['mailfrom'];
 	    else $mailfrom  =   "john.bragato@bluefly.com";
 	if(isset($_POST['mailto'])) $mailto   			= $_POST['mailto'];
-        else $mailto  =   "john.bragato@bluefly.com,stephen.parker@bluefly.com,jacqueline.wahba@bluefly.com,james.hoetker@bluefly.com,lydia.jeanlouis.temp@bluefly.com,conrad.sanderson@bluefly.com,steven.vaz@bluefly.com";
+        else $mailto  =   "john.bragato@bluefly.com,stephen.parker@bluefly.com,jacqueline.wahba@bluefly.com,james.hoetker@bluefly.com,lydia.jeanlouis.temp@bluefly.com,conrad.sanderson@bluefly.com";
 	if(isset($_POST['mailsubject'])) $mailsubject   = $_POST['mailsubject'];
         else $mailsubject  =   "Upcoming Event Status Report";
 	if(isset($_POST['mailbody'])) $mailbody   		= $_POST['mailbody'];
@@ -123,7 +123,7 @@
 	$smtp_params['port'] = '465';
 	$smtp_params['auth'] = true;
 	$smtp_params['username'] = 'john.bragato@bluefly.com';
-	$smtp_params['password'] = 'forty000one';
+	$smtp_params['password'] = '0zeroseven';
 	$smtp_params['persist'] = true;
 	
 	$mail =& Mail::factory('smtp', $smtp_params);
