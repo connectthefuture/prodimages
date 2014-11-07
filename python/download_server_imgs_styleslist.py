ff2a3934b47b4a40d78e932912a77326f947f315
johnb <john.bragato@gmail.com>
2014-11-07 15:12:09 -0500

    x

--- python/download_server_imgs_styleslist.py
+++ python/download_server_imgs_styleslist.py
@@ -32,7 +32,7 @@ def main(styleslist=None, root_dir=None):
     regex_r = re.compile(r'.*?\r.*?')
     regex_n = re.compile(r'.*?\n.*?')
     regex_Space = re.compile(r'.*?\n.*?')
-
+    print args
 
     # args1 = args[0].split('\n')   #(','.join(str(arg) for arg in args)).split('\n')
     # try:
