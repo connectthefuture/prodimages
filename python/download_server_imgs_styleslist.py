aa9e2b22e59ff1e978c8884440ba826afb3a05d4
johnb <john.bragato@gmail.com>
2014-11-08 19:27:54 -0500

    x

--- python/download_server_imgs_styleslist.py
+++ python/download_server_imgs_styleslist.py
@@ -87,7 +87,7 @@ def main(styleslist=None, root_dir=None):
         except IOError:
             pass   
 
-    perStyle = '' 
+    perStyle = 'NA' 
     # round((countAll)/(countOne),2)
     print '{0} Styles Found\n\t{1} Files Downloaded\n{2} Avg Files Per Style'.format(str(countOne), str(countAll), str(perStyle))
 
