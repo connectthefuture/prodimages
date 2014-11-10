23b16692b9c29667ca0f2f3bb17870e19cc17346
johnb <john.bragato@gmail.com>
2014-11-10 16:25:47 -0500

    x

--- python/identify_nonwhitebg.py
+++ python/identify_nonwhitebg.py
@@ -1,60 +1,65 @@
 #!/usr/bin/env python
-import os, sys, re, csv, glob, subprocess, shutil
+# -*- coding: utf-8 -*-
 
-filesdir = '/Users/johnb/GitHub-Primary'
-testdir  = '/Users/johnb/Dropbox/testimgs'
-tjpg = glob.glob(os.path.join(testdir,'*.jpg'))
-gjpg = glob.glob(os.path.join(filesdir,'*.jpg'))
-gpng = glob.glob(os.path.join(filesdir,'*.png'))
+def main(filesdir=None):
+    import os, sys, re, csv, glob, subprocess, shutil
+    if not filesdir:
+        filesdir = sys.argv[1]
+    gjpg = glob.glob(os.path.join(filesdir,'*.jpg'))
+    gpng = glob.glob(os.path.join(filesdir,'*.png'))
 
-pngout=os.path.join(filesdir,'pngout')
-jpgout=os.path.join(filesdir,'jpgout')
+    pngout=os.path.join(filesdir,'pngout')
+    jpgout=os.path.join(filesdir,'jpgout')
 
-try:
-    os.makedirs(pngout, 16877)
-except:
-    pass
-
-try:
-    os.makedirs(jpgout, 16877)
-except:
-    pass
-
-ongrey = []
-for f in gjpg:
     try:
-        # newf = f.replace('_l','_n')
-        cmd=['convert', f, '-virtual-pixel', 'edge', '-blur', '0x15', '-fuzz', '2%', '-bordercolor', 'white', '-border', '2x2', '-trim','-format', '%@', 'info:-']
-        ret = subprocess.check_output(cmd, stdin=None, stderr=subprocess.STDOUT, shell=False)
-        not_white = "400x480+0+0"
-        oldimg = '{}'.format(ret.split('__')[-1])
-
-        if str(oldimg) == str(not_white):
-            ongrey.append(f)
-            try:
-                shutil.move(f,jpgout)
-            except:
-                pass
+        os.makedirs(pngout, 16877)
     except:
         pass
-        
-print ongrey
 
-ongreypng = []
-for f in gpng:
     try:
-        # newf = f.replace('_l','_n')
-        cmd=['convert', f, '-virtual-pixel', 'edge', '-blur', '0x15', '-fuzz', '2%', '-bordercolor', 'white', '-border', '2x2', '-trim','-format', '%@', 'info:-']
-        ret = subprocess.check_output(cmd, stdin=None, stderr=subprocess.STDOUT, shell=False)
-        not_white = "400x480+0+0"
-        oldimg = '{}'.format(ret.split('__')[-1])
-
-        if str(oldimg) == str(not_white):
-            ongreypng.append(f)
-            try:
-                shutil.move(f,pngout)
-            except:
-                pass
+        os.makedirs(jpgout, 16877)
     except:
-         pass
-print ongreypng
+        pass
+
+    ongrey = []
+    for f in gjpg:
+        try:
+            # newf = f.replace('_l','_n')
+            cmd=['convert', f, '-virtual-pixel', 'edge', '-blur', '0x15', '-fuzz', '2%', '-bordercolor', 'white', '-border', '2x2', '-trim','-format', '%@', 'info:-']
+            ret = subprocess.check_output(cmd, stdin=None, stderr=subprocess.STDOUT, shell=False)
+            not_white = "400x480+0+0"
+            oldimg = '{}'.format(ret.split('__')[-1])
+
+            if str(oldimg) == str(not_white):
+                ongrey.append(f)
+                try:
+                    shutil.move(f,jpgout)
+                except:
+                    pass
+        except:
+            pass
+            
+    print ongrey
+
+    ongreypng = []
+    for f in gpng:
+        try:
+            # newf = f.replace('_l','_n')
+            cmd=['convert', f, '-virtual-pixel', 'edge', '-blur', '0x15', '-fuzz', '2%', '-bordercolor', 'white', '-border', '2x2', '-trim','-format', '%@', 'info:-']
+            ret = subprocess.check_output(cmd, stdin=None, stderr=subprocess.STDOUT, shell=False)
+            not_white = "400x480+0+0"
+            oldimg = '{}'.format(ret.split('__')[-1])
+
+            if str(oldimg) == str(not_white):
+                ongreypng.append(f)
+                try:
+                    shutil.move(f,pngout)
+                except:
+                    pass
+        except:
+             pass
+    print ongreypng
+
+
+if __name__ == '__main__':
+    main()
