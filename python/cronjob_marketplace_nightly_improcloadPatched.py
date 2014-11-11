7f0f09b6863bda3bfbc21418762da40e77aa5476
johnb <john.bragato@gmail.com>
2014-11-11 01:01:45 -0500

    x

--- python/cronjob_marketplace_nightly_improcloadPatched.py
+++ python/cronjob_marketplace_nightly_improcloadPatched.py
@@ -65,14 +65,13 @@ def sqlQuery_GetIMarketplaceImgs(vendor=None,vendor_brand=None, po_number=None, 
     return styles
 
 
-def post_or_put_style_to_api(update_styles, api_url=None, AuthToken=None):
+def post_or_put_style_to_api(colorstyle, api_url=None, AuthToken=None):
     import requests, json
     import http.client, urllib.parse
-    if not api_url:
-        api_url = 'http://prodimages.ny.bluefly.com/image-update/'
+    api_url = 'http://prodimages.ny.bluefly.com/image-update/'
     update_styles = list(set(sorted(update_styles)))
     for colorstyle in update_styles:
-        data = {'colorstyle': colorstyle, 'user': 'james:hoetker'}
+        data = {'colorstyle': colorstyle}
         #params = urllib.parse.urlencode(data)
         params = json.dumps(data)
         auth = {'Authorization': 'Token ' + AuthToken}
@@ -81,6 +80,7 @@ def post_or_put_style_to_api(update_styles, api_url=None, AuthToken=None):
         # conn = http.client.HTTPConnection(api_cache_clear, 80)
         # conn.request("PUT", "/", BODY)
         #response = conn.getresponse()
+        
         try:
             response = requests.post(api_cache_clear, params=params)
             print response.status, response.method, data
@@ -90,11 +90,12 @@ def post_or_put_style_to_api(update_styles, api_url=None, AuthToken=None):
                 response = requests.put(api_cache_clear, params=params)
                 print response.status, response.method, data
             except:
+                curlauth = 'Authorization: Token ' + AuthToken
+                curldata = 'colorstyle=' + colorstyle
                 try:
-                    subprocess.call([ 'curl', '-u', 'james:hoetker' '-d', params, '-H', headers, api_cache_clear])
-                    subprocess.call([ 'curl', '-u', 'james:hoetker', '-d', params, '-H', headers, '-X', 'PUT', api_cache_clear])
+                    subprocess.call([ 'curl', '-u', 'james:hoetker', '-d', curldata, '-H', curlauth, '-X', 'PUT', api_cache_clear])
                 except:
-                     subprocess.call([ 'curl', '-u', 'james:hoetker', '-d', params, '-H', headers, '-X', 'PUT', api_cache_clear])
+                     subprocess.call([ 'curl', '-u', 'james:hoetker' '-d', curldata, '-H', curlauth, api_cache_clear])
 
 ############################################################ RUN ##################################################
 ############################################################ RUN ##################################################
