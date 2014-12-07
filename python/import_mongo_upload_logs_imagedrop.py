1432b6aaf738053c2b0640a932bbb08e122406da
johnb <john.bragato@gmail.com>
2014-12-07 11:24:13 -0500

    x

--- python/import_mongo_upload_logs_imagedrop.py
+++ python/import_mongo_upload_logs_imagedrop.py
@@ -63,17 +63,22 @@ def update_filerecord_pymongo(database_name=None, collection_name=None, batchid=
     mongo = pymongo.Connection('127.0.0.1')
     mongo_db = mongo[database_name]
     mongo_collection = mongo_db[collection_name]
+
     key = {'colorstyle': colorstyle}
     data = { "$set":{'format': format,'batchid': batchid,'alt': alt,'timestamp': timestamp}},
+    test= mongo_collection.findone(key)
+    if not test:
     # Returns the '_id' key associated with the newly created document
-    new_insertobj_id = mongo_collection.update(key, data, {upsert=True})
-    #    new_insertobj_id = mongo_collection.insert({'colorstyle': colorstyle,'format': format,'batchid': batchid,'alt': alt,'timestamp': timestamp})
-    #new_insertobj_id = mongo_collection.insert({'colorstyle': colorstyle,'format': format,'batchid': batchid,'alt': alt,'timestamp': timestamp}, continue_on_error=True, upsert=True)
+        new_insertobj_id = mongo_collection.update(key, data, {upsert=True})
+        #    new_insertobj_id = mongo_collection.insert({'colorstyle': colorstyle,'format': format,'batchid': batchid,'alt': alt,'timestamp': timestamp})
+        #new_insertobj_id = mongo_collection.insert({'colorstyle': colorstyle,'format': format,'batchid': batchid,'alt': alt,'timestamp': timestamp}, continue_on_error=True, upsert=True)
 
 
-    print "Inserted: {0}\nImageNumber: {1}\nFormat: {2}\nID: {3}".format(colorstyle,alt, format,new_insertobj_id)
-    return new_insertobj_id
-
+        print "Inserted: {0}\nImageNumber: {1}\nFormat: {2}\nID: {3}".format(colorstyle,alt, format,new_insertobj_id)
+        return new_insertobj_id
+    else:
+        print key
+        return key
 
 def normalize_json_tounicode(input_data):
     from kitchen.text.converters import getwriter, to_unicode
