5eb9ddc3007ef8a9b246cbf4154c6a6fbffa49ee
johnb <john.bragato@gmail.com>
2014-12-07 16:07:55 -0500

    x

--- python/import_mongo_upload_logs_imagedrop.py
+++ python/import_mongo_upload_logs_imagedrop.py
@@ -69,8 +69,8 @@ def update_filerecord_pymongo(database_name=None, collection_name=None, batchid=
     #data = { "$set":{'format': format,'batchid': batchid,'alt': alt,'timestamp': timestamp}},
     #data = {'colorstyle': colorstyle, 'format': format,'batchid': batchid,'alt': alt,'timestamp': timestamp}
     data = { "$set":{'format': format,'batchid': batchid,'alt': alt,'timestamp': timestamp}}
-    mongo_collection.create_index([("colorstyle", pymongo.ASCENDING)], background=True)
-    #mongo_collection.create_index([("colorstyle", pymongo.ASCENDING),("alt", pymongo.ASCENDING)], background=True)
+    #mongo_collection.create_index([("colorstyle", pymongo.ASCENDING)], background=True)
+    mongo_collection.create_index([("colorstyle", pymongo.ASCENDING),("alt", pymongo.ASCENDING)], background=True)
     new_insertobj_id = mongo_collection.update(key, data, upsert=True, multi=True)
     print "Inserted: {0}\nImageNumber: {1}\nFormat: {2}\nID: {3}".format(colorstyle,alt, format,new_insertobj_id)
     return new_insertobj_id
