e2ccef012397e1865d15768c9ed89f27ac416a5e
johnb <jbragato@yahoo.com>
2014-12-09 14:45:59 -0500

    x

--- python/import_mongo_upload_logs_imagedrop.py
+++ python/import_mongo_upload_logs_imagedrop.py
@@ -154,7 +154,7 @@ def check_running_process(check_proc_regex=None, kill_found_procs=False):
             procname   = pdict['name']
             procuser   = pdict['username']
             procstatus = pdict['status']
-            if regex_proc_check.findall(procname) and procuser == 'root:
+            if regex_proc_check.findall(procname) and procuser == 'root':
                 print procname,  procuser, procstatus
                 found_conflicts_bypid.append(pdict['pid'])
             else:
