5d9f328a863d755fb8094713160229b77853bc83
johnb <john.bragato@gmail.com>
2014-12-08 21:25:01 -0500

    x

--- python/import_mongo_upload_logs_imagedrop.py
+++ python/import_mongo_upload_logs_imagedrop.py
@@ -108,112 +108,6 @@ def get_filerecord_pymongo(database_name=None, collection_name=None, batchid=Non
     return results
 
 
-def normalize_json_tounicode(input_data):
-    from kitchen.text.converters import getwriter, to_unicode
-    import json
-    from collections import defaultdict
-    from os import path
-    data = []
-    if type(input_data) == 'str':
-        if path.isfile(input_data):
-            jsondata = json.load(open(input_data))
-            print 'FILE'
-            for row in jsondata:
-                datarow = {}
-                for k,v in row.items():
-                    if type(v) == unicode:
-                        v = to_unicode(v)
-                    if type(k) == unicode:
-                        k = to_unicode(k)
-                    if type(v) == int:
-                        v = str(v)
-                        v = to_unicode(v)
-                    if type(k) == int:
-                        k = str(k)
-                        k = to_unicode(k)
-                    #print type(v), type(k)
-                    datarow[k] = v
-                #data[to_unicode(row[k])] = datarow
-                data.append(datarow)
-        else:
-            jsondata = json.dumps(input_data)
-            print 'STR'
-    else:
-        jsondata = input_data
-        datarow =  {} ##defaultdict(list)
-        for k,v in jsondata.iteritems():
-            if type(v) == unicode:
-                v = to_unicode(v)
-            if type(k) == unicode:
-                k = to_unicode(k)
-            if type(v) == int:
-                v = str(v)
-                v = to_unicode(v)
-            if type(k) == int:
-                k = str(k)
-                k = to_unicode(k)
-            #print type(v), type(k)
-            #datarow[k].append(v)
-            datarow[k] = v
-        #data[to_unicode(row[k])] = datarow
-        data.append(datarow)
-        print 'ELSE', type(input_data)
-    return data
-
-
-class MongodbClient:
-    import pymongo
-    #conn = pymongo.Connection('mongodb://tutorial-test:u3ZYh136@ds029187.mongolab.com:29187/tutorial-test')
-    def __init__(self):
-        #super(SuperClass, self).__init__()
-        self.hostname           = ''
-        self.database_name      = ''
-        self.db                 = ''
-        self.conn               = ''
-        self.collection_name    = ''
-        self.defaultHost        = True
-        self.alt_host_port      = ''
-        self.item               = ''
-
-        # Set Global Style number variable if available in env
-        try:
-            self.objects = data
-        except:
-            self.objects = ''
-            pass
-
-    def connect_to_mongodb(self):
-        if defaultHost:
-            self.conn = pymongo.Connection('mongodb://127.0.0.1:27017/' + database_name)
-        else:
-            self.conn = pymongo.Connection('mongodb://' + alt_host_port + '/' + database_name)
-        db = self.conn[database_name]
-        return self.db
-
-    def find_in_collection(self):
-        #db.collection_name.insert({})
-        db.collection_name.find()
-        return list(db.collection_name.find())
-
-    def save(self):
-        self.collection_name.insert(self.objects)
-
-    def fetch(self):
-        return self.collection_name.find_one({"id": self.item })
-
-
-class ImageDropMongodbClient(MongodbClient):
-    import pymongo
-    #conn = pymongo.Connection('mongodb://tutorial-test:u3ZYh136@ds029187.mongolab.com:29187/tutorial-test')
-    def __init__(self, **kargs): ##  data,
-        #super(ImageDropMongodbClient, self).__init__()
-        self.batchid   = self.item['batchid']
-        self.colorstyle = self.item['colorstyle']
-        self.alt        = self.item['alt']
-        self.format     = self.item['format']
-        self.timestamp  = self.item['timestamp']
-
-
 def main_check(datarow=None):
     import sys,os,re, sqlalchemy, json
     regex_uploadlogs = re.compile(r'^.*?/Post_Complete/ImageDrop/bkup/LSTransfer.+?\.[txtTXT]{3}$')
@@ -255,32 +149,34 @@ def main_update(dirname=None):
             dirname = '/mnt/Post_Complete/ImageDrop/bkup'
     ## Take the compiled k/v pairs and Format + Insert into Mongo DB
     transfer_batches = parse_upload_log_files_indir(dirname=dirname)
-    for batch in transfer_batches:
-        database_name = 'images'
-        collection_name = 'uploads_imagedrop'
-        for row in batch:
-            #print row
-            for k,v in row.items():
-                ## Build object of key/values for insert
-                batchid = row['batchid']
-                colorstyle = row['colorstyle']
-                alt = row['alt']
-                format = row['format']
-                timestamp = row['timestamp']
-                #print locals()
-                ## Perform the Insert to mongodb
-                #uploads_imagedrop.find({'colorstyle': colorstyle, 'app_config_id':{'$in':app_config_ids}})
-                #expr = { "$or": [ {"uploads_imagedrop": { "$exists": False }}, {"colorstyle": colorstyle}]}
+    try:
+        while transfer_batches.next():
+            database_name = 'images'
+            collection_name = 'uploads_imagedrop'
+            for row in batch:
+                #print row
+                for k,v in row.items():
+                    ## Build object of key/values for insert
+                    batchid = row['batchid']
+                    colorstyle = row['colorstyle']
+                    alt = row['alt']
+                    format = row['format']
+                    timestamp = row['timestamp']
+                    #print locals()
+                    ## Perform the Insert to mongodb
+                    #uploads_imagedrop.find({'colorstyle': colorstyle, 'app_config_id':{'$in':app_config_ids}})
+                    #expr = { "$or": [ {"uploads_imagedrop": { "$exists": False }}, {"colorstyle": colorstyle}]}
 
-                #for c in collection_name.find(expr):
-                #    print [ k.upper() for k in sorted(c.keys()) ]
-                if regex_valid_colorstyle_file.findall(row['filename']):
-                    ## inserts only, not updates, will create multiple records if exists already
-                    update_filerecord_pymongo(database_name=database_name, collection_name=collection_name, batchid=batchid, colorstyle=colorstyle, alt=alt, format=format, timestamp=timestamp)
-                    print "Successful Insert to uploads_imagedrop {0} --> {1}".format(k,v)
-                else:
-                    pass
-
+                    #for c in collection_name.find(expr):
+                    #    print [ k.upper() for k in sorted(c.keys()) ]
+                    if regex_valid_colorstyle_file.findall(row['filename']):
+                        ## inserts only, not updates, will create multiple records if exists already
+                        update_filerecord_pymongo(database_name=database_name, collection_name=collection_name, batchid=batchid, colorstyle=colorstyle, alt=alt, format=format, timestamp=timestamp)
+                        print "Successful Insert to uploads_imagedrop {0} --> {1}".format(k,v)
+                    else:
+                        pass
+    except StopIteration:
+        print "Successful Batch Update Completed uploads_imagedrop..."
 
 def main(dirname=None):
     import sys,os,re, sqlalchemy, json
