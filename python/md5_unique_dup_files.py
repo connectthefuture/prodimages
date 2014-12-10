7dbc378edca3dd4b4d9d01ec3183f1d6f6b01f7e
johnb <john.bragato@gmail.com>
2014-12-10 17:55:28 -0500

    md5 sum function fixed a bit

--- python/md5_unique_dup_files.py
+++ python/md5_unique_dup_files.py
@@ -3,24 +3,30 @@
 
 def find_duplicate_imgs(dname):
     import hashlib
-    import os
+    import os, __builtin__
     
     hash_table = {}
     dups = []
-
+    dname = os.path.abspath(dname)
+    os.chdir(os.path.abspath(dname))
+    #print os.listdir(dname)
     for img in os.listdir(dname):
-        #print img
+        print img
         img_path = os.path.join(dname, img)
-        _file = open(img_path, "rb")
-        content = _file.read()
-        _file.close()
-        md5 = hashlib.md5(content)
-        _hash = md5.hexdigest()
+        try:
+                
+            _file = __builtin__.open(img_path, "rb")
+            content = _file.read()
+            _file.close()
+            md5 = hashlib.md5(content)
+            _hash = md5.hexdigest()
 
-        if _hash in hash_table.keys():
-            dups.append(img)
-        else:
-            hash_table[_hash] = img
+            if _hash in hash_table.keys():
+                dups.append(img)
+            else:
+                hash_table[_hash] = img
+        except:
+            pass
     return hash_table, dups
             
 ############################################
@@ -29,12 +35,12 @@ def find_duplicate_imgs(dname):
 
 import os,sys
 
-dname = sys.argv[1]
+dname = '/Users/johnb/Desktop/clinton_103114' #sys.argv[1]
 def main():
-    unique_files, duplicates = find_duplicate_imgs(dname)
-    unique_files = unique_files.values()
-    return unique_files, duplicates 
+    md5checksum_pairs, duplicates = find_duplicate_imgs(dname)
+    unique_files = md5checksum_pairs.values()
+    return unique_files, duplicates, md5checksum_pairs
 
 if __name__ == '__main__':
-    result = main()
-    print result
+    unique_files, duplicates, md5checksum_pairs = main()
+    print unique_files, duplicates, md5checksum_pairs
