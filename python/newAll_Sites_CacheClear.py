6f2b28e09abad0738a7db773a7a75c7920aafc53
johnb <john.bragato@gmail.com>
2014-12-12 14:33:19 -0500

    x

--- python/newAll_Sites_CacheClear.py
+++ python/newAll_Sites_CacheClear.py
@@ -176,7 +176,7 @@ def main(colorstyle_list=None):
 
     for colorstyle in colorstyle_list:
         bflypdp_url = "http://www.bluefly.com/Bluefly-generic-pdp-slug/p/{0}/detail.fly".format(colorstyle)
-        found_links = ''#url_get_links(bflypdp_url)
+        found_links = url_get_links(bflypdp_url)
         version =  query_version_number(colorstyle)[colorstyle]['version']
         ## static standard urls
         oldlistpg    = 'http://cdn.is.bluefly.com/mgen/Bluefly/prodImage.ms?productCode={0}&width=157&height=188'.format(colorstyle)
