53f47e40ae9ebf68cc3743bc0a7f34e454ebba94
johnb <john.bragato@gmail.com>
2014-12-13 23:59:43 -0500

    gitpull mongo app

--- bash/gitpulldamnation.sh
+++ bash/gitpulldamnation.sh
@@ -2,25 +2,25 @@
 
 . ~/.bash_profile
 
-PWD=/home/johnb/virtualenvs/DAMNATION/src
+# PWD=/home/imageman/virtualenvs/DAMNATION/src
 
-. $PWD/../bin/activate
+# . "$PWD"/../bin/activate
 
-echo `date` >> ~/.damnationgit.log
+# echo `date` >> ~/.DAMNATIONgit.log
 
 
-cd /home/johnb/virtualenvs/DAMNATION/src && git pull origin master >> ~/.damnationgit.log
+# cd /home/imageman/virtualenvs/DAMNATION/src && git pull origin master >> ~/.DAMNATIONgit.log
 
-sleep 7
+# sleep 7
 
-test=`tail -1 ~/.damnationgit.log`
+# test=`tail -1 ~/.DAMNATIONgit.log`
 
-if [ "$test" == "Already up-to-date." ]; then
-echo "Nothing to Do"
-else
-# echo "ELSE"
-cd /home/johnb/virtualenvs/DAMNATION/src && source ../bin/activate ## && python $PWD/manage.py syncdb --migrate >> ~/.damnation.log
-fi;
+# if [ "$test" == "Already up-to-date." ]; then
+# echo "Nothing to Do"
+# else
+# # echo "ELSE"
+# cd /home/imageman/virtualenvs/DAMNATION/src && source ../bin/activate ## && python $PWD/manage.py syncdb --migrate >> ~/.mongodj.log
+# fi;
 
 
 
