f79054ed7e440755785576f13e8fadadba3903d8
johnb <jbragato@yahoo.com>
2014-12-18 21:36:52 -0500

    x

--- python/mongo_gridfs_insert_file.py
+++ python/mongo_gridfs_insert_file.py
@@ -13,11 +13,10 @@ def connect_gridfs_mongodb(db_name='None'):
 
 
 def insert_file_gridfs_file7(filepath=None, metadata=None, db_name=None):
+    import os
     db, fs = connect_gridfs_mongodb(db_name=db_name)
-    print type(filepath)
-    print dir(filepath)
     try:
-        filename = filepath.spilt('/')[-1]
+        filename = os.path.basename(filepath)
         content_type= 'image/' + filename.split('.')[-1]
         #content-type=content_type
         with fs.new_file(filename=filename, metadata=metadata) as fp:
