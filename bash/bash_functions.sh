f4af03cddca7bb8999ecd499181c3fa2364704bd
johnb <jbragato@yahoo.com>
2015-02-09 16:21:56 -0500

    x

--- bash/bash_functions.sh
+++ bash/bash_functions.sh
@@ -379,7 +379,7 @@ function cache_clear_dir ()
 
 function cache_clear_dir_postapi ()
 {
-    for f in $(find "$1" -maxdepth 4 -iname \*.jpg -exec basename {} \;| cut -c 1-9 | sort -nru); do 
+    for f in $(find "$1" -maxdepth 1 -iname \*.jpg -exec basename {} \;| cut -c 1-9 | sort -nru); do 
     curl -u stephen:parker -d colorstyle="${f}" -X POST http://prodimages.ny.bluefly.com/image-update/
     #curl -u stephen:parker -d colorstyle="f" -X PUT http://prodimages.ny.bluefly.com/image-update/
     #/usr/local/batchRunScripts/python/newAll_Sites_CacheClear.py "$f"; 
