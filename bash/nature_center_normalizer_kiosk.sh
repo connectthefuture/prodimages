c3dcc8efdc17c7eeb54b400e37fee664aba85ec8
johnb <jbragato@yahoo.com>
2015-03-03 06:13:28 -0500

    x

--- bash/nature_center_normalizer_kiosk.sh
+++ bash/nature_center_normalizer_kiosk.sh
@@ -6,7 +6,22 @@ export PWD=/mnt/Post_Complete/Complete_to_Load/nature_center
 echo "`date`_`pwd`" >> ~/.naturecenter.log
 
 
-regex_matcherator_matched=`python py-scriptReturning-Styles.py`
+regex_matcherator_matched=`python -c '''
+def main(dname=None):
+    import os.path
+    import glob
+    dname = os.path.abspath(dname)
+    destdir = os.path.abspath(os.path.join(dname, '../output'))
+    fpaths = glob.glob(os.path.join(dname, '*/*.*'))
+    ##destdir, dname
+    for fpath in fpaths:
+        ##retoutput = run_imgconv_function(fpath,destdir)
+        scriptname = os.path.dirname(fpath).split('/')[-1]
+        dest = os.path.join(destdir, scriptname)
+        print fpath, dest ;
+
+main(dname="\$f");'''`
+
 
 if [ "$regex_matcherator_matched" == "" ]; then
 echo "NothingToDo `date`_`pwd`" > ~/.naturecenter.log
