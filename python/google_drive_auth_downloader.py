03e73e9f8151c665b8f4d3d729c5dada96762350
johnb <jbragato@yahoo.com>
2015-03-20 18:50:45 -0400

    useragent

--- python/google_drive_auth_downloader.py
+++ python/google_drive_auth_downloader.py
@@ -3,97 +3,97 @@
 
 #import sys
 #sys.argv[1:] = ''
-def create_googleDriveapi_service2(serviceName=None, version=None, client_id=None,client_secret=None,redirect_uri=None, scope=None):
-    import httplib2
-    from oauth2client.file import Storage
-    from oauth2client.client import OAuth2WebServerFlow
-    from oauth2client import tools
-    import os, datetime, argparse, apiclient
-    from apiclient import http, errors
-    # if serviceName == 'drive':
-    #     print serviceName
-    #     client_id = '390426411557-fsk0n5k1g5fnj1gs1te2f19kq5vfftgk.apps.googleusercontent.com'
-    # ##########################Vars
-    # elif serviceName == 'calendar':
-    #     print serviceName
-    #     client_id = '924881045523-kc7leju7role0too3k4itlo864eprl1u.apps.googleusercontent.com'
+# def create_googleDriveapi_service2(serviceName=None, version=None, client_id=None,client_secret=None,redirect_uri=None, scope=None):
+#     import httplib2
+#     from oauth2client.file import Storage
+#     from oauth2client.client import OAuth2WebServerFlow
+#     from oauth2client import tools
+#     import os, datetime, argparse, apiclient
+#     from apiclient import http, errors
+#     # if serviceName == 'drive':
+#     #     print serviceName
+#     #     client_id = '390426411557-fsk0n5k1g5fnj1gs1te2f19kq5vfftgk.apps.googleusercontent.com'
+#     # ##########################Vars
+#     # elif serviceName == 'calendar':
+#     #     print serviceName
+#     #     client_id = '924881045523-kc7leju7role0too3k4itlo864eprl1u.apps.googleusercontent.com'
     
-    client_id = client_id
-    client_secret= client_secret  #'rqZxYuy0Cht37rJ0GSZ05YoY'
-    scope =  scope
-    user_agent = 'User-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.10; en-US; rv:33.0) Gecko/20100101 Firefox/33.0' ##'Python2.7'
-    BROWSERdeveloperKey='AIzaSyBHozNPRDnVkdPo_JlP_4TLbNrJIsd3bQ4'
-    SERVERdeveloperKey='AIzaSyDe68JsIJK5O5Cqd-tAVGqaSeHqcFCNPh8'
-    ##
-    # if not serviceName:
-    #     serviceName = 'calendar'
-    # if not version:
-    #     version = 'v3'
-    ###########
-    ###########
-    # The client_id and client_secret are copied from the API Access tab on
-    # the Google APIs Console
-    FLOW = OAuth2WebServerFlow(
-        client_id=client_id,
-        client_secret=client_secret,
-        scope=scope,
-        redirect_uri=redirect_uri,
-        user_agent=user_agent,)
-    #        approval_prompt='force')
+#     client_id = client_id
+#     client_secret= client_secret  #'rqZxYuy0Cht37rJ0GSZ05YoY'
+#     scope =  scope
+#     user_agent =  'Python2.7'
+#     BROWSERdeveloperKey='AIzaSyBHozNPRDnVkdPo_JlP_4TLbNrJIsd3bQ4'
+#     SERVERdeveloperKey='AIzaSyDe68JsIJK5O5Cqd-tAVGqaSeHqcFCNPh8'
+#     ##
+#     # if not serviceName:
+#     #     serviceName = 'calendar'
+#     # if not version:
+#     #     version = 'v3'
+#     ###########
+#     ###########
+#     # The client_id and client_secret are copied from the API Access tab on
+#     # the Google APIs Console
+#     FLOW = OAuth2WebServerFlow(
+#         client_id=client_id,
+#         client_secret=client_secret,
+#         scope=scope,
+#         redirect_uri=redirect_uri,
+#         user_agent=user_agent,)
+#     #        approval_prompt='force')
 
-    # If the Credentials don't exist or are invalid, run through the native client
-    # flow. The Storage object will ensure that if successful the good
-    # Credentials will get written back to a file.
-    py_dir = os.path.dirname(os.path.realpath(__file__))
-    #py_dir = os.path.dirname(os.path.realpath(os.curdir))
-    print py_dir
-    os.chdir(py_dir)
+#     # If the Credentials don't exist or are invalid, run through the native client
+#     # flow. The Storage object will ensure that if successful the good
+#     # Credentials will get written back to a file.
+#     py_dir = os.path.dirname(os.path.realpath(__file__))
+#     #py_dir = os.path.dirname(os.path.realpath(os.curdir))
+#     print py_dir
+#     os.chdir(py_dir)
     
-    #storage_file = os.path.join(os.path.dirname(py_dir), 'calendar.dat')
-    storage_file = os.path.join(py_dir, serviceName + '.dat')
-    STORAGE = Storage(storage_file)
+#     #storage_file = os.path.join(os.path.dirname(py_dir), 'calendar.dat')
+#     storage_file = os.path.join(py_dir, serviceName + '.dat')
+#     STORAGE = Storage(storage_file)
 
-    # To disable the local server feature, replace with '' in the following line:
-    args = '' #'--noauth_local_webserver'
-    parser = argparse.ArgumentParser(parents=[tools.argparser])
-    FLAGS = parser.parse_args()
-    ##
-    #
-    credentials = STORAGE.get()
-    if credentials is None or credentials.invalid == True:
-        authorize_url = FLOW.step1_get_authorize_url()
-        print 'Go to the following link in your browser: ' + authorize_url
-        code = raw_input('Enter verification code: ').strip()
-        credentials = FLOW.step2_exchange(code)
-        STORAGE.put(credentials)
-        print credentials, STORAGE, storage_file
-        #credentials = tools.run_flow(FLOW, STORAGE, FLAGS)
-    ##
-    # Create an httplib2.Http object to handle our HTTP requests and authorize it
-    # with our good Credentials.
-    http = httplib2.Http()
-    http = credentials.authorize(http)
+#     # To disable the local server feature, replace with '' in the following line:
+#     args = '' #'--noauth_local_webserver'
+#     parser = argparse.ArgumentParser(parents=[tools.argparser])
+#     FLAGS = parser.parse_args()
+#     ##
+#     #
+#     credentials = STORAGE.get()
+#     if credentials is None or credentials.invalid == True:
+#         authorize_url = FLOW.step1_get_authorize_url()
+#         print 'Go to the following link in your browser: ' + authorize_url
+#         code = raw_input('Enter verification code: ').strip()
+#         credentials = FLOW.step2_exchange(code)
+#         STORAGE.put(credentials)
+#         print credentials, STORAGE, storage_file
+#         #credentials = tools.run_flow(FLOW, STORAGE, FLAGS)
+#     ##
+#     # Create an httplib2.Http object to handle our HTTP requests and authorize it
+#     # with our good Credentials.
+#     http = httplib2.Http()
+#     http = credentials.authorize(http)
 
-    from apiclient.discovery import build
-    #client = apiclient.APIClient()
-    # Build a service object for interacting with the API.
-    service = build(serviceName=serviceName, version=version, http=http)
-    print STORAGE._filename
-    return service
+#     from apiclient.discovery import build
+#     #client = apiclient.APIClient()
+#     # Build a service object for interacting with the API.
+#     service = build(serviceName=serviceName, version=version, http=http)
+#     print STORAGE._filename
+#     return service
 
 
-def instantiate_google_drive_service2():
-    #import apiclient, sys
-    #from googleapi_service import create_googleapi_service
-    serviceName = 'drive'
-    version = 'v2'
-    client_secret = 'dccI63nfXqddT5BZcjcs67lj'
-    client_id = '355881409068-167vm2c1oqjkmdmb2kulaugu63ehgcim.apps.googleusercontent.com'
-    scope = 'https://www.googleapis.com/auth/drive'
-    # drive_file = drive_file_instance
-    redirect_uri = 'urn:ietf:wg:oauth:2.0:oob'
-    service = create_googleDriveapi_service(serviceName=serviceName, version=version, client_secret=client_secret, client_id=client_id, redirect_uri=redirect_uri, scope=scope)
-    return service
+# def instantiate_google_drive_service2():
+#     #import apiclient, sys
+#     #from googleapi_service import create_googleapi_service
+#     serviceName = 'drive'
+#     version = 'v2'
+#     client_secret = 'dccI63nfXqddT5BZcjcs67lj'
+#     client_id = '355881409068-167vm2c1oqjkmdmb2kulaugu63ehgcim.apps.googleusercontent.com'
+#     scope = 'https://www.googleapis.com/auth/drive'
+#     # drive_file = drive_file_instance
+#     redirect_uri = 'urn:ietf:wg:oauth:2.0:oob'
+#     service = create_googleDriveapi_service(serviceName=serviceName, version=version, client_secret=client_secret, client_id=client_id, redirect_uri=redirect_uri, scope=scope)
+#     return service
 
 
 def download_google_drive_file(service=None, image_url=None, destpath=None):
@@ -133,14 +133,6 @@ if __name__ == '__main__':
         image_url = sys.argv[1]
         destpath  = sys.argv[2]
         res = download_google_drive_file(image_url=image_url, destpath=destpath)
-    except:
-        print 'Failed, please supply bot the image_url and destpath args as sys.argv[1] and [2], respectively'
+    except IndexError:
+        print 'Failed, please supply both the image_url and destpath args as sys.argv[1] and [2], respectively'
 
-    print res
-
-
-
-#image_url = 'https://drive.google.com/open?id=0B6gg_FhatSi8ak03dFEwZzBOVGM&authuser=0'
-#destpath  = '~/MARKETPLACE/HidesignAmerica/154530/357944502_3.jpg'
-#res = download_google_drive_file(image_url=image_url, destpath=destpath)
-#print res
