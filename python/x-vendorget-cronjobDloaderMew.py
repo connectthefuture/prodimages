ba20494de64373648c78bb51b280c3ec1f2b54e7
johnb <jbragato@yahoo.com>
2015-03-21 17:05:31 -0400

    x

--- python/x-vendorget-cronjobDloaderMew.py
+++ python/x-vendorget-cronjobDloaderMew.py
@@ -244,7 +244,7 @@ def main(vendor=None, dest_root=None, ALL=None):
 	## Process the files running each brand in a separate parallel process
 	## TODO: Make possible to include all the urls in 1 queue and send/add to and upload queue
 	import multiprocmagick
-	multiprocmagick.run_multiproccesses_magick(searchdir=imagedir)
+	multiprocmagick.run_multiproccesses_magick(searchdir=dest_root)
 	print 'Done With multiprocmagick'
 
 if __name__ == '__main__':
