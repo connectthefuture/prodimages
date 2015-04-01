c02282518bf194d33774b328414aba66a0fd277f
johnb <john.bragato@gmail.com>
2015-04-01 18:24:14 -0400

    x

--- python/jbmodules/image_processing/marketplace/multiprocmagick.py
+++ python/jbmodules/image_processing/marketplace/multiprocmagick.py
@@ -41,10 +41,10 @@ class Task(object):
         #time.sleep(0.1) # pretend to take some time to do the work
         import jbmodules.image_processing.magick_tweaks.convert_img_srgb
         jbmodules.image_processing.magick_tweaks.convert_img_srgb.main(image_file=self.img)
-        # except AttributeError:
-        #     print self.img, ' <-- AttributeError'
+        except AttributeError:
+            print self.img, ' <-- AttributeError'
         #     convert_img_srgb.main(image_file=self.img)
-        #     pass
+            pass
         print self.img, ' <-- self.img ', self.rgbmean
         pngout = magickProc2.subproc_magick_png(self.img, rgbmean=dict(self.rgbmean), destdir=self.destdir)
         magickProc2.subproc_magick_large_jpg(pngout, destdir=self.destdir)
