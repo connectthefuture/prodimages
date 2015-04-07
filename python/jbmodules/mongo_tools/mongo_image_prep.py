f816a8cf6f55b7cee275100ef9144435da9da35a
johnb <jbragato@yahoo.com>
2015-04-07 00:21:57 -0400

    x

--- python/jbmodules/mongo_tools/mongo_image_prep.py
+++ python/jbmodules/mongo_tools/mongo_image_prep.py
@@ -132,13 +132,13 @@ def update_filerecord_pymongo(db_name=None, collection_name=None, filename=None,
         data = { "$set":{'format': format, 'metadata': metadata, 'alt': alt, 'upload_ct': 1,'timestamp': timestamp}}
         # mongo_collection.create_index([("colorstyle", pymongo.ASCENDING)], unique=True, sparse=True, background=True)
     try:
-        mongo_collection.create_index("md5", unique=True, sparse=False, background=True)
+        mongo_collection.create_index(key_str, unique=False, sparse=False, background=True)
     except pymongo.errors.DuplicateKeyError:
         print ' DuplicateKey Error', key_str
         pass
     # mongo_collection.create_index([("colorstyle", pymongo.ASCENDING),("alt", pymongo.DECENDING)], background=True)
 
-    upsertobjid = mongo_collection.update(key, data, upsert=True, multi=True)
+    upsertobjid = mongo_collection.update(key, data, upsert=True, multi=True, safe=True)
     print "Inserted: {0}\nImageNumber: {1}\nFormat: {2}\nID: {3}\nCheck: {4}".format(colorstyle,alt, format,upsertobjid, check)
     return check, upsertobjid
 
