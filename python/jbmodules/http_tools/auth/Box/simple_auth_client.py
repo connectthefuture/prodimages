ed123efbd9e2fdb1891bb408dd3e128b406614fb
johnb <jbragato@yahoo.com>
2015-04-14 04:02:59 -0400

    x

--- python/jbmodules/http_tools/auth/Box/simple_auth_client.py
+++ python/jbmodules/http_tools/auth/Box/simple_auth_client.py
@@ -14,12 +14,16 @@ def store_tokens(access_token, refresh_token):
 def authorize_client(**kwargs):
     from boxsdk import OAuth2
     oauth = OAuth2(
-        client_secret=kwargs.get('client_secret'),
+        client_secret= kwargs.get('client_secret'),
         client_id = kwargs.get('client_id'),
-        store_tokens=store_tokens,
+        access_token= kwargs.get('access_token') #DTPwrgGkhZkxKvTWVTT4KBwLwr6ELPvj'
+        #store_tokens=store_tokens,
     )
-    auth_url, csrf_token = oauth.get_authorization_url(kwargs.get('redirect_uri'))
-    return auth_url, csrf_token
+    
+#    auth_url, csrf_token = oauth.get_authorization_url(kwargs.get('redirect_uri'))
+#    print auth_url
+#    access_token, refresh_token = oauth.authenticate('DTPwrgGkhZkxKvTWVTT4KBwLwr6ELPvj')
+    return oauth #auth_url, csrf_token
 
 
 def instantiate_boxapi_client():
@@ -27,8 +31,9 @@ def instantiate_boxapi_client():
     client_secret = 'g4R1o909fgf1PSsa5mLMDslpAwcbfIQl'
     client_id = 'bxccmj5xnkngs8mggxv5ev49zuh80xs9'
     #scope = 'https://app.box.com/services/auth_download_client'
+    access_token = 'DTPwrgGkhZkxKvTWVTT4KBwLwr6ELPvj' 
     redirect_uri = 'http://localhost' #'urn:ietf:wg:oauth:2.0:oob'
-    oauth  = authorize_client(client_secret=client_secret, client_id=client_id,redirect_uri=redirect_uri)
+    oauth  = authorize_client(client_secret=client_secret, client_id=client_id,access_token=access_token,redirect_uri=redirect_uri)
     client = Client(oauth)
     return client
 
@@ -68,12 +73,20 @@ def download_boxapi_auth_file(client=None, image_url=None, destpath=None, **kwar
         
         
 
-url = 'https://capturemrg.box.com/s/cy5fuoata8eiq5n53i5gvwphqfvb061x'
+url1 = 'https://capturemrg.box.com/s/cy5fuoata8eiq5n53i5gvwphqfvb061x'
+url2 = 'https://relic7.box.com/s/amf29gle65fs87a9ulayi66cacb16aby'
 
-file_id = regex_boxapi_ret_fileid(url)
+import requests
 
-if file_id:
-    print file_id
-client = instantiate_boxapi_client()
+res = requests.get(url2)
+print dir(res)
+print res
 
-print client.
+print requests.head(url2).raw
+#file_id = regex_boxapi_ret_fileid(url)
+#
+#if file_id:
+#    print file_id
+#client = instantiate_boxapi_client()
+#
+#print client.file(file_id.values()[0]).content()
