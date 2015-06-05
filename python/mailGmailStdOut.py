5f1b8a788d73b29ceb3cb6e3842da8f950187f5a
johnb <jbragato@yahoo.com>
2015-06-05 19:34:58 -0400

    x

--- python/mailGmailStdOut.py
+++ python/mailGmailStdOut.py
@@ -5,26 +5,26 @@
 
 def send_text_via_gmail(toaddr, text=None):
     import smtplib, os.path, email
-    from email.MIMEMultipart import MIMEMultipart
+
     from email.MIMEBase import MIMEBase
-    from email.MIMEText import MIMEText
+    from email.mime.text import MIMEText
     from email import Encoders
     #print('Enter username or return to accept the default: \n')
     gmail_user = str(input('Enter your Gmail or GoogleApps Address in single quotes: '))
     gmail_pass = str(input('Enter your password in single quotes: '))
-    msg = MIMEMultipart()
+    msg = MIMEText(text)
     msg['From']    = gmail_user
     msg['To']      = toaddr
-    msg['Subject'] = str(len(text.split('\n'))), ' Records Included'
+    msg['Subject'] = str(len(text.splitlines())) + ' Lines Included'
 
-    msg.attach(MIMEText(text))
 
     mailServer = smtplib.SMTP("smtp.gmail.com", 587)
     #mailServer.ehlo()
     mailServer.starttls()
     #mailServer.ehlo()
     mailServer.login(gmail_user, gmail_pass)
-    mailServer.sendmail(gmail_user, toaddr, msg)
+    print(type(msg), type( msg.as_string()))
+    mailServer.sendmail(gmail_user, toaddr, msg.as_string())
     mailServer.quit()
     print(' Mail has been sent to %s' % toaddr)    
 
@@ -32,13 +32,13 @@ def send_text_via_gmail(toaddr, text=None):
 if __name__ == '__main__':
     import sys
     try:
-        toaddr = sys.argv[1]
+        toaddr = str(sys.argv[1])
         if len(toaddr.split('@')) == 2:
-            content = sys.argv[2]
+            content = str(sys.argv[2])
         else:
             content = toaddr
             toaddr  = 'john.bragato@gmail.com'
-        send_text_via_gmail(toaddr, text=str(content).splitlines())
+        send_text_via_gmail(toaddr, text=content)
     except IndexError:
         print('Please supply the to address and email content as arg 1 and 2, respectively.')
 
