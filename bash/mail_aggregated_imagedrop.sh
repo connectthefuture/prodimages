84de947a936ab12a60dcbab701d7afd815bfd455
johnb <john.bragato@gmail.com>
2015-06-08 09:56:31 -0400

    x

--- bash/mail_aggregated_imagedrop.sh
+++ bash/mail_aggregated_imagedrop.sh
@@ -4,7 +4,7 @@
 
 
 
-fname=$(find /mnt/Post_Complete/ImageDrop/bkup/*LSTransfer* -type f -cmin -5 -exec ls -cltrs {} \;| awk '{ print $NF }' | tail -1)
+fname=$(find /mnt/Post_Complete/ImageDrop/bkup/*LSTransfer* -type f -mmin -20 -exec ls -cltrs {} \;| awk '{ print $NF }' | tail -1)
 
 allfiles=`cat "$fname" | grep \.png | wc -l`
 primaryonly=`cat "$fname" | grep \.png | wc -l`
