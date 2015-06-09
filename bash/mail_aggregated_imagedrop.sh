2f887059e8fd6d6d651e03d49a416f9c34c95d1f
johnb <jbragato@yahoo.com>
2015-06-09 00:40:55 -0400

    x

--- bash/mail_aggregated_imagedrop.sh
+++ bash/mail_aggregated_imagedrop.sh
@@ -14,11 +14,21 @@ process_time=`ls -cltrs "$fname" | awk '{print $7,$8,$9}'`
 main_styles_list=`cat "$fname" | grep \_m.jpg | awk '{ print $NF }' | cut -c1-9 | sort -run` 
 alt_styles_list=`cat "$fname" | grep \_alt0?.*ng | awk '{ print $NF }' | cut -c1-9 | sort -run` 
 
+main_query_list=$(for X in "${main_styles_list}"; do echo "${X}";done)
+alt_query_list=$(for X in "${alt_styles_list}"; do echo "${X}";done)
+
+
 echo  "${allfiles} files - ${primaryonly} Styles at ${process_time} ${altonly} --- ${main_styles_list} - ${alt_styles_list}"
 
+aggregates="PLACE_HOLDER"
+
 subject=$(echo "Most_Recent_Upload: ${allfiles} files - ${primaryonly} Styles at ${process_time}")
-content=`echo "Total Styles: ${allfiles} Main Images Total: ${primaryonly} Total Alts: ${altonly} $(for X in ${alt_styles_list}; do echo \"${X}\";done) -- $(for X in ${main_styles_list};do echo \"${X}\"; done)"`
+content=`echo "Total Styles: ${allfiles} Main Images Total: ${primaryonly} Total Alts: ${altonly} -- Results --> ${aggregates}"`
+
+# $(for X in ${alt_styles_list}; do echo \"${X}\";done) -- $(for X in ${main_styles_list};do echo \"${X}\"; done)"`
 #content=`echo "<html><body><table><tr>Total Styles: ${allfiles} </tr><tr>Main Images Total: ${primaryonly} </tr><tr>Total Alts: ${altonly}</tr></table><table> $(for X in ${alt_styles_list}; do echo "<tr>${X}</tr>";done)</table><table> $(for X in ${main_styles_list};do echo "<tr>${X}</tr>"; done) </table></body></html>"`
+
+
 
 
 /usr/local/batchRunScripts/python/mailGmailStdOut.py "${content}" "${subject}"
