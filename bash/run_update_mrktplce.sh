62e4d601d748b9842e0f50d2074a81708ed9e6ce
johnb <jbragato@yahoo.com>
2015-06-11 18:24:26 -0400

    x

--- bash/run_update_mrktplce.sh
+++ bash/run_update_mrktplce.sh
@@ -5,13 +5,14 @@
 
 query_marketplace_inprog="/usr/local/batchRunScripts/sql/marketplace_update_filter.sql"
 echo "$query_marketplace_inprog"
-result=`sqlplus -S 'prod_team_ro/'9thfl00r'@//borac101-vip.l3.bluefly.com:1521/bfyprd1' @${query_marketplace_inprog}`
+result='sqlplus -S prod_team_ro/'9thfl00r'@//borac101-vip.l3.bluefly.com:1521/bfyprd1 @${query_marketplace_inprog}'
+echo "$result" '\n' $result ;
 
 cd /usr/local/batchRunScripts/python/jbmodules/image_processing/marketplace
 echo "Total to Update ...." ;
 echo "$result" | xargs -n1 | wc -l ;
 
 for f in "$result"; do
-/usr/local/batchRunScripts/python/jbmodules/image_processing/marketplace/x-vendorget-module-cronjobDloader.py "$f" ;
+#/usr/local/batchRunScripts/python/jbmodules/image_processing/marketplace/x-vendorget-module-cronjobDloader.py "$f" ;
 echo Completed "$f" ;
 done
