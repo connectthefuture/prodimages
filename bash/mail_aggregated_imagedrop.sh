55e5ab0f333d5b2724b7d782b46b1fc63421dfcb
johnb <jbragato@yahoo.com>
2015-06-16 16:11:10 -0400

    x

--- bash/mail_aggregated_imagedrop.sh
+++ bash/mail_aggregated_imagedrop.sh
@@ -36,7 +36,13 @@ alt_results=$(mysql --host=127.0.0.1 --port=3301 --column-names=True --table --u
 
 subject=$(echo "Last Upload: ${allfiles} Files - Total Styles: ${primaryonly} at ${process_time}")
 
-content=$(echo "Total Styles: ${allfiles} '\n'Primary-Images: ${primaryonly} '\t' Alt-Images: ${altonly} '\n\n'---- Main-Results --> '\n\n'${main_results} \n\n\n----> Alt-Results -->\n\n ${alt_results}") ## --- \n -- ${msql} - ${asql}")
+content=$(echo "Total Styles: 	${allfiles} \
+				Primary-Images: ${primaryonly} \
+				Alt-Images: 	${altonly} \
+				---- Main-Results --> \
+				${main_results}  \
+				----> Alt-Results --> \
+				${alt_results}") ## --- \n -- ${msql} - ${asql}")
 
 # $(for X in ${alt_styles_list}; do echo \"${X}\";done) -- $(for X in ${main_styles_list};do echo \"${X}\"; done)"`
 #content=`echo "<html><body><table><tr>Total Styles: ${allfiles} </tr><tr>Main Images Total: ${primaryonly} </tr><tr>Total Alts: ${altonly}</tr></table><table> $(for X in ${alt_styles_list}; do echo "<tr>${X}</tr>";done)</table><table> $(for X in ${main_styles_list};do echo "<tr>${X}</tr>"; done) </table></body></html>"`
