c7387465f58cee76ec4a2c000f5ccb351bfdb81d
johnb <john.bragato@gmail.com>
2015-06-17 05:44:41 -0400

    x

--- bash/run_update_mrktplce.sh
+++ bash/run_update_mrktplce.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-. ~/.bash_profile
+#. ~/.bash_profile
 
 
 query_marketplace_inprog="/usr/local/batchRunScripts/sql/marketplace_update_filter.sql"
@@ -13,10 +13,10 @@ res=$(${runit})
 cd /usr/local/batchRunScripts/python/jbmodules/image_processing/marketplace
 echo "Total to Update ...." ;
 
-# for f in `echo "$res" | xargs -n1 | grep -v selected`; do
-# 	/usr/local/batchRunScripts/python/jbmodules/image_processing/marketplace/x-vendorget-module-cronjobDloader.py "$f" ;
-# 	#echo Completed "$f" ;
-# done
+for f in `echo "$res" | xargs -n1 | grep -v selected`; do
+    #/usr/local/batchRunScripts/python/jbmodules/image_processing/marketplace/x-vendorget-module-cronjobDloader.py "$f" ;
+    echo Completed "$f" ;
+done
 
 ##parallel -P2 -X --progress echo {} ::: 
 $runit
