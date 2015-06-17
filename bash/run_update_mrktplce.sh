88372b4cef78ce47199c4cc76b76c35a0329bff0
johnb <jbragato@yahoo.com>
2015-06-17 12:57:45 -0400

    x

--- bash/run_update_mrktplce.sh
+++ bash/run_update_mrktplce.sh
@@ -2,12 +2,15 @@
 
 . ~/.bash_profile
 
+shopt -s xpg_echo
+shopt -u hostcomplete extquote
 
 query_marketplace_inprog="/usr/local/batchRunScripts/sql/marketplace_update_filter.sql"
 
 sqlcmd="sqlplus -S prod_team_ro/9thfl00r@//borac101-vip.l3.bluefly.com:1521/bfyprd1 @${query_marketplace_inprog}"
-runit=$(echo "$sqlcmd")
-res=$(${runit})
+# sqlcmd="sqlplus -S prod_team_ro/9thfl00r@//borac101-vip.l3.bluefly.com:1521/bfyprd1 @/usr/local/batchRunScripts/sql/marketplace_update_filter.sql"
+runit=$(echo $sqlcmd)
+res=$(runit)
 #res=`$sqlcmd`
 
 cd /usr/local/batchRunScripts/python/jbmodules/image_processing/marketplace
@@ -19,8 +22,8 @@ echo "Total to Update ...." ;
 # done
 
 ##parallel -P2 -X --progress echo {} ::: 
-$runit
-echo $(echo "$res" | grep -v selected | xargs -n1) | wc -l
+echo $runit
+echo $(echo $res | grep -v selected | xargs -n1) | wc -l
 
 echo $(shopt)
 ##/usr/local/batchRunScripts/python/jbmodules/image_processing/marketplace/x-vendorget-module-cronjobDloader.py
