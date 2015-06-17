e2fa684038aa4dfc409504be6d739b52371ae0d6
johnb <jbragato@yahoo.com>
2015-06-17 16:40:10 -0400

    x

--- bash/run_update_mrktplce.sh
+++ bash/run_update_mrktplce.sh
@@ -7,7 +7,7 @@
 # export LANGUAGE="en" ;
 query_marketplace_inprog="/usr/local/batchRunScripts/sql/marketplace_update_filter.sql"
 
-RES='$(sqlplus prod_team_ro/9thfl00r@//borac101-vip.l3.bluefly.com:1521/bfyprd1 @\\$query_marketplace_inprog) | xargs -n1 | grep -v selected'
+RES='$($ORACLE_HOME/bin/sqlplus prod_team_ro/9thfl00r@//borac101-vip.l3.bluefly.com:1521/bfyprd1 @$query_marketplace_inprog) | xargs -n1 | grep -v selected'
 #res='sqlplus -S prod_team_ro/9thfl00r@//borac101-vip.l3.bluefly.com:1521/bfyprd1 @/usr/local/batchRunScripts/sql/marketplace_update_filter.sql'
 #runit=$(${sqlcmd} | /bin/bash)
 #res=$runit
