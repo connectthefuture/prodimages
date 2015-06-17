cab34b6ea16fa2033a9c19248b89dc56e166685e
johnb <jbragato@yahoo.com>
2015-06-17 14:34:23 -0400

    x

--- bash/run_update_mrktplce.sh
+++ bash/run_update_mrktplce.sh
@@ -4,7 +4,7 @@
 
 # shopt -s xpg_echo expand_aliases direxpand
 # shopt -u hostcomplete extquote
-
+export LANGUAGE="en" ;
 query_marketplace_inprog="/usr/local/batchRunScripts/sql/marketplace_update_filter.sql"
 
 ##res=`sqlplus -S prod_team_ro/9thfl00r@//borac101-vip.l3.bluefly.com:1521/bfyprd1 @$query_marketplace_inprog | xargs -n1 | grep -v selected`
