8010f7aedf0b777c51d220303136bbe0412b5b99
johnb <jbragato@yahoo.com>
2015-06-17 14:16:28 -0400

    x

--- bash/run_update_mrktplce.sh
+++ bash/run_update_mrktplce.sh
@@ -7,7 +7,7 @@
 
 query_marketplace_inprog="/usr/local/batchRunScripts/sql/marketplace_update_filter.sql"
 
-res=`sqlplus -S prod_team_ro/9thfl00r@//borac101-vip.l3.bluefly.com:1521/bfyprd1 @${query_marketplace_inprog} | xargs -n1 | grep -v selected`
+res=`sqlplus -S prod_team_ro/9thfl00r@//borac101-vip.l3.bluefly.com:1521/bfyprd1 @$query_marketplace_inprog | xargs -n1 | grep -v selected`
 # sqlcmd="sqlplus -S prod_team_ro/9thfl00r@//borac101-vip.l3.bluefly.com:1521/bfyprd1 @/usr/local/batchRunScripts/sql/marketplace_update_filter.sql"
 #runit=$(${sqlcmd} | /bin/bash)
 #res=$runit
