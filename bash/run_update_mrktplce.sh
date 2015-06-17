82576da3e2147513699aa09b40ef46002609c81d
johnb <jbragato@yahoo.com>
2015-06-17 15:31:03 -0400

    x

--- bash/run_update_mrktplce.sh
+++ bash/run_update_mrktplce.sh
@@ -7,7 +7,7 @@
 # export LANGUAGE="en" ;
 query_marketplace_inprog="/usr/local/batchRunScripts/sql/marketplace_update_filter.sql"
 
-res="sqlplus -S prod_team_ro/9thfl00r\@\/\/borac101-vip.l3.bluefly.com:1521/bfyprd1 \@$query_marketplace_inprog | xargs -n1 | grep -v selected"
+RES="$(sqlplus -S prod_team_ro/9thfl00r\@\/\/borac101-vip.l3.bluefly.com:1521/bfyprd1 \@$query_marketplace_inprog) | xargs -n1 | grep -v selected"
 #res='sqlplus -S prod_team_ro/9thfl00r@//borac101-vip.l3.bluefly.com:1521/bfyprd1 @/usr/local/batchRunScripts/sql/marketplace_update_filter.sql'
 #runit=$(${sqlcmd} | /bin/bash)
 #res=$runit
@@ -15,9 +15,9 @@ res="sqlplus -S prod_team_ro/9thfl00r\@\/\/borac101-vip.l3.bluefly.com:1521/bfyp
 locale ;
 #cd /usr/local/batchRunScripts/python/jbmodules/image_processing/marketplace
 echo "Total to Update ...." ;
-$($res) ;
+`$RES` ;
 
-for f in `${res}`
+for f in `${RES}` ; 
 do
 # 	/usr/local/batchRunScripts/python/jbmodules/image_processing/marketplace/x-vendorget-module-cronjobDloader.py "$f" ;
 echo Completed "$f" ;
