87dc4392a9e8e4728096c320278d7a8348ebb65e
johnb <jbragato@yahoo.com>
2015-06-17 15:46:59 -0400

    x

--- bash/run_update_mrktplce.sh
+++ bash/run_update_mrktplce.sh
@@ -7,12 +7,13 @@
 # export LANGUAGE="en" ;
 query_marketplace_inprog="/usr/local/batchRunScripts/sql/marketplace_update_filter.sql"
 
-RES="$(sqlplus prod_team_ro/9thfl00r@//borac101-vip.l3.bluefly.com:1521/bfyprd1 @$query_marketplace_inprog) | xargs -n1 | grep -v selected"
+RES='$(sqlplus prod_team_ro/9thfl00r@//borac101-vip.l3.bluefly.com:1521/bfyprd1 @$query_marketplace_inprog) | xargs -n1 | grep -v selected'
 #res='sqlplus -S prod_team_ro/9thfl00r@//borac101-vip.l3.bluefly.com:1521/bfyprd1 @/usr/local/batchRunScripts/sql/marketplace_update_filter.sql'
 #runit=$(${sqlcmd} | /bin/bash)
 #res=$runit
 #res=`$sqlcmd`
-locale ;
+cat 
+#locale ;
 #cd /usr/local/batchRunScripts/python/jbmodules/image_processing/marketplace
 echo "Total to Update ...." ;
 echo $RES ;
