2efdef2a5329474ea6daf4347a846dc937d071a4
johnb <jbragato@yahoo.com>
2015-06-17 15:28:53 -0400

    x

--- bash/run_update_mrktplce.sh
+++ bash/run_update_mrktplce.sh
@@ -7,7 +7,7 @@
 # export LANGUAGE="en" ;
 query_marketplace_inprog="/usr/local/batchRunScripts/sql/marketplace_update_filter.sql"
 
-res="sqlplus -S prod_team_ro/9thfl00r@//borac101-vip.l3.bluefly.com:1521/bfyprd1 \@$query_marketplace_inprog | xargs -n1 | grep -v selected"
+res="sqlplus -S prod_team_ro/9thfl00r\@\/\/borac101-vip.l3.bluefly.com:1521/bfyprd1 \@$query_marketplace_inprog | xargs -n1 | grep -v selected"
 #res='sqlplus -S prod_team_ro/9thfl00r@//borac101-vip.l3.bluefly.com:1521/bfyprd1 @/usr/local/batchRunScripts/sql/marketplace_update_filter.sql'
 #runit=$(${sqlcmd} | /bin/bash)
 #res=$runit
@@ -15,7 +15,7 @@ res="sqlplus -S prod_team_ro/9thfl00r@//borac101-vip.l3.bluefly.com:1521/bfyprd1
 locale ;
 #cd /usr/local/batchRunScripts/python/jbmodules/image_processing/marketplace
 echo "Total to Update ...." ;
-$($res) ; 
+$($res) ;
 
 for f in `${res}`
 do
@@ -24,7 +24,7 @@ echo Completed "$f" ;
 done
 
 ##parallel -P2 -X --progress echo {} :::
-echo $ENV
+echo $(env | sort)
 #echo $(echo ${res} | grep -v selected | xargs -n1) ##| wc -l
 
 ##/usr/local/batchRunScripts/python/jbmodules/image_processing/marketplace/x-vendorget-module-cronjobDloader.py
