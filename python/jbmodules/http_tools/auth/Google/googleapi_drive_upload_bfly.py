57577fd1b17be5cb45278a0e5ed52e204b281cf1
johnb <jbragato@yahoo.com>
2015-06-22 00:51:08 -0400

    removed unused funcs

--- python/jbmodules/http_tools/auth/Google/googleapi_drive_upload_bfly.py
+++ python/jbmodules/http_tools/auth/Google/googleapi_drive_upload_bfly.py
@@ -8,80 +8,6 @@ from oauth2client.client import SignedJwtAssertionCredentials
 from apiclient.http import MediaFileUpload
 
 
-
-def create_googleapi_service(serviceName=None, version=None, client_id=None,client_secret=None,redirect_uri=None, scope=None):
-    import httplib2
-    from oauth2client.file import Storage
-    from oauth2client.client import OAuth2WebServerFlow
-    from oauth2client import tools
-    import os, datetime, argparse, apiclient
-    user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.10; en-US; rv:33.0) Gecko/20100101 Firefox/33.0' ##'Python2.7'
-    BROWSERdeveloperKey='AIzaSyBHozNPRDnVkdPo_JlP_4TLbNrJIsd3bQ4'
-    SERVERdeveloperKey='AIzaSyDe68JsIJK5O5Cqd-tAVGqaSeHqcFCNPh8'
-    #####################
-    #####################
-    # The client_id and client_secret are copied from the API Access tab on
-    # the Google APIs Console
-    FLOW = OAuth2WebServerFlow(
-        client_id=client_id,
-        client_secret=client_secret,
-        scope=scope,
-        redirect_uri=redirect_uri,
-        user_agent=user_agent)
-
-    # If the Credentials don't exist or are invalid, run through the native client
-    # flow. The Storage object will ensure that if successful the good
-    # Credentials will get written back to a file.
-    py_dir = os.path.dirname(os.path.realpath(__file__))
-    #py_dir = os.path.dirname(os.path.realpath(os.curdir))
-    print py_dir, ' Pydir'
-    os.chdir(py_dir)
-    
-    # storage_file = os.path.join(os.path.dirname(py_dir), 'calendar.dat')
-    storage_file = os.path.join(py_dir, serviceName + '.dat')
-    STORAGE = Storage(storage_file)
-
-    # To disable the local server feature, replace with '' in the following line:
-    # args = '' #'--noauth_local_webserver'
-    # parser = argparse.ArgumentParser(parents=[tools.argparser])
-    # FLAGS = parser.parse_args()
-    ##
-    #
-    credentials = STORAGE.get()
-    if credentials is None or credentials.invalid == True:
-        authorize_url = FLOW.step1_get_authorize_url()
-        print 'Go to the following link in your browser: ' + authorize_url
-        code = raw_input('Enter verification code: ').strip()
-        credentials = FLOW.step2_exchange(code)
-        STORAGE.put(credentials)
-        print credentials, STORAGE, storage_file
-        #credentials = tools.run_flow(FLOW, STORAGE, FLAGS)
-    ##
-    # Create an httplib2.Http object to handle our HTTP requests and authorize it
-    # with our good Credentials.
-    http = httplib2.Http()
-    http = credentials.authorize(http)
-
-    from apiclient import discovery
-    #client = apiclient.APIClient()
-    # Build a service object for interacting with the API.
-    service = discovery.build(serviceName=serviceName, version=version, http=http)
-    return service
-
-
-def instantiate_google_drive_service_bfly():
-    serviceName = 'drive'
-    version = 'v2'
-    client_secret = 'np60_xlDLHYircCLaxjf9-Y-'
-    client_id = '153570890903-c4au43k7jq36mv4vve174tmrkfa2orln.apps.googleusercontent.com'
-    scope = 'https://www.googleapis.com/auth/drive'
-    # drive_file = drive_file_instance
-    redirect_uri = 'urn:ietf:wg:oauth:2.0:oob'
-    service = create_googleapi_service(serviceName=serviceName, version=version, client_secret=client_secret, client_id=client_id, redirect_uri=redirect_uri, scope=scope)
-    return service
-
-
-
 def instantiate_google_drive_serviceAccount_bfly():
     # drive_file = drive_file_instance
     import httplib2
@@ -105,6 +31,24 @@ def instantiate_google_drive_serviceAccount_bfly():
     return drive_service
 
 
+def download_file_drive(file_id=file_id, destpath=destpath):
+    from os import chdir, path
+    import apiclient
+    drive_service = instantiate_google_drive_serviceAccount_bfly()
+    request = drive_service.files().get_media(fileId=file_id)
+    fdest = open(destpath, 'w')
+    media_request = apiclient.http.MediaIoBaseDownload(fdest, request)
+    while True:
+        try:
+            download_progress, done = media_request.next_chunk()
+        except apiclient.errors.HttpError, error:
+            print 'An error occurred: %s' % error
+            return media_request
+        if download_progress:
+            print 'Download Progress: %d%%' % int(download_progress.progress() * 100)
+        if done:
+            print 'Download Complete', destpath
+            return destpath
 
 
 def upload_file_drive(srcfile):
