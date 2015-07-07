4deb19dd3ec72393a04ea2aab5475cbbb003162c
johnb <jbragato@yahoo.com>
2015-07-07 08:03:55 -0400

    use cloud now

--- python/jbmodules/redis_tools/drive_uploadgoogle_redis_too.py
+++ python/jbmodules/redis_tools/drive_uploadgoogle_redis_too.py
@@ -467,12 +467,12 @@ class GooglePubSubClient:
 #        [user:*username*:password] – {string} – (md5 hash password, no example)"""
 
 # In[6]:
-
+######### REDIS ##########
 import redis
-#redis_host = 'pub-redis-17996.us-east-1-4.3.ec2.garantiadata.com'
-#redis_port = 17996
-redis_host = '127.0.0.1'
-redis_port = 6379
+redis_host = 'pub-redis-17996.us-east-1-4.3.ec2.garantiadata.com'
+redis_port = 17996
+#redis_host = '127.0.0.1'
+#redis_port = 6379
 
 
 r = redis.Redis(host=redis_host, port=redis_port,  encoding='utf-8', encoding_errors='strict')  ##,db=0, password=None, socket_timeout=None, connection_pool=None, unix_socket_path=None)
