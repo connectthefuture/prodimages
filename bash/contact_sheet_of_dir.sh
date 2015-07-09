2690315e0e1ea0433f97eacfaa4c159a0c2afbe3
johnb <jbragato@yahoo.com>
2015-07-09 12:44:22 -0400

    contact sheet maker shell script use arg 1 s dir to seach for images

---
+++ bash/contact_sheet_of_dir.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+. ~/.bash_profile
+#convert -format jpg $1 -depth 8 -density 72 -units pixelsperinch -fill grey50 -colorize 40 miff:- | 
+
+#composite -dissolve 15 - -colorspace sRGB -quality 95 ~/virtualenvs/GitHub-prodimages/python/jbmodules/image_processing/magick_tweaks/img/Bluefly_Logo_WatermarkSmall.png "$2"$(basename "$1")
+
+
+#echo ${JOHJ/J/JPEG}
+
+montage -verbose -label '%f' -font Ubuntu -pointsize 10 -background '#000000' -frame 1 -fill 'white' -define jpeg:size=150x180 -geometry 150x180+2+4 -auto-orient $1\*.jpg $1/shotlist.jpg
