1c0395ea35944a004f881b18c59117eae096b35e
johnb <jbragato@yahoo.com>
2015-07-21 16:51:44 -0400

    openporter

---
+++ python/test_open_ports.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+
+def port_tester(port, host=None):
+    import socket;
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    if not host:
+        host = '127.0.0.1'
+    result = sock.connect_ex((host,port))
+    if result == 0:
+       print "Port {} is open on {}".format(port,host)
+    else:
+       print "Port {} is not open on {}".format(port,host)
+
+
+def main(args):
+    import sys
+    args = sys.argv
+    try:
+        host = args[2]
+        port = sys.argv[1]
+    except IndexError:
+        host = None
+        port = sys.argv[1]
+    if len(port.split()) == 1:
+        port_tester(port, host=None)
+    else:
+        for p in port.split():
+            port_tester(p, host=None)
+    return
+
+
+if __name__ == '__main__':
+    main()
