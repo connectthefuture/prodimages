c4668640a4299ca19f1816865198098bf1ff63f6
johnb <jbragato@yahoo.com>
2015-08-19 05:16:07 -0400

    du ?

--- mozu/prodimages_upload.py
+++ mozu/prodimages_upload.py
@@ -18,7 +18,7 @@ def init_pg_mktble_fnc_trig():
     createfunc_nowonupdate = "CREATE OR REPLACE FUNCTION update_updated_at_column() RETURNS trigger LANGUAGE plpgsql AS $$ BEGIN NEW.updated_at = NOW(); RETURN NEW; END; $$;"
     createtrig_nowonupdate = "CREATE TRIGGER images_bfly_mozu_updated_at_modtime BEFORE UPDATE ON public.images_bfly_mozu FOR EACH ROW EXECUTE PROCEDURE update_updated_at_column();"
     # Auto incr after modify
-    createfunc_incronupdate = "CREATE SEQUENCE seq_update_ct INCREMENT BY 1 MINVALUE 1; CREATE OR REPLACE FUNCTION incr_updated_ct() RETURNS trigger AS $BODY$ BEGIN NEW.updated_ct := nextval('seq_update_ct'); RETURN NEW; END; $BODY$ LANGUAGE 'plpgsql';"
+    #createfunc_incronupdate = "CREATE SEQUENCE seq_update_ct INCREMENT BY 1 MINVALUE 1; CREATE OR REPLACE FUNCTION incr_updated_ct() RETURNS trigger AS $BODY$ BEGIN NEW.updated_ct := nextval('seq_update_ct'); RETURN NEW; END; $BODY$ LANGUAGE 'plpgsql';"
     createtrig_incronupdate = "CREATE TRIGGER images_bfly_mozu_incr_updated_ct BEFORE UPDATE ON public.images_bfly_mozu FOR EACH ROW EXECUTE PROCEDURE incr_updated_ct();"
 
     createfuncalter_incronupdate = "CREATE SEQUENCE seq_update_ct INCREMENT BY 1 MINVALUE 1; ALTER TABLE images_bfly_mozu_updated_at_modtime ALTER seq_update_ct SET DEFAULT nextval('seq_update_ct'); "
@@ -31,8 +31,8 @@ def init_pg_mktble_fnc_trig():
     conn.commit()
     cur.execute(createtrig_nowonupdate)
     conn.commit()
-    cur.execute(createfunc_incronupdate)
-    conn.commit()
+    #cur.execute(createfunc_incronupdate)
+    #conn.commit()
     cur.execute(createtrig_incronupdate)
     conn.commit()
     cur.execute(createfuncalter_incronupdate)
