d0454e7149107a23adb65f587cbc11e8bf765f3e
johnb <jbragato@yahoo.com>
2015-09-14 23:02:37 -0400

    xbash funx

--- bash/bash_functions.sh
+++ bash/bash_functions.sh
@@ -496,6 +496,14 @@ PS1="$TITLEBAR $GRAY-$CYAN-$LIGHT_CYAN($CYAN\u$GRAY@$CYAN\h\$DARK_BLUE)$CYAN-$LI
 PS2="$LIGHT_CYAN-$CYAN-$GRAY-$NO_COLOUR "
 }
 
+function recent_styles_uploaded () 
+    {
+    if [[ "$#" > 0 ]]; then MINUTESAGO=$1
+    else MINUTESAGO=60 
+    fi;
+    QUERY=`echo -e "select distinct t1.colorstyle from www_django.image_update t1 join product_snapshot_live t2 on t1.colorstyle=t2.colorstyle where create_dt < date_sub(now(), interval $MINUTESAGO minute) and (t2.image_ready_dt is not null and t2.image_ready_dt != \"0000-00-00\");"` ; 
+    $(mysql --host=127.0.0.1 --port=3301 --column-names=False --user=root --password=mysql -e "$QUERY" -D www_django;) ;
+}
 
 # Local Variables: 
 # mode:shell-script 
