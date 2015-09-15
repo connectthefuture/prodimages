407a3cbe2ef3dd384cc26cc3f09f8ad26c50b52b
johnb <jbragato@yahoo.com>
2015-09-15 17:29:43 -0400

    x

--- bash/clear_newcache_from_djapi.sh
+++ bash/clear_newcache_from_djapi.sh
@@ -8,7 +8,7 @@ TODAY=`date "+Date:%Y-%m-%d_%H:%M:%S"`
 export -f recent_styles_uploaded;
 STYLES=$(recent_styles_uploaded | xargs -n1 | sort -nru)
 STCOUNT=$(echo "${STYLES[@]}" | xargs -n1 | wc -l)
-echo -e "\n---------\nBatchSize:${STCOUNT}\n${TODAY}" 2>&1 >> /mnt/Post_Complete/Complete_Archive/AUTOCCLEARLOG.log
+echo -e "\n---------\nBatchSize:$STCOUNT\n$TODAY" 2>&1 >> /mnt/Post_Complete/Complete_Archive/AUTOCCLEARLOG.log
 for f in $STYLES; do
 	echo -e "${TODAY}\tStyle: ${f}\tTotal:${STCOUNT}\n" 2>&1 >> /mnt/Post_Complete/Complete_Archive/AUTOCCLEARLOG.log
 	/usr/local/batchRunScripts/python/anotherTest_Sites_CacheClear.py "$f" 2>&1 > /dev/null && echo -e "${TODAY}\tStyle:${f}\tTotal:${STCOUNT}\n"
