2b3c5bafa01e5b42c6de70c81ac047a4927dd254
johnb <jbragato@yahoo.com>
2015-10-02 15:28:46 -0400

    use requests to send to pm

--- python/jbmodules/http_tools/send_data/send_pm_photo_update.py
+++ python/jbmodules/http_tools/send_data/send_pm_photo_update.py
@@ -17,6 +17,15 @@ def update_pm_photodate(colorstyle):
     update_url,
     ])
 
+
+def update_pm_photodate_purepy(colorstyle):
+    import requests
+    update_url = 'http://dmzimage01.l3.bluefly.com:8080/photo/{0}'.format(colorstyle)
+    data= {'sample_image': 'Y', 'photographed_date': 'now'}
+    res = requests.put(update_url,data=data)
+    return res
+
+
 if __name__ == '__main__':
     import sys
     r = update_pm_photodate(sys.argv[1])
