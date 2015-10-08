dbec7b18dd8282c018d389a8edb1a9572948a0bb
johnb <jbragato@yahoo.com>
2015-10-08 16:39:34 -0400

    python startup log func

--- python/pythonstartup.py
+++ python/pythonstartup.py
@@ -13,6 +13,8 @@ import csv
 import glob
 import collections
 readline.parse_and_bind("tab: complete")
+
+
 #import sqlalchemy
 # from bs4 import BeautifulSoup
 # import requests
@@ -168,6 +170,21 @@ regex_exif = re.compile(pattern_exiftag + "=" + pattern_exifval)
 # #END###### FTP FUNX
 
 # """
+# we define a decorator
+def log(original_function, filename=None):
+    import logging
+    from os import path as path
+    if filename is None:
+        filename = str("__main__" + "_log.txt")
+    logging.basicConfig(filename=filename, level=logging.INFO)
+    print "Logging to … {0}".format(path.abspath(filename))
+    def new_function(*args, **kwargs):
+        result = original_function(*args, **kwargs)
+        with open(filename, "ab+") as logfile:
+            logfile.write("Function '%s' called with positional arguments %s and keyword arguments %s. The result was %s.\n" % (original_function.__name__, args, kwargs, result))
+        return result
+    return new_function
+
 # Return Date Formatted for Inserting to MySQL db
 # """
 # def dateMysql(date):
