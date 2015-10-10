01fc3bbce5cfcc31192a82d4bf9c1a97ce322180
johnb <jbragato@yahoo.com>
2015-10-09 21:19:42 -0400

    getting real close

--- mozu/mozu_image_util_functions.py
+++ mozu/mozu_image_util_functions.py
@@ -1,6 +1,35 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+# Defining a log function decorator to use as @log
+def log(original_function, filename=None):
+    import logging
+    from os import path as path
+    if filename is None:
+        filename = str("__main__" + "_log.txt")
+    logging.basicConfig(filename=filename, level=logging.INFO)
+    print "Logging to … {0}".format(path.abspath(filename))
+    def new_function(*args, **kwargs):
+        result = original_function(*args, **kwargs)
+        with open(filename, "ab+") as logfile:
+            logfile.write("Function '%s' called with positional arguments %s and keyword arguments %s. The result was %s.\n" % (original_function.__name__, args, kwargs, result))
+        return result
+    return new_function
+
+### Generic Logger
+def mr_logger(src_filepath,*args):
+    import datetime
+    current_dt = datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d')
+    logged_items = []
+    if len(args) > 0:
+        for arg in args:
+            logit = "{}\t{}\n".format(current_dt,arg)
+            logged_items.append(logit)
+    for i in logged_items:
+        with open(src_filepath, 'ab+') as f:
+            f.write(i)
+    return src_filepath
+
 ### Utility Funx - Get File Data
 ## util func calcs md5 of file
 def md5_checksumer(src_filepath):
@@ -81,39 +110,28 @@ def get_exif_all_data(src_filepath):
     else: pass
 
 ## Compile Inserts as dict with key == bluefly file name
-def compile_data_db_import(list_of_images,**kwargs):
-    images_insert_dict = {}
+def compile_todict_for_class_instance_properties(list_of_images,**kwargs):
+    instance_properties = {}
     for img in list_of_images:
         bf_imageid = img.split('/')[-1]
         
         try:
-            mozu_image_table = mozu_image_table_instance()
+            #mozu_image_table = mozu_image_table_instance()
             md5checksum = md5_checksumer(img)
-            image_metadata = get_exif_all_data(img)
+            if not kwargs.get('tags'):
+                image_metadata = get_exif_all_data(img)
+                tags = image_metadata.values()
+            else:
+                tags = kwargs.get('tags')
         except TypeError: 
             print 'TYPE Error'
             pass
 
-        images_insert_dict[img] = dict(bf_imageid  = bf_imageid, 
-                                                mz_imageid  = kwargs.get('mz_imageid', 'NA'), 
-                                                md5checksum = md5checksum)
-                                                #image_metadata = image_metadata)
-        #print 'METADATA --> ', image_metadata
-    return images_insert_dict
-
-### Generic Logger
-def mr_logger(filepath,*args):
-    import datetime
-    current_dt = datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d')
-    logged_items = []
-    if len(args) > 0:
-        for arg in args:
-            logit = "{}\t{}\n".format(current_dt,arg)
-            logged_items.append(logit)
-    for i in logged_items:
-        with open(filepath, 'ab+') as f:
-            f.write(i)
-    return filepath
+        instance_properties[img] = dict(bf_imageid  = bf_imageid, 
+                                        mz_imageid = kwargs.get('mz_imageid', 'NA'), 
+                                        md5checksum = md5checksum),
+                                        tags = (list(set(sorted(tags)))
+    return instance_properties
 
 ###########################
 def main():
