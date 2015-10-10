753c237dc2af88f37e401779625fa55e9babbdc6
johnb <jbragato@yahoo.com>
2015-10-10 00:11:51 -0400

    split db tables complete

--- mozu/mozu_exec.py
+++ mozu/mozu_exec.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-## Use Classes - Could be in separate file
-# from RESTClient import MozuRestClient
+from RESTClient import MozuRestClient
 def count_total__files_documents(MozuRestClient,mz_imageid):
     mzclient = MozuRestClient(mz_imageid=mz_imageid)
     totalCount = mzclient.get_mz_image()['totalCount']
@@ -52,35 +51,14 @@ def upload_new(MozuRestClient,src_filepath,**kwargs):
     return doc_resp
 
 
-########################### DB Table Defs ############################
-##
-def mozu_image_table_instance(**kwargs):
-    import sqlalchemy, datetime
-    from sqlalchemy import Table, Column, Integer, String, DateTime, MetaData, create_engine
-    from sqlalchemy import Sequence, FetchedValue, Text
-    from sqlalchemy.dialects import oracle as oracle_dialect
-
-    db_uri = 'oracle+cx_oracle://MZIMG:p1zza4me@qarac201-vip.qa.bluefly.com:1521/bfyqa1201'
-    engine = sqlalchemy.create_engine(db_uri, implicit_returning=False, coerce_to_decimal=False)
-    metadata = MetaData(bind=engine)  #, quote_schema=True, schema='bfyqa1201')
-    mozu_image_table = Table( 'mozu_image', metadata,
-        #Column('id', Integer, Sequence('mozu_image_seq_trigger'), primary_key=True),
-        Column('id', Integer, server_default=FetchedValue(), primary_key=True),
-        Column('bf_imageid', String(19), unique=True, nullable=False),
-        Column('mz_imageid', String(37)), 
-        Column('md5checksum', String(32)),
-        Column('created_date', oracle_dialect.DATE, server_default=FetchedValue()), 
-        Column('modified_date', oracle_dialect.DATE, onupdate=datetime.datetime.now), 
-        Column('updated_count', Integer, default=0)    
-        )
-    return mozu_image_table
-
 ###########################
 ### Main - Conditions ##
 def main(insert_list_filepaths):
     import sqlalchemy
-    from mozu_image_util_functions import *
     from RESTClient import MozuRestClient
+    from db import mozu_image_table_instance
+    from mozu_image_util_functions import *
+
     compiled_instance_vars = compile_todict_for_class_instance_variables(insert_list_filepaths)
     
     for k,v in compiled_instance_vars.iteritems():
@@ -89,31 +67,51 @@ def main(insert_list_filepaths):
         mz_imageid = v['mz_imageid']
         md5checksum = v['md5checksum']
         tags        = v['tags']
+
         #image_metadata = v['image_metadata']
         mozu_image_table = mozu_image_table_instance()
+
         try:
-            mz_imageid = upload_new(MozuRestClient,src_filepath)
-            load_content_resp = upsert_content_mz_image(src_filepath=src_filepath,tags=tags) 
-            v['mz_imageid'] = mz_imageid
-            insert_records = mozu_image_table.insert(values=dict(**v))
-            insert_records.execute()
-            print 'Inserted --> ', v.items(), ' <-- ', insert_records
+            mozu_client = MozuRestClient(dict(**v))
+            mz_imageid = upload_new(client,src_filepath)
+            load_content_resp = upsert_content_mz_image(mozu_client, src_filepath=src_filepath,tags=tags) 
+            if load_content_resp.http_status_code < 400:
+                v['mz_imageid'] = mz_imageid
+                insert_db = mozu_image_table.insert(values=dict(**v))
+                insert_db.execute()
+                print 'Inserted --> ', v.items(), ' <-- ', insert_db
+            elif load_content_resp.http_status_code == 409:
+                mz_imageid = mozu_image_table.select( whereclause=( (mozu_image_table.c.bf_imageid == v['bf_imageid']) ) )
+                upsert_content_resp = upsert_content_mz_image(mozu_client,dict(**v)):
+                if upsert_content_resp.http_status_code < 300:
+                    update_db = mozu_image_table.update(values=dict(**v),whereclause=mozu_image_table.c.bf_imageid==v['bf_imageid'])
+                res = update_db.execute()
+                print res, 'Updated--> ', v.items(), ' <-- ', update_db
+            else:
+                print("HTTP Status: {}\n Raising Integrity Error").format(load_content_resp.http_status_code)
+                raise sqlalchemy.exc.IntegrityError()
         # Update
         except sqlalchemy.exc.IntegrityError:
             print 'IntegrityError ', v
-            old_mz_imageid = mozu_image_table.select(
-                whereclause=( (mozu_image_table.c.bf_imageid == v['bf_imageid'])  & (mozu_image_table.c.md5checksum <> v['md5checksum']) )
-                )
-            update_records = mozu_image_table.update(values=dict(**v),whereclause=mozu_image_table.c.bf_imageid==v['bf_imageid'])
-            res = update_records.execute()
-            print res, 'Updated--> ', v.items(), ' <-- ', update_records
+            mozu_client = MozuRestClient(dict(**v))
+            mz_imageid = mozu_image_table.select( whereclause=( (mozu_image_table.c.bf_imageid == v['bf_imageid']) ) )
+            upsert_content_resp = upsert_content_mz_image(mozu_client,dict(**v))
+            if upsert_content_resp.http_status_code < 300:
+                update_db = mozu_image_table.update(values=dict(**v),whereclause=mozu_image_table.c.bf_imageid==v['bf_imageid'])
+                res = update_db.execute()
+            print res, 'Updated with Integrity Errors --> ', v.items(), ' <-- ', update_db
             pass
 
+## Run in shell as mozu_exec.py *args
 if __name__ == '__main__':
     import sys
     import os.path as path
-    if path.isfile(sys.argv[1]):
-        insert_list_filepaths =  sys.argv[1] ##'/mnt/Post_Complete/Complete_Archive/xTestFiles/xTestMarketplace/999999/360128501.png'
-        main([insert_list_filepaths])
-
-
+    insert_list = []
+    try:
+        if path.isfile(sys.argv[1]):
+            for arg in sys.argv:
+                insert_list.append(arg)##'/mnt/Post_Complete/Complete_Archive/xTestFiles/xTestMarketplace/999999/360128501.png'    
+        insert_list_filepaths = list(set(sorted(insert_list)))
+        main(insert_list_filepaths)
+    except IndexError:
+        print("To Run in shell you must provide at least 1 file path as an argument. \nArgs Separated by space. \n\t mozu_exec.py \*args")
