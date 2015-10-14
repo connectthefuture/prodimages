cb9e465787ecec7d78ff03f5202a869967b0f1e2
johnb <jbragato@yahoo.com>
2015-10-14 19:37:59 -0400

    using base config now

--- mozu/RESTClient.py
+++ mozu/RESTClient.py
@@ -1,19 +1,8 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-def get_mozu_client_authtoken():
-        #  "http://requestb.in/q66719q6" # http://private-227f3-relic7.apiary-mock.com
-        import requests, json
-        _auth_url = "https://home.staging.mozu.com/api/platform/applications/authtickets"
-        _auth_headers = {'Content-type': 'application/json', 'Accept-Encoding': 'gzip, deflate'}
-        _auth_request = {'applicationId' : 'bluefly.product_images.1.0.0.release', 'sharedSecret' : '53de2fb67cb04a95af323693caa48ddb'}
-        _auth_response = requests.post(_auth_url, data=json.dumps(_auth_request), headers=_auth_headers, verify=False)
-        # TODO: 5) add Validation(regex) to prevent unwanted updates
-        print "Auth Response: {0}".format(_auth_response.status_code)
-        _auth_response.raise_for_status()
-        _auth = _auth_response.json()
-        print "Auth Ticket: {0}".format(_auth["accessToken"])
-        return _auth["accessToken"] #, _auth_response.status_code
+# Import initial static vars and Auth func
+from base_config import *
 
 class MozuBflyDocument:
     pass
