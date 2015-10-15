06a2352a8a1b55798423bf5b331e997d0d000448
johnb <jbragato@yahoo.com>
2015-10-15 04:18:50 -0400

    x

--- mozu/RESTClient.py
+++ mozu/RESTClient.py
@@ -171,9 +171,9 @@ class MozuRestClient:
         print "DocumentGetResponse: {0}".format(_document_response.json())
         print document_list_uri
         try:
-            return _document_response.json()
-        except KeyError:
             return _document_response
+        except KeyError:
+            return _document_response.headers
 
 
     ## GET - Document
@@ -186,9 +186,9 @@ class MozuRestClient:
         MozuRestClient.http_status_code = _document_response.status_code
         print "DocumentGetResponse: {0}".format(_document_response.status_code)
         try:
-            return _document_response.json()
-        except KeyError:
             return _document_response
+        except KeyError:
+            return _document_response.headers
 
 
     ## GET - Document
