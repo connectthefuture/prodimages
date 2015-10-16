42b2dcf7925bad6ce8b220c652f63827a986cb24
johnb <jbragato@yahoo.com>
2015-10-16 02:25:29 -0400

    x

--- mozu/RESTClient.py
+++ mozu/RESTClient.py
@@ -218,8 +218,10 @@ class MozuRestClient:
             _qstring_fields.append("includeInactive={{include_inactive}}".format(**kwargs))
 
         if _qstring_fields:
-            from urllib import urlencode
-            _qstring = urlencode(_qstring_fields)
+            from urllib import urlencode, quote_plus
+            #_qstring = urlencode(_qstring_fields)
+            _qstring = "&".join(_qstring_filter)
+            _qstring = quote_plus(_qstring)
             document_list_uri = document_list_uri + "?" + _qstring
 
         _document_list_response = requests.get(document_list_uri, data=json.dumps(self.document_payload), headers=self.headers, verify=False)
