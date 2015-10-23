f9d7400a58de5b3bf16805b84ac5a040bf6f24fb
johnb <jbragato@yahoo.com>
2015-10-22 22:41:01 -0400

    x

--- mozu/RESTClient.py
+++ mozu/RESTClient.py
@@ -124,14 +124,14 @@ class MozuRestClient:
         from mozu_image_util_functions import include_keys
         from urllib import urlencode, unquote
         ## Default qstring params camel cased to adhere to mozu format
-        if kwargs.get("name") or kwargs.get("bf_imageid"):
+        if kwargs.get("name"): # or kwargs.get("bf_imageid"):
             kwargs['name'] =  kwargs.get("bf_imageid", kwargs.get("name"))
-            kwargs["pageSize" ] = kwargs.get("page_size", "50")
+            kwargs["pageSize"] = kwargs.get("page_size", "50")
             qstring_args = include_keys(kwargs, __mozu_document_filter_valid_keys__)
             _qstring = "?{}".format(urlencode(qstring_args))
         elif not kwargs.get("mz_imageid"):
             kwargs["sortBy"] =  kwargs.get("sort_by", "name+desc")
-            kwargs["pageSize" ] = kwargs.get("page_size", "50")
+            kwargs["pageSize"] = kwargs.get("page_size", "50")
             kwargs["startIndex" ] = kwargs.get("start_index", "0")
             qstring_args = include_keys(kwargs, __mozu_query_filter_valid_keys__)
             print qstring_args
