0cc40134b706c37dd604b58e64a7dc74cb141359
johnb <jbragato@yahoo.com>
2015-10-26 13:09:12 -0400

    x

--- mozu/batch_loader.py
+++ mozu/batch_loader.py
@@ -22,7 +22,7 @@ def batch_load_dated_mozu_jpgs(rootdir=None):
     dated_dir = "dateloaded_" + str(todaysdate).replace(",", "")
     archive_uploaded_day = path.join(archive, dated_dir)
     imgdest_jpg_mozu = path.join(archive_uploaded_day, 'JPG_MOZU_LOAD')
-    # imgdest_jpg_mozu_loaded = path.join(imgdest_jpg_mozu, 'LOADED')
+    imgdest_jpg_mozu_loaded = path.join(imgdest_jpg_mozu, 'LOADED')
 
 
     # try:
@@ -36,7 +36,28 @@ def batch_load_dated_mozu_jpgs(rootdir=None):
     import mozu_exec, mozu_image_util_functions
     ## Compress and convert to jpg and store in separate dir for concurrent xfers
     #if path.isfile(pngout):
-    load_batch_mozu_list = glob.glob(path.join(archive_uploaded_day, '*/JPG_MOZU_LOAD/*.??[gG]'))
-    for f in load_batch_mozu_list:
-        print path.abspath(f)
-        #jpgout = mozu_image_util_functions.magick_convert_to_jpeg(f,destdir=imgdest_jpg_mozu)
+    load_collect_batch_mozu_list = glob.glob(path.join(archive_uploaded_day, '*/JPG_MOZU_LOAD/*.??[gG]'))
+    for f in load_collect_batch_mozu_list:
+        shutil.move(f, imgdest_jpg_mozu)
+
+    load_batch_mozu_list = glob.glob(path.join(imgdest_jpg_mozu, '*.??[gG]'))
+    mozu_exec.main(load_batch_mozu_list)
+    for f in load_collect_batch_mozu_list:
+        shutil.move(f, imgdest_jpg_mozu_loaded)
+    load_batch_mozu_done = glob.glob(path.join(imgdest_jpg_mozu_loaded, '*.*[gG]'))
+
+    return load_batch_mozu_done
+
+    #for f in load_batch_mozu_list:
+    #    print path.abspath(f)
+    #    #jpgout = mozu_image_util_functions.magick_convert_to_jpeg(f,destdir=imgdest_jpg_mozu)
+
+
+
+if __name__ == '__main__':
+    import sys
+    try:
+        rootdir = sys.argv[1]
+        batch_load_dated_mozu_jpgs(rootdir)
+    except IndexError:
+        batch_load_dated_mozu_jpgs()
