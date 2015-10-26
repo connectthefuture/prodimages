987ee07d697710a2f4eeff35726bba84e926dc04
johnb <jbragato@yahoo.com>
2015-10-26 12:04:10 -0400

    x

--- mozu/batch_loader.py
+++ mozu/batch_loader.py
@@ -13,11 +13,16 @@ def batch_load_dated_mozu_jpgs(rootdir=None):
     todaysdatearch = todaysdatefull # '{:%Y,%m,%d,%H,%M}'.format(datetime.datetime.now())
 
     ## Define for Creating Archive dirs
-    archive = '/mnt/Post_Complete/Complete_Archive/Uploaded'
-    #archive_uploaded = path.join(archive, "dateloaded_" + str(todaysdate).replace(",", ""), "uploaded_" + str(todaysdatefullsecs).replace(",", ""))
-    archive_uploaded_day = path.join(archive, "dateloaded_" + str(todaysdate).replace(",", ""))
+    if not rootdir:
+        rootdir = archive
+    else:
+        archive = '/mnt/Post_Complete/Complete_Archive/Uploaded'
+
+    # archive_uploaded = path.join(archive, "dateloaded_" + str(todaysdate).replace(",", ""), "uploaded_" + str(todaysdatefullsecs).replace(",", ""))
+    dated_dir = "dateloaded_" + str(todaysdate).replace(",", "")
+    archive_uploaded_day = path.join(archive, dated_dir)
     imgdest_jpg_mozu = path.join(archive_uploaded_day, 'JPG_MOZU_LOAD')
-    imgdest_jpg_mozu_loaded = path.join(imgdest_jpg_mozu, 'LOADED')
+    # imgdest_jpg_mozu_loaded = path.join(imgdest_jpg_mozu, 'LOADED')
 
 
     # try:
@@ -28,4 +33,9 @@ def batch_load_dated_mozu_jpgs(rootdir=None):
     # for f in img_list:
     #     shutil.copy2(f, imgdest_jpg_mozu)
 
-    final_mozu_list = glob.glob(path.join(imgdest_jpg_mozu, '*/*/*.??[gG]'))
+    import mozu_exec, mozu_image_util_functions
+    ## Compress and convert to jpg and store in separate dir for concurrent xfers
+    #if path.isfile(pngout):
+    load_batch_mozu_list = glob.glob(path.join(archive_uploaded_day, '*/JPG_MOZU_LOAD/*.??[gG]'))
+    for f in load_batch_mozu_list:
+        jpgout = mozu_image_util_functions.magick_convert_to_jpeg(f,destdir=imgdest_jpg_mozu)
