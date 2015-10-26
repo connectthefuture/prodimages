4f0da34f20dc2d64c30c9ca734f9c78c0d8bee55
johnb <jbragato@yahoo.com>
2015-10-26 17:14:41 -0400

    batch_loader

--- mozu/batch_loader.py
+++ mozu/batch_loader.py
@@ -1,26 +1,25 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-def batch_load_dated_mozu_jpgs(rootdir=None):
+def batch_load_dated_mozu_jpgs(archive_root=None):
     ########## MOZU - Five ##########
     ### Date Defs
-    from os import chdir, path, curdir
+    from os import path  # , curdir, chdir
     import datetime, glob, shutil
 
     todaysdatefullsecs = '{:%Y%m%d%H%M%S}'.format(datetime.datetime.now())
     todaysdatefull = todaysdatefullsecs[:12]
     todaysdate = todaysdatefull[:8] # '{:%Y,%m,%d}'.format(datetime.datetime.now())
-    todaysdatearch = todaysdatefull # '{:%Y,%m,%d,%H,%M}'.format(datetime.datetime.now())
 
     ## Define for Creating Archive dirs
-    if not rootdir:
-        rootdir = archive
+    if archive_root:
+        pass  # rootdir = archive
     else:
-        archive = '/mnt/Post_Complete/Complete_Archive/Uploaded'
+        archive_root = '/mnt/Post_Complete/Complete_Archive/Uploaded'
 
     # archive_uploaded = path.join(archive, "dateloaded_" + str(todaysdate).replace(",", ""), "uploaded_" + str(todaysdatefullsecs).replace(",", ""))
     dated_dir = "dateloaded_" + str(todaysdate).replace(",", "")
-    archive_uploaded_day = path.join(archive, dated_dir)
+    archive_uploaded_day = path.join(archive_root, dated_dir)
     imgdest_jpg_mozu = path.join(archive_uploaded_day, 'JPG_MOZU_LOAD')
     imgdest_jpg_mozu_loaded = path.join(imgdest_jpg_mozu, 'LOADED')
 
@@ -33,7 +32,7 @@ def batch_load_dated_mozu_jpgs(rootdir=None):
     # for f in img_list:
     #     shutil.copy2(f, imgdest_jpg_mozu)
 
-    import mozu_exec, mozu_image_util_functions
+    import mozu_exec  #, mozu_image_util_functions
     ## Compress and convert to jpg and store in separate dir for concurrent xfers
     #if path.isfile(pngout):
     load_collect_batch_mozu_list = glob.glob(path.join(archive_uploaded_day, '*/JPG_MOZU_LOAD/*.??[gG]'))
@@ -47,10 +46,9 @@ def batch_load_dated_mozu_jpgs(rootdir=None):
     load_batch_mozu_done = glob.glob(path.join(imgdest_jpg_mozu_loaded, '*.*[gG]'))
 
     return load_batch_mozu_done
-    #for f in load_batch_mozu_list:
+    # for f in load_batch_mozu_list:
     #    print path.abspath(f)
     #    #jpgout = mozu_image_util_functions.magick_convert_to_jpeg(f,destdir=imgdest_jpg_mozu)
-
 
 
 if __name__ == '__main__':
