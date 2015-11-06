8c41b5935c8f3461e479feb06979dd83fada141f
johnb <jbragato@yahoo.com>
2015-11-06 06:14:52 -0500

    x

--- python/timeout.py
+++ python/timeout.py
@@ -14,7 +14,7 @@ def timeout(seconds=10, error_message=os.strerror(errno.ETIME)):
     def decorator(func):
         def _handle_timeout(signum, frame):
             raise TimeoutError(error_message)
-        @functools.wraps(func)
+        #@functools.wraps(func)
         def wrapper(*args, **kwargs):
             signal.signal(signal.SIGALRM, _handle_timeout)
             signal.alarm(seconds)
