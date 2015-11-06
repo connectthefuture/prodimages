2ff29948bbab4dabc5fae440f2d6d9d61a769a05
johnb <jbragato@yahoo.com>
2015-11-06 06:20:16 -0500

    x

--- python/jbmodules/http_tools/timeout.py
+++ python/jbmodules/http_tools/timeout.py
@@ -6,16 +6,18 @@ import errno
 import os
 import signal
 
+
 class TimeoutError(Exception):
     pass
 
 def timeout(seconds=10, error_message=os.strerror(errno.ETIME)):
     def decorator(func):
-        def _handle_timeout(signum, frame):
-            raise TimeoutError(error_message)
-
+        def _handle_timeout_sysargs(signum, frame):
+            #raise TimeoutError(error_message)
+            sys.argv[1]
+        #@functools.wraps(func)
         def wrapper(*args, **kwargs):
-            signal.signal(signal.SIGALRM, _handle_timeout)
+            signal.signal(signal.SIGALRM, _handle_timeout_sysargs)
             signal.alarm(seconds)
             try:
                 result = func(*args, **kwargs)
