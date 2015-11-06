d530a442d068c78c10a7d58ee5527f86d55a30a4
johnb <jbragato@yahoo.com>
2015-11-06 03:59:33 -0500

    x

--- python/jbmodules/http_tools/ftp_functions.py
+++ python/jbmodules/http_tools/ftp_functions.py
@@ -99,7 +99,7 @@ def listcontents_ftplib(ftp_dir, remote_dir='', ext_filter='', download='', dest
     except AttributeError:
         print '96 -- AttributeError Error'
     finally:
-        print 'End ', cnt, ' <-- TotalCount \vSince {0:%b %d -- %Y}'.format(moddate)
+        print 'End ',  ' <-- \nFiles Modified: ', cnt, '\t\tSince {0:%b %d -- %Y}'.format(moddate)
         #session.storbinary('STOR ' + filename, fileread, 8*1024)
         session.quit()
     sorted_ftpdict = collections.OrderedDict(sorted(ftpmodtime_dict.items(), key=lambda t: t[1][0], reverse=False))
