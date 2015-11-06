20388f93aa6ba2c1f1dd22104a33a50e037cf595
johnb <jbragato@yahoo.com>
2015-11-06 06:13:45 -0500

    x

--- python/timeout.py
+++ python/timeout.py
@@ -6,6 +6,7 @@ import errno
 import os
 import signal
 
+
 class TimeoutError(Exception):
     pass
 
@@ -13,7 +14,7 @@ def timeout(seconds=10, error_message=os.strerror(errno.ETIME)):
     def decorator(func):
         def _handle_timeout(signum, frame):
             raise TimeoutError(error_message)
-
+        @functools.wraps(func)
         def wrapper(*args, **kwargs):
             signal.signal(signal.SIGALRM, _handle_timeout)
             signal.alarm(seconds)
