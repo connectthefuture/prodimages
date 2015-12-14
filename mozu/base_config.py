52dd4fd0c07911aa13dbe5e02682d17134d4670f
johnb <jbragato@yahoo.com>
2015-12-14 15:25:17 -0500

    x

--- mozu/base_config.py
+++ mozu/base_config.py
@@ -73,14 +73,17 @@ def get_mozu_client_authtoken():
     #  "http://requestb.in/q66719q6" #
     import requests, json
     set_environment()
-
+    _auth_headers_prod_addition = {'x-vol-tenant': TENANT_PRD, 'x-vol-master-catalog': MOZU_MASTER_CATID_PRD }
     _auth_headers = {'Content-type': 'application/json', 'Accept-Encoding': 'gzip, deflate'}
     if globals()['PROD'] == False:
         _auth_request = __STG_AUTH__
         _auth_url     = __MOZU_AUTH_URL_STG__
+
     elif globals()['PROD'] == True:
         _auth_request = __PRD_AUTH__
         _auth_url     = __MOZU_AUTH_URL_PRD__
+        _auth_headers = _auth_headers + _auth_headers_prod_addition
+        print _auth_headers
     else:
         _auth_request = __STG_AUTH__
         _auth_url     = __MOZU_AUTH_URL_STG__
