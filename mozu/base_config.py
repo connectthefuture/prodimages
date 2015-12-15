fea40f8ffdc3a164ee3c3d072802a11def6184c6
johnb <jbragato@yahoo.com>
2015-12-15 16:21:29 -0500

    x

--- mozu/base_config.py
+++ mozu/base_config.py
@@ -6,7 +6,7 @@
 # globals()['DEBUG'] = False #True
 from os import environ
 try:
-    if environ.get('PRD_ENV') and environ.get('PRD_ENV') != False:
+    if environ.get('PRD_ENV') and environ.get('PRD_ENV') is not False:
         globals()['PRD_ENV'] = True
     else:
         globals()['PRD_ENV'] = False #True
