2b54540fb9d69b28716a8c03bb3b72eb415af2e8
johnb <jbragato@yahoo.com>
2015-12-31 06:20:46 -0500

    x

--- python/jbmodules/image_processing/marketplace/x_vendorget_module_cronjobDloader.py
+++ python/jbmodules/image_processing/marketplace/x_vendorget_module_cronjobDloader.py
@@ -423,7 +423,7 @@ def download_mplce_url(urldest_tuple):
         #from jbmodules
         try:
             res = pycurl_ftp(imageurl=image_url,destpath=destpath)
-            print 'FTEPPEE --> ', res
+            print 'FTEPPEE --> {}\n{}\t\n'.format(res, image_url, destpath)
             return destpath
         except pycurl.error, error:
             print 'Pycurl error in FTP Download --> ', error
