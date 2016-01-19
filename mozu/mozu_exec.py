e2310165a25beee20d44665789be9da2ef9c50f0
johnb <jbragato@yahoo.com>
2016-01-19 17:20:46 -0500

    x

--- mozu/mozu_exec.py
+++ mozu/mozu_exec.py
@@ -247,11 +247,12 @@ def main(fileslist):
                     print "HTTP Status: {}\n Raising Integrity Error".format(content_resp.status_code)
                     raise ValueError #sqlalchemy.exc.IntegrityError()
             except TypeError:
-                print 'TYPE Error -- 409 DOCUMENT EXISTS continuing with update-->select query'
-                mozu_image_table = mozu_image_table_instance()
-                table_args = include_keys(values, __mozu_image_table_valid_keys__)
                 try:
-                    #mz_imageid = mozu_image_table.select(whereclause=((mozu_image_table.c.bf_imageid == table_args['bf_imageid']))).execute().fetchone()['mz_imageid']
+                    print 'TYPE Error -- 409 DOCUMENT EXISTS continuing with update-->select query'
+                    mozu_image_table = mozu_image_table_instance()
+                    table_args = include_keys(values, __mozu_image_table_valid_keys__)
+
+                    mz_imageid = mozu_image_table.select(whereclause=((mozu_image_table.c.bf_imageid == table_args['bf_imageid']))).execute().fetchone()['mz_imageid']
                     #md5checksum = mozu_image_table.select(whereclause=((mozu_image_table.c.bf_imageid == table_args['md5checksum']))).execute().fetchone()['mz_imageid']
                     # bf_imageid = mozu_image_table.select( whereclause=( (mozu_image_table.c.bf_imageid == table_args['mz_imageid']) ) ).execute().fetchone()['bf_imageid']
                     table_args['mz_imageid'] = values['mz_imageid'] = mz_imageid
