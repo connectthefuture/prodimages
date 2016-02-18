cf18b769ac2992cfdaa6cc8006a4b1a833d3f941
johnb <jbragato@yahoo.com>
2016-02-17 20:44:07 -0500

    x

--- python/jbmodules/image_processing/marketplace/x_vendorget_module_cronjobDloader.py
+++ python/jbmodules/image_processing/marketplace/x_vendorget_module_cronjobDloader.py
@@ -88,7 +88,7 @@ def sqlQuery_GetIMarketplaceImgs(vendor=None, vendor_brand=None, po_number=None,
         #consigstyle['vendor_style'] = row['vendor_style']
         styles[style_alt] = styledata
 
-    #print consigstyles
+    print '\n\n\n\n\n\n\n\t\tAttemptingt\t Download and Processing\vof\n\t\v{0} Image Urls\n\n\n\n\n\n'.format(len(styles))
     connection.close()
     return styles
 
