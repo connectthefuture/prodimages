8a9b14ef73403c35758b1f255db18a8abfa792a8
johnb <john.bragato@gmail.com>
2016-02-19 18:09:31 -0500

    commiting a bunch of reverts to the earlier reverts today on mozu exec, not touching base class

--- mozu/mozu_exec.py
+++ mozu/mozu_exec.py
@@ -38,7 +38,7 @@ def resource_documents_list(**kwargs):
 #######################################
 # Post - New Image, Creates Document
 @log
-def upload_new_data_content(**kwargs):
+def upload_new(**kwargs):
     from RESTClient import MozuRestClient
     from db import mozu_image_table_instance
 
@@ -64,17 +64,13 @@ def upload_new_data_content(**kwargs):
 
 # @log
 # # PUT - Upload/Update Image/DocumentContent
-def update_content_mz_image(**kwargs):
-    from RESTClient import MozuRestClient
-    from db import mozu_image_table_instance
-    mzclient = MozuRestClient(**kwargs)
-    content_response = mzclient.send_content(**kwargs)
-    print content_response.headers, "Update Mozu Content"
-    mozu_image_table = mozu_image_table_instance()
-    table_args = include_keys(kwargs, __mozu_image_table_valid_keys__)
-    update_db = mozu_image_table.update(values=dict(**table_args))
-    print content_response.headers, "Update DB MZ_IMAGE"
-    return content_response
+# def upsert_data_mz_image(**kwargs):
+#     from RESTClient import MozuRestClient
+#     if not args:
+#         mzclient = MozuRestClient(**kwargs)
+#     update_resp = mzclient.send_content(**kwargs)
+#     print update_resp.headers, "UpsertContent"
+#     return update_resp
 
 # PUT - Update Document Data and Content- Properties/Metadata
 @log
@@ -117,7 +113,6 @@ def upsert_data_mz_image(**kwargs):
                         print content_response, "Not in DB. Insert Result: ", insert_result.fetchone()['mz_imageid']
                         return insert_result.fetchone()
                     except sqlalchemy.exc.IntegrityError:
-                        print '116 mozuexec error-- SQLAlchemy Integrity Error'
                         content_response = mzclient.send_content(**kwargs)
                         if kwargs.get('bf_imageid'):
                             kwargs.get('bf_imageid')
@@ -145,53 +140,9 @@ def upsert_data_mz_image(**kwargs):
             #pass
 
     else:
-        res = upload_new_data_content(**kwargs)
+        res = upload_new(**kwargs)
         return res
-# DELETE - Delete Image/DocumentContent - Everything
-@log
-def delete_document_data_content(**kwargs):
-    from RESTClient import MozuRestClient
-    from db import mozu_image_table_instance
-    mzclient = MozuRestClient(**kwargs)
-    delete_resp = mzclient.delete_mz_image()
-    mozu_image_table = mozu_image_table_instance()
 
-    delete_db = mozu_image_table.delete( whereclause=(mozu_image_table.c.mz_imageid == kwargs.get('mz_imageid')) )
-    # res = delete_db.execute()
-    # TODO: Need to delete from db or alter insome way
-    print delete_resp.headers, "Delete \n", delete_db, "\nMZ CLIENTID in FUNCtion: ", kwargs
-    return delete_resp
-
-
-### GET Images - Content
-#
-# def download_document_content(outfile=None, **kwargs):
-#     from RESTClient import MozuRestClient
-#     print kwargs, 'KWARGS-26'
-#     mzclient = MozuRestClient(**kwargs)
-#     from os import path as path
-#     image_content = mzclient[     if not mzclient.bf_imageid:
-#         # Get bflyid from Oracle using mz_id
-#         from db import mozu_image_table_instance
-          #mozu_image_table = mozu_image_table_instance()
-#         bf_imageid = mozu_image_table.select( whereclause=( (mozu_image_table.c.mz_imageid == mzclient.mz_imageid) ) ).execute().fetchone()['bf_imageid']
-#         mzclient.bf_imageid = bf_imageid
-#     if not outfile:
-#         outfile = path.join('/tmp', mzclient.bf_imageid)
-#     else: pass
-#     with open(outfile,'w') as f:
-#         f.write(image_content)
-#     print locals(), "Downloaded Content"
-#     return path.abspath(outfile)
-#
-#
-# def read_document_content_headers(**kwargs):
-#     from RESTClient import MozuRestClient
-#     print kwargs, 'KWARGS-47'
-#     mzclient = MozuRestClient(**kwargs)
-#     image_data = mzclient.headers
-#     print image_data
-#     return image_data
 
 
 #######################################
@@ -202,7 +153,7 @@ def delete_document_data_content(**kwargs):
 def main(fileslist):
     import sqlalchemy
     from db import mozu_image_table_instance
-    from mozu_image_util_functions import compile_todict_for_class_instance_variables, magick_convert_to_jpeg
+    from mozu_image_util_functions import compile_todict_for_class_instance_variables, magick_convert_to_jpeg, netsrv101_path_maker
     # Compiles Data Payload and other Vars per Doc -- Including src_filepath -- **values keys set per instance
     # print type(fileslist), '<--Type\tLenLoFilepaths', len(fileslist), '\t', fileslist
     ### Date Defs
@@ -219,15 +170,16 @@ def main(fileslist):
     archive_uploaded_day = path.join(archive, "dateloaded_" + str(todaysdate).replace(",", ""))
     imgdest_jpg_mozu = path.join(archive_uploaded_day, 'JPG_MOZU_LOAD')
     # imgdest_jpg_mozu_loaded = path.join(imgdest_jpg_mozu, 'LOADED')
-
     if path.dirname(fileslist[0]).split('/')[-1] == 'JPG_MOZU_LOAD':
         #         fileslistX= [magick_convert_to_jpeg(f) for f in fileslist if f.split('.')[-1] == 'png']
-        fileslist_jpegs = [magick_convert_to_jpeg(f, destdir=imgdest_jpg_mozu) for f in fileslist if f]
+        fileslist = [magick_convert_to_jpeg(f, destdir=imgdest_jpg_mozu) for f in fileslist if f]
     else:
-        fileslist_jpegs = fileslist # [ path.abspath(f) for f in fileslist if f ]
-
-    compiled_instance_vars = compile_todict_for_class_instance_variables(fileslist=fileslist_jpegs)
+        fileslist = fileslist # [ path.abspath(f) for f in fileslist if f ]
+    if not path.isfile(fileslist[0]):
+        fileslist = netsrv101_path_maker(fileslist)
+    compiled_instance_vars = compile_todict_for_class_instance_variables(fileslist=fileslist)
     # print type(compiled_instance_vars), '<--Type\tLenCompiledInsVars', len(compiled_instance_vars), '\tKeys: ', compiled_instance_vars.keys()
+    # print compiled_instance_vars, "186-MZEXECY"
     for key,values in compiled_instance_vars.iteritems():
         # v = include_keys(values, __mozu_image_table_valid_keys__)
         # print "IncludedKeys: {}\n\tkey:\t{}\n\tvalues:\t{}".format(v.items(), key , values.popitem())
@@ -236,36 +188,36 @@ def main(fileslist):
             # ## ---> before loading(would actually need to redo the md5checksum from compiler)
             # Insert -- Then try Update if Insert to DB fails or Create NewDoc Fails to Mozu
             try:
-                content_resp = upload_new_data_content(**values)
-                if int(content_resp.keys()[0]) < 400:
+                values['mz_imageid'], response = upload_new(**values)
+                load_content_resp = upload_new(**values)
+                mozu_image_table = mozu_image_table_instance()
+                if int(load_content_resp.keys()[0]) < 400:
                     table_args = include_keys(values, __mozu_image_table_valid_keys__)
                     insert_db = mozu_image_table.insert(values=dict(**table_args))
                     insert_db.execute()
                     print 'Inserted --> ', values.items(), ' <-- ', insert_db
-                elif int(content_resp.keys()[0]) == 409:
-                    raise TypeError
+                elif int(load_content_resp.keys()[0]) == 409:
+                    table_args = include_keys(values, __mozu_image_table_valid_keys__)
+                    mz_imageid = mozu_image_table.select( whereclause=( (mozu_image_table.c.bf_imageid == table_args['bf_imageid']) ) ).execute().fetchone()['mz_imageid']
+                    bf_imageid = mozu_image_table.select( whereclause=( (mozu_image_table.c.mz_imageid == table_args['mz_imageid']) ) ).execute().fetchone()['bf_imageid']
+                    from mozu_exec_del_update import update_content_mz_image
+                    table_args['bf_imageid'] = values['bf_imageid'] = bf_imageid
+                    table_args['mz_imageid'] = values['mz_imageid'] = mz_imageid
+                    resp = update_content_mz_image(**values)
+                    print('RESP 207 mzexec: {}'.format(resp))
+                    #upsert_content_resp = upsert_data_mz_image(**values)  # ,dict(**values))
+                    if resp.http_status_code < 300:
+                        update_db = mozu_image_table.update(values=dict(**table_args),whereclause=mozu_image_table.c.bf_imageid == table_args['bf_imageid'])
+                        res = update_db.execute()
+                        print res, 'Updated--> ', values.items(), ' <-- ', update_db
                 else:
-                    print "HTTP Status: {}\n Raising Integrity Error".format(content_resp.status_code)
-                    raise ValueError #sqlalchemy.exc.IntegrityError()
-            except TypeError:
-                print 'TYPE Error -- 409 DOCUMENT EXISTS continuing with update-->select query'
-                mozu_image_table = mozu_image_table_instance()
-                table_args = include_keys(values, __mozu_image_table_valid_keys__)
-                mz_imageid = mozu_image_table.select(whereclause=((mozu_image_table.c.bf_imageid == table_args['bf_imageid']))).execute().fetchone()['mz_imageid']
-                #md5checksum = mozu_image_table.select(whereclause=((mozu_image_table.c.bf_imageid == table_args['md5checksum']))).execute().fetchone()['mz_imageid']
-                # bf_imageid = mozu_image_table.select( whereclause=( (mozu_image_table.c.bf_imageid == table_args['mz_imageid']) ) ).execute().fetchone()['bf_imageid']
-                table_args['mz_imageid'] = values['mz_imageid'] = mz_imageid
-                update_content_resp = update_content_mz_image(**values)
-                print "Updated Process Complete, ", update_content_resp.headers
-                if update_content_resp.status_code < 300:
-                    update_db = mozu_image_table.update(values=dict(**table_args),whereclause=mozu_image_table.c.bf_imageid == table_args['bf_imageid'])
-                    res = update_db.execute()
-                    print res, 'Updated--> ', values.items(), ' <-- ', update_db
+                    print "HTTP Status: {}\n Raising Integrity Error".format(load_content_resp.http_status_code)
+                    raise sqlalchemy.exc.IntegrityError()
             except ValueError: #sqlalchemy.exc.IntegrityError:
-                print 'VALUE Error and everything is or will be commented out below because it is in the db already'
+                print 'Type or VALUE Error and everything is or will be commented out below because it is in the db already'
                 #return 'IntegrityError'
             except KeyError:  # sqlalchemy.exc.IntegrityError:
-                print 'KEY Error and everything is or will be commented out below because it is in the db already'
+                print 'TYPE or Value Error and everything is or will be commented out below because it is in the db already'
                 #return 'IntegrityError'
                 #pass
                 # except IOError:
