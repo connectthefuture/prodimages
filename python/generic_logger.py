8f3b2706d590282860f20a348f96d030cb345fe1
johnb <john.bragato@gmail.com>
2016-02-19 20:08:48 -0500

    adding logging tools and configs

---
+++ python/generic_logger.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+def basic_log_file_obj(log_configuration='Admin_Client', **kwargs):
+    import logging, datetime
+    from os import path as path
+    configfile = kwargs.get('configfile', 'generic_logger_config.ini')
+    if outfile is None:
+        outfile = kwargs.get('outfile', str(__file__.__name__ + "_log.txt"))
+        outfile = path.join("/root/DropboxSync/bflyProdimagesSync/log", path.filename(outfile))
+    if configfile is not None:
+        logging.config.fileConfig(path.normpath(configfile))
+        hdlr = logging.FileHandler(outfile)
+        #formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+        formatter = logging.Formatter('%(asctime)s | %(name)s | %(module)s-%(lineno)04d | %(levelname)s | %(message)s')
+        hdlr.setFormatter(formatter)
+        myLogger = logging.getLogger(log_configuration)
+        myLogger.addHandler(hdlr) 
+        myLogger.setLevel(logging.WARNING)
+        return myLogger
+    else:
+        logging.basicConfig(filename=outfile, level=logging.DEBUG) # level=logging.INFO)
+        myLogger = logging.getLogger(log_configuration)      
+        imsg='\nLOGGING Level 1 - Active....\nINFO MODE SET'
+        wmsg='\nLOGGING Level 2 - Active....\nWARNING MODE SET'
+        emsg='\nLOGGING Level 3 - Active....\nERROR MODE SET'
+        cmsg='\nLOGGING Level 4 - Active....\nCRITICAL MODE SET'
+        exmsg='\nEXCEPTION!!!!!....\n--------Exception Raised---------\n'
+        dmsg='\nLOGGING DEBUGER - Active....\nDEBUG MODE SET'
+        myLogger.info(imsg)
+        myLogger.warn(wmsg)
+        myLogger.error(emsg)
+        myLogger.critical(cmsg)
+        myLogger.exception(exmsg)
+        myLogger.debug(dmsg)
+        myLogger.setLevel(logging.WARNING)
+        return myLogger
+
+#logger.error('We have a problem')
+#logger.info('While this is just chatty')
+
+
+if __name__ == '__main__':
+    pass()
+
+# if __name__ == '__main__':
+#     # Configure the logger
+#     # loggerConfigFileName: The name and path of your configuration file
+#     logging.config.fileConfig(path.normpath(loggerConfigFileName))
+
+#     # Create the logger
+#     # Admin_Client: The name of a logger defined in the config file
+#     myLogger = logging.getLogger('Admin_Client')
+
+#     imsg='GENERIC LOGGING Level 1 - Active....\nINFO MODE SET'
+#     wmsg='GENERIC LOGGING Level 2 - Active....\nWARNING MODE SET'
+#     emsg='GENERIC LOGGING Level 3 - Active....\nERROR MODE SET'
+#     cmsg='GENERIC LOGGING Level 4 - Active....\nCRITICAL MODE SET'
+#     dmsg='GENERIC LOGGING DEBUGGER - Active....\nDEBUG MODE SET'
+#     myLogger.info(imsg)
+#     myLogger.warn(wmsg)
+#     myLogger.error(emsg)
+#     myLogger.critical(cmsg)
+#     myLogger.debug(dmsg)
+
+#     # Shut down the logger
+#     logging.shutdown()
--- python/logging_func_output.py
+++ python/logging_func_output.py
@@ -1,18 +1,68 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-
-def log_to_file(original_function, filename=None):
-    import logging
+#### Logging Function Decorator ####
+# Defining a log function decorator to use as @log
+def log(original_function, filename=None):
+    import logging, datetime, json
     from os import path as path
     if filename is None:
-        filename = str(__file__ + "_log.txt")
-    logging.basicConfig(filename=filename, level=logging.INFO)
-    print "Logging to … {0}".format(path.abspath(filename))
+        filename = path.join("/root/DropboxSync/bflyProdimagesSync/log", str(original_function.__name__ + "_log.txt"))
+    logging.basicConfig(filename=filename, level=logging.DEBUG) # level=logging.INFO)
+    start_time = datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d--%H:%M.%S')
+    # print "Logging to … {0}".format(path.abspath(filename))
+    def new_function(*args, **kwargs):
+        try:
+            result = original_function(*args, **kwargs)
+            with open(filename, "wb+") as logfile:
+                logfile.write("\nStart: {0}".format(start_time))
+                logfile.write( "\n\tFunction \"%s\" called with\n\tkeyword arguments: %s\n\tpositional arguments: %s.\nThe result was %s.\n" % (original_function.__name__, json.dumps(kwargs), args, result)
+                )
+                end_time = datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d--%H:%M.%S')
+                logfile.write("\nEnd: {0}".format(end_time))
+            return result
+        except TypeError:
+            print 'NoneTypeError in Logger'
+            return
+    return new_function
+
+
+### Generic Logger
+def mr_logger(src_filepath,*args):
+    import datetime
+    current_dt = datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d')
+    logged_items = []
+    if len(args) > 0:
+        for arg in args:
+            logit = "{}\t{}\n".format(current_dt,arg)
+            logged_items.append(logit)
+    for i in logged_items:
+        with open(src_filepath, 'ab+') as f:
+            f.write(i)
+    return src_filepath
+
+
+## Log function args and calls to outfile
+def log_to_file(original_function, outfile=None, configfile=None):
+    import logging, datetime
+    from os import path as path
+    configfile = kwargs.get('configfile', 'logconfig.ini')
+    if outfile is None:
+        outfile = kwargs.get('outfile', str(original_function.__name__ + "_log.txt"))
+        outfile = path.join("/root/DropboxSync/bflyProdimagesSync/log", path.filename(outfile))
+    if configfile is not None:
+        logging.config.fileConfig(path.normpath(configfile))
+    else:
+        logging.basicConfig(filename=outfile, level=logging.DEBUG) # level=logging.INFO)
+    start_time = datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d--%H:%M.%S')
+    print "Logging to … {0}".format(path.abspath(outfile))
     def new_function(*args, **kwargs):
         result = original_function(*args, **kwargs)
-        with open(filename, "ab+") as logfile:
+        with open(outfile, "ab+") as logfile:
             logfile.write("Function '%s' called with positional arguments %s and keyword arguments %s. The result was %s.\n" % (original_function.__name__, args, kwargs, result))
         return result
     return new_function
 
+
+if __name__ == '__main__':
+    pass()
