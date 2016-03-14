30b2e9714b621ca95109c2959c0f84047741dbe0
johnb <john.bragato@gmail.com>
2016-03-14 15:22:06 -0400

    xxx

--- python/newAll_Sites_CacheClear.py
+++ python/newAll_Sites_CacheClear.py
@@ -105,7 +105,7 @@ def subproc_localIS(colorstyle=None,version=None):
     import subprocess
     from time import sleep
     cmdBF     = 'curl -X POST -d style={0} -d version={1} http://clearcache.bluefly.corp/BFClear2.php'.format(colorstyle,version).split(' ')
-    cmdMobile = 'curl -X POST -d style={0} -d version={1} http://clearcache.bluefly.corp/BFMobileClear2.php'.format(colorstyle,version).split(' ')
+    cmdMobile = 'curl -X POST -d style={0} http://clearcache.bluefly.corp/BFMobileClear2.php'.format(colorstyle).split(' ')
     subprocess.call(cmdBF)
     sleep(.2)
     subprocess.call(cmdMobile)
