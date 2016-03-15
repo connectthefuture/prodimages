48fb791ebefd63987b12e91412f20ec4cfeec4b3
johnb <john.bragato@gmail.com>
2016-03-15 02:06:09 -0400

    x

--- python/parse_args_test.py
+++ python/parse_args_test.py
@@ -6,6 +6,10 @@ import argparse
 # Define and Instantiate parser Base
 parser = argparse.ArgumentParser(description='Initial primary script description and title') #,add_help=False)
 #
+##############################
+## Bluefly Script Related Args
+##############################
+#
 ######### Style
 parser.add_argument('--style',
     action='store_true', help='Valid 9 Digit Bluefly Style' )
@@ -18,13 +22,30 @@ parser.add_argument('styles_list',
 parser.add_argument('--vendor','-vend' ,
     action='store_true', help='VendorID or Name, fuzzy searches are valid' )
 #
+###### Cmdline arg as Choice for Scripts Actions. What do you want it to do.
+parser.add_argument('--options', '-o',
+    default='new', choices=['new', 'update', 'reload', 'delete'], help='Choice of Script Actions')
+
+
+###################
+## Genral Base Args
+###################
 ######## Days-Since
+#
 parser.add_argument('--days-since', '-days',
     action='store', type=int, help='Number of days to include in script and related queries')
 #
 ######## Adding Remaining Args Not Specified in Parser
 parser.add_argument('args',
     nargs=argparse.REMAINDER)
+#
+###### Verbose Flag
+parser.add_argument('-v', '--verbose',
+    action='store_true', help='verbose output' )
+#
+############################
+## File + Image Related Args
+############################
 #
 ######## Outfile - write
 parser.add_argument('--outfile',
@@ -33,15 +54,6 @@ parser.add_argument('--outfile',
 ####### Output Format if Image File
 parser.add_argument('--format', '-fmt',
     action='store', help='The Desired Image format for Output. Options: png, jpeg, tiff, gif')
-#
-###### Cmdline arg as Choice for Scripts Actions. What do you want it to do.
-parser.add_argument('--options', '-o',
-    default='new', choices=['new', 'update', 'reload', 'delete'], help='Choice of Script Actions')
-#
-###### Verbose Flag
-parser.add_argument('-v', '--verbose',
-    action='store_true', help='verbose output' )
-
 
 
 ##new parser, for script file calling this as parent
@@ -53,7 +65,7 @@ parser.add_argument('-v', '--verbose',
 
 
 def main():
-    import argparse
+    import argparse, sys
 
     parser = argparse.ArgumentParser()
 
@@ -77,8 +89,10 @@ def main():
                                help='Remove the contents of the directory, too',
                                )
 
-    print parser.parse_args()
+    args = sys.argv[1:]
+    parsed = parser.parse_args(''.join(args))
 
+    return parsed
 
 
 
