d25f62ec11b0403b1cb586d7d5132f3800976dc3
johnb <john.bragato@gmail.com>
2016-03-21 15:03:12 -0400

    test

--- python/newAll_Sites_CacheClear.py
+++ python/newAll_Sites_CacheClear.py
@@ -174,6 +174,7 @@ def main(colorstyle_list=None):
         bflypdp_url = "http://www.bluefly.com/Bluefly-generic-pdp-slug/p/{0}/detail.fly".format(colorstyle)
         found_links = url_get_links(bflypdp_url)
         try:
+            print 'Version before query {}'.format(locals().get('version'))
             version =  query_version_number(colorstyle).get('colorstyle')['version']
         except:
             print('IndexErr - Version Error style {}'.format(colorstyle))
