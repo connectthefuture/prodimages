101d7f6225bf6c3ce8ad0595b211828e6b410fdb
johnb <john.bragato@gmail.com>
2016-03-29 03:07:23 -0400

    x

--- python/reload_mozu_from_l3.py
+++ python/reload_mozu_from_l3.py
@@ -22,6 +22,7 @@ def main(styles_list, ext='.png', root_dir='/mnt/images'):
 
     flist = []
     for fname in styles_list:
+        print 'PreCond ', root_dir,fname+ext
         if fname is not None:
             src = path.join(root_dir, fname[:4], fname + ext)
             #from RESTClient import MozuRestClient
