712439e18ff75a776f26005b3a1df4c9c01eb576
johnb <john.bragato@gmail.com>
2016-03-29 03:03:05 -0400

    x

--- python/reload_mozu_from_l3.py
+++ python/reload_mozu_from_l3.py
@@ -4,20 +4,22 @@
 
 def main(styles_list, ext='.png', root_dir='/mnt/images'):
     from os import getcwd, environ, path, chdir
+    import sys
     MOZU_CODE_DIR = '/usr/local/batchRunScripts/mozu'
     JBMODULES_ROOT = '/usr/local/batchRunScript/python/jbmodules'
     chdir(MOZU_CODE_DIR)
-    print getcwd()
-
+    sys.path.append(MOZU_CODE_DIR)
+    sys.path.append(JBMODULES_ROOT)
+    import db, mozu_image_util_functions, mozu_exec
+#####
+    #
+    #######
+    #######
     environ['SQLALCHEMY_DATABASE_URI'] = 'oracle+cx_oracle://MZIMG:m0zu1mages@borac102-vip.l3.bluefly.com:1521/bfyprd12'
     environ['PRD_ENV'] = "1"
     # environ['PYDEBUG'] = "1"
     print('Starting.\nReloading {0} Images for {1} Styles from {2} to Mozu'.format(ext.lstrip('.').upper(), len(styles_list), root_dir))
 
-    import sys
-    sys.path.append(JBMODULES_ROOT)
-    sys.path.append()
-    import db, mozu_image_util_functions
     flist = []
     for fname in styles_list:
         if fname is not None:
@@ -28,7 +30,6 @@ def main(styles_list, ext='.png', root_dir='/mnt/images'):
             print src,'\n', fname, '\t', ext
             flist.append(src)
 
-    import mozu_exec
     mozu_exec.main(flist)
     print('Finished.\nReloaded {0} Images for {1} Styles from {2} to Mozu'.format(ext.lstrip('.').upper(), len(flist), root_dir))
     loaded_filenames = [f.split('/')[-1].split('.')[0] for f in flist if f is not None]
