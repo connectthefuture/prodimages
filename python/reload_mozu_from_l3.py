9cc77e169c70135a53a3e6a7e8342a6376262153
johnb <john.bragato@gmail.com>
2016-03-29 02:59:22 -0400

    x

--- python/reload_mozu_from_l3.py
+++ python/reload_mozu_from_l3.py
@@ -25,7 +25,7 @@ def main(styles_list, ext='.png', root_dir='/mnt/images'):
             #from RESTClient import MozuRestClient
             #rest_client = MozuRestClient()
             #resp = rest_client.send_content(src_filepath=src)
-            print(src)
+            print src,'\n', fname, '\t', ext
             flist.append(src)
 
     import mozu_exec
