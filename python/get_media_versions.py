08397a5b7c64602c02cbd15eca5a2f27289173bd
johnb <john.bragato@gmail.com>
2016-03-31 20:05:41 -0400

    media ver gets

--- python/get_media_versions.py
+++ python/get_media_versions.py
@@ -10,19 +10,52 @@ def query_version_number(colorstyle):
         colorstyles = colorstyle
         colorstyles = tuple(["{0}".format(s) for s in colorstyles])
         querymake_version_number = "SELECT DISTINCT POMGR.PO_LINE.PRODUCT_COLOR_ID as colorstyle, POMGR.PRODUCT_COLOR.IMAGE_READY_DT as image_ready_dt, POMGR.PRODUCT_COLOR.VERSION as version FROM POMGR.PRODUCT_COLOR RIGHT JOIN POMGR.PO_LINE ON POMGR.PO_LINE.PRODUCT_COLOR_ID = POMGR.PRODUCT_COLOR.ID RIGHT JOIN POMGR.PO_HDR ON POMGR.PO_HDR.ID = POMGR.PO_LINE.PO_HDR_ID RIGHT JOIN POMGR.VENDOR ON POMGR.VENDOR.ID = POMGR.PO_HDR.VENDOR_ID INNER JOIN POMGR.LK_PO_TYPE ON POMGR.LK_PO_TYPE.ID = POMGR.PO_HDR.PO_TYPE_ID LEFT JOIN POMGR.INVENTORY ON POMGR.INVENTORY.PRODUCT_COLOR_ID = POMGR.PRODUCT_COLOR.ID LEFT JOIN POMGR.PRODUCT_DETAIL ON POMGR.PRODUCT_COLOR.PRODUCT_ID = POMGR.PRODUCT_DETAIL.PRODUCT_ID LEFT JOIN POMGR.PRODUCT_COLOR_DETAIL ON POMGR.PRODUCT_COLOR.PRODUCT_ID = POMGR.PRODUCT_COLOR_DETAIL.PRODUCT_COLOR_ID WHERE POMGR.PRODUCT_COLOR.IMAGE_READY_DT is not null AND POMGR.PO_LINE.PRODUCT_COLOR_ID in {0} ORDER BY POMGR.PO_LINE.PRODUCT_COLOR_ID DESC Nulls Last, POMGR.PRODUCT_COLOR.IMAGE_READY_DT DESC Nulls Last".format(colorstyles)
+        q= """SELECT DISTINCT
+              POMGR.PRODUCT_COLOR_DETAIL.PRODUCT_COLOR_ID AS "colorstyle",
+              POMGR.PRODUCT_COLOR.IMAGE_READY_DT          AS image_ready_dt,
+              POMGR.PRODUCT_COLOR.VERSION                 AS VERSION0,
+              POMGR.PRODUCT_COLOR_DETAIL.MEDIA_VERSION    AS "media_version",
+              POMGR.PRODUCT_COLOR.MODIFIED_DATE           AS MODIFIED_DATE1
+            FROM
+              POMGR.PRODUCT_COLOR
+            LEFT JOIN POMGR.PRODUCT_COLOR_DETAIL
+            ON
+              POMGR.PRODUCT_COLOR.id = POMGR.PRODUCT_COLOR_DETAIL.PRODUCT_COLOR_ID
+            WHERE
+              POMGR.PRODUCT_COLOR_DETAIL.PRODUCT_COLOR_ID in {0}
+            AND POMGR.PRODUCT_COLOR.VENDOR_STYLE NOT LIKE '%VOID%'
+            ORDER BY
+              image_ready_dt DESC Nulls Last""".format(colorstyles)
     else:
         if type(colorstyle) == list:
             colorstyle=colorstyle[0]
+        q= """SELECT DISTINCT
+              POMGR.PRODUCT_COLOR_DETAIL.PRODUCT_COLOR_ID AS "colorstyle",
+              POMGR.PRODUCT_COLOR.IMAGE_READY_DT          AS image_ready_dt,
+              POMGR.PRODUCT_COLOR.VERSION                 AS VERSION0,
+              POMGR.PRODUCT_COLOR_DETAIL.MEDIA_VERSION    AS "media_version",
+              POMGR.PRODUCT_COLOR.MODIFIED_DATE           AS MODIFIED_DATE1
+            FROM
+              POMGR.PRODUCT_COLOR
+            LEFT JOIN POMGR.PRODUCT_COLOR_DETAIL
+            ON
+              POMGR.PRODUCT_COLOR.id = POMGR.PRODUCT_COLOR_DETAIL.PRODUCT_COLOR_ID
+            WHERE
+              POMGR.PRODUCT_COLOR_DETAIL.PRODUCT_COLOR_ID like '{0}%'
+            AND POMGR.PRODUCT_COLOR.VENDOR_STYLE NOT LIKE '%VOID%'
+            ORDER BY
+              image_ready_dt DESC Nulls Last""".format(colorstyle)
+    else:
         querymake_version_number = "SELECT DISTINCT POMGR.PO_LINE.PRODUCT_COLOR_ID as colorstyle, POMGR.PRODUCT_COLOR.IMAGE_READY_DT as image_ready_dt, POMGR.PRODUCT_COLOR.VERSION as version FROM POMGR.PRODUCT_COLOR RIGHT JOIN POMGR.PO_LINE ON POMGR.PO_LINE.PRODUCT_COLOR_ID = POMGR.PRODUCT_COLOR.ID RIGHT JOIN POMGR.PO_HDR ON POMGR.PO_HDR.ID = POMGR.PO_LINE.PO_HDR_ID RIGHT JOIN POMGR.VENDOR ON POMGR.VENDOR.ID = POMGR.PO_HDR.VENDOR_ID INNER JOIN POMGR.LK_PO_TYPE ON POMGR.LK_PO_TYPE.ID = POMGR.PO_HDR.PO_TYPE_ID LEFT JOIN POMGR.INVENTORY ON POMGR.INVENTORY.PRODUCT_COLOR_ID = POMGR.PRODUCT_COLOR.ID LEFT JOIN POMGR.PRODUCT_DETAIL ON POMGR.PRODUCT_COLOR.PRODUCT_ID = POMGR.PRODUCT_DETAIL.PRODUCT_ID LEFT JOIN POMGR.PRODUCT_COLOR_DETAIL ON POMGR.PRODUCT_COLOR.PRODUCT_ID = POMGR.PRODUCT_COLOR_DETAIL.PRODUCT_COLOR_ID WHERE POMGR.PRODUCT_COLOR.IMAGE_READY_DT is not null AND POMGR.PO_LINE.PRODUCT_COLOR_ID like '%{0}%' ORDER BY POMGR.PO_LINE.PRODUCT_COLOR_ID DESC Nulls Last, POMGR.PRODUCT_COLOR.IMAGE_READY_DT DESC Nulls Last".format(colorstyle)
 
     try:
-        result = connection.execute(querymake_version_number)
+        result = connection.execute(q)
         styles = {}
         for row in result:
             #style_info = {}
             #style_info['version'] = row['version']
             # Convert Colorstyle to string then set as KEY
-            styles[str(row['colorstyle'])] = row['version']
+            styles[str(row['colorstyle'])] = row['media_version']
         return styles
     except sqlalchemy.exc.DatabaseError:
         print 'This Search needs to have more than 1 style, \nyou returned zero or 1 style'
