ceec57c0fb8b3f32698f40ece07c55548050572e
johnb <john.bragato@gmail.com>
2016-04-08 17:05:19 -0400

    simple text file reader util added for reading cmd line text files

---
+++ python/text_file_reader.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+
+def main(url):
+    import requests
+    resp = requests.get(url)
+    if resp.apparent_encoding == 'ascii':
+        lines_list = resp.content.split('\r')
+
+
+####################################
+# Define and Instantiate parser Base
+import argparse
+#####
+parser = argparse.ArgumentParser(description='Initial primary script description and title') #,add_help=False)
+#
+###### Cmdline arg as Choice for Scripts Actions. What do you want it to do.
+parser.add_argument('--file', '-f',
+    type=argparse.FileType('R'), help='Provide the full path to the file to be read')
+##
+######## Outfile - write
+parser.add_argument('--outfile',
+    type=argparse.FileType('w'), help='The Output Destination Absolute Filepath. Reletive filepaths only is acceptable if output is to current directory')
+#
+parser.add_argument('--options', '-o',
+    default='split', choices=['split', 'line', 'replace', 'delete'], help='Choice of Script Actions')
+
+if __name__ == '__main__':
+    print parser.parse_args()
+    print('\n\n\n\n\n----------\n\n\n\n')
+    #main()
