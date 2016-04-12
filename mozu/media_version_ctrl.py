0b1a73ba000cc7ea4250d2cd50135f3b2c7d71bc
johnb <john.bragato@gmail.com>
2016-04-12 16:14:49 -0400

    rm unnecessary vars

--- mozu/media_version_ctrl.py
+++ mozu/media_version_ctrl.py
@@ -104,7 +104,7 @@ def build_media_version_number_data_batch(colorstyles,**kwargs):
 def _exec_put_data_batch(**kwargs):
     import requests, json
     media_version_api_url    = 'http://ccapp102.l3.bluefly.com:17080/manager/api/v2/productsattributes/update'
-    qa_media_version_api_url = 'http://manager.qa.bluefly.com/manager/api/v2/productsattributes/update'
+    qa_media_version_api_url = "http://manager.qa.bluefly.com/manager/api/v2/productsattributes/update"
     dest_url = kwargs.get('dest_url', media_version_api_url)
     headers = kwargs.get('headers', {"Content-Type": "application/json"} )
     data = kwargs.get('data', '')
